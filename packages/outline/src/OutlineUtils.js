6d340aef111d60d0908eae4c5bc9eaff3b6aa372
Dominic Gannaway <dg@domgan.com>
2020-11-30 10:29:59 +0000

    Add editor key

--- packages/outline/src/OutlineUtils.js
+++ packages/outline/src/OutlineUtils.js
@@ -1,5 +1,7 @@
 // @flow strict
 
+let keyCounter = 0;
+
 // inviariant(condition, message) will refine types based on "condition", and
 // if "condition" is false will throw an error. This functio is special-cased
 // in flow itself, so we can't name it anything else.
@@ -9,4 +11,8 @@ export function invariant(cond: boolean, message: string) {
     err.name = 'Invariant Violation';
     throw err;
   }
+}
+
+export function generateRandomKey(): string {
+  return '#' + keyCounter++;
 }

8825fb7dd0468712d0bd2a56ed9f08a16eeaad34
Dominic Gannaway <dg@domgan.com>
2020-12-16 22:16:20 +0000

    Fix script

--- scripts/prepare-release.js
+++ scripts/prepare-release.js
@@ -18,10 +18,7 @@ async function prepareOutlineReactPackage() {
   await exec(`mkdir ./packages/outline-react/npm`);
   await exec(`cp -R ./packages/outline-react/dist ./packages/outline-react/npm`);
   await exec(`cp -R ./packages/outline-react/*.js ./packages/outline-react/npm`);
-  // await exec(`cp -R ./packages/outline-react/OutlineEnv.js ./packages/outline-react/npm`);
-  // await exec(`cp -R ./packages/outline-react/OutlineHotKeys.js ./packages/outline-react/npm`);
-  // await exec(`cp -R ./packages/outline-react/use* ./packages/outline-react/npm`);
-  // await exec(`cp -R ./packages/outline-react/package.json ./packages/outline-react/npm`);
+  await exec(`cp -R ./packages/outline-react/package.json ./packages/outline-react/npm`);
   await exec(`cp -R LICENSE ./packages/outline-react/npm`);
   await exec(`cp -R ./packages/outline-react/README.md ./packages/outline-react/npm`);
 }

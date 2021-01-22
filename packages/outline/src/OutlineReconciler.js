ba283749d88b02cf4897d3c4db9fac6d3a2ed1d7
Dominic Gannaway <dg@domgan.com>
2021-01-22 19:31:52 +0000

    Remove console.log

--- packages/outline/src/OutlineReconciler.js
+++ packages/outline/src/OutlineReconciler.js
@@ -545,7 +545,6 @@ function reconcileSelection(selection: Selection, editor: OutlineEditor): void {
       focusOffset = 1;
     }
   }
-  console.log(anchorOffset, focusOffset);
   domSelection.setBaseAndExtent(
     anchorDOM.firstChild,
     anchorOffset,

5b1f3262381706d552127f3862cf6fd96fd6c04a
Dominic Gannaway <dg@domgan.com>
2021-01-28 10:35:10 +0000

    Add placeholder tests

--- packages/outline/src/__tests__/OutlineEditor-test.js
+++ packages/outline/src/__tests__/OutlineEditor-test.js
@@ -5,6 +5,11 @@ let ReactTestUtils;
 let Outline;
 let ParagraphNode;
 
+function sanitizeHTML(html) {
+  // Remove the special space characters
+  return html.replace(/\uFEFF/g, '');
+}
+
 describe('OutlineEditor tests', () => {
   beforeEach(() => {
     React = require('react');
@@ -91,6 +96,61 @@ describe('OutlineEditor tests', () => {
       parent: '_1',
       type: 'text',
       url: null,
+    });
+  });
+
+  describe('Placeholder', () => {
+    it('Placeholder shows when there is no content', () => {
+      editor.setPlaceholder('Placeholder text');
+
+      editor.update((view) => {
+        const paragraph = ParagraphNode.createParagraphNode();
+        const text = Outline.createTextNode();
+        paragraph.append(text);
+        view.getRoot().append(paragraph);
+      }, true);
+
+      expect(sanitizeHTML(container.innerHTML)).toBe(
+        '<div contenteditable="true"><div class="placeholder" style="display: block;">' +
+          'Placeholder text</div><p><span data-text="true"><br></span></p></div>',
+      );
+    });
+
+    it('Placeholder does not should when there is content', () => {
+      editor.setPlaceholder('Placeholder text');
+
+      editor.update((view) => {
+        const paragraph = ParagraphNode.createParagraphNode();
+        const text = Outline.createTextNode('Some text');
+        paragraph.append(text);
+        view.getRoot().append(paragraph);
+      }, true);
+
+      expect(sanitizeHTML(container.innerHTML)).toBe(
+        '<div contenteditable="true"><div class="placeholder" style="display: none;">' +
+          'Placeholder text</div><p dir="ltr"><span data-text="true">Some text</span></p></div>',
+      );
+    });
+
+    it('Placeholder does not should when there are two paragraphs', () => {
+      editor.setPlaceholder('Placeholder text');
+
+      editor.update((view) => {
+        const paragraph = ParagraphNode.createParagraphNode();
+        const text = Outline.createTextNode();
+        paragraph.append(text);
+        const paragraph2 = ParagraphNode.createParagraphNode();
+        const text2 = Outline.createTextNode();
+        paragraph2.append(text2);
+        view.getRoot().append(paragraph);
+        view.getRoot().append(paragraph2);
+      }, true);
+
+      expect(sanitizeHTML(container.innerHTML)).toBe(
+        '<div contenteditable="true"><div class="placeholder" style="display: none;">' +
+          'Placeholder text</div><p><span data-text="true"><br></span></p><p>' +
+          '<span data-text="true"><br></span></p></div>',
+      );
     });
   });
 });

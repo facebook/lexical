4ef3f5dc0c80936a590abf57763d399c7d26edd5
Dominic Gannaway <dg@domgan.com>
2021-02-08 17:50:18 +0000

    Refactor OutlineSelection tests

--- scripts/build.js
+++ scripts/build.js
@@ -53,7 +53,8 @@ const outlineExtensionsExternals = outlineExtensions.map((node) => {
 
 const outlineReactModules = fs
   .readdirSync(path.resolve('./packages/outline-react/src'))
-  .map((str) => path.basename(str, '.js'));
+  .map((str) => path.basename(str, '.js'))
+  .filter((str) => !str.includes('__tests__'));
 const outlineReactModuleExternals = outlineReactModules.map((module) => {
   const external = `outline-react/${module}`;
   wwwMappings[external] = module;

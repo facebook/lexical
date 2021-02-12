f8f12a2297a02e4ac87990db1e1941e599ffca9d
Yangshun Tay <tay.yang.shun@gmail.com>
2021-02-13 02:27:35 +0800

    core: add setHashtag mutator to OutlineTextNode (#94)
    * core: add setHashtag mutator to OutlineTextNode
    
    * change mutator
    
    * Fix Flow

--- packages/outline/src/OutlineTextNode.js
+++ packages/outline/src/OutlineTextNode.js
@@ -420,6 +420,10 @@ export class TextNode extends OutlineNode {
   }
 
   // Mutators
+  setHashtag(): TextNode {
+    const newFlags = this.getTextNodeFormatFlags('hashtag', null, true);
+    return this.setFlags(newFlags);
+  }
 
   setURL(url: string | null): TextNode {
     shouldErrorOnReadOnly();

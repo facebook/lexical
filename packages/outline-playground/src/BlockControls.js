00428e5e02fdf23ae4e1f70338fb325bd4d4a913
Dominic Gannaway <trueadm@users.noreply.github.com>
2021-03-01 11:48:45 +0000

    Create block control dropdown (#139)
    * Create block control dropdown
    
    * Add Code node

--- packages/outline-playground/src/BlockControls.js
+++ packages/outline-playground/src/BlockControls.js
@@ -3,9 +3,17 @@
 import type {OutlineEditor, ViewModel} from 'outline';
 import {HeadingNode} from 'outline-extensions/HeadingNode';
 import {ListNode} from 'outline-extensions/ListNode';
+import {createParagraphNode} from 'outline-extensions/ParagraphNode';
+import {createHeadingNode} from 'outline-extensions/HeadingNode';
+import {createListNode} from 'outline-extensions/ListNode';
+import {createListItemNode} from 'outline-extensions/ListItemNode';
+import {createQuoteNode} from 'outline-extensions/QuoteNode';
+import {createCodeNode} from 'outline-extensions/CodeNode';
 
 import * as React from 'react';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
+// $FlowFixMe
+import {createPortal} from 'react-dom';
 
 // TODO: create a functional dropdown and selection input
 export default function BlockControls({
@@ -17,6 +25,8 @@ export default function BlockControls({
   const [position, setPosition] = useState(0);
   const [editorPosition, setEditorPosition] = useState(0);
   const [blockType, setBlockType] = useState('paragraph');
+  const [showDropDown, setShowDropDown] = useState(false);
+  const blockControlsRef = useRef(null);
 
   useEffect(() => {
     return editor.addUpdateListener((viewModel: ViewModel) => {
@@ -51,15 +61,242 @@ export default function BlockControls({
   }, [editor, editorPosition, selectedBlockKey]);
 
   return selectedBlockKey !== null ? (
-    <div id="block-controls" style={{top: position}}>
-      <div
-        className="button"
-        role="button"
-        tabIndex="0"
-        aria-label="Formatting Options">
-        <span className={'block-type ' + blockType} />
-        <span className="block-arrow" />
+    <>
+      <div id="block-controls" style={{top: position}} ref={blockControlsRef}>
+        <button
+          onClick={() => setShowDropDown(!showDropDown)}
+          aria-label="Formatting Options">
+          <span className={'block-type ' + blockType} />
+          <span className="block-arrow" />
+        </button>
       </div>
-    </div>
+      {showDropDown &&
+        createPortal(
+          <DropdownList
+            editor={editor}
+            blockType={blockType}
+            blockControlsRef={blockControlsRef}
+            setShowDropDown={setShowDropDown}
+          />,
+          document.body,
+        )}
+    </>
   ) : null;
+}
+
+function DropdownList({
+  editor,
+  blockType,
+  blockControlsRef,
+  setShowDropDown,
+}: {
+  editor: OutlineEditor,
+  blockType: string,
+  blockControlsRef: {current: null | HTMLElement},
+  setShowDropDown: (boolean) => void,
+}): React$Node {
+  const dropDownRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const blockControls = blockControlsRef.current;
+    const dropDown = dropDownRef.current;
+
+    if (blockControls !== null && dropDown !== null) {
+      const {top, right} = blockControls.getBoundingClientRect();
+      const {width} = dropDown.getBoundingClientRect();
+      dropDown.style.top = `${top + 40}px`;
+      dropDown.style.left = `${right - width}px`;
+    }
+  }, [blockControlsRef]);
+
+  useEffect(() => {
+    const dropDown = dropDownRef.current;
+    const blockControls = blockControlsRef.current;
+
+    if (dropDown !== null && blockControls !== null) {
+      const handle = (event: MouseEvent) => {
+        // $FlowFixMe: no idea why flow is complaining
+        const target: HTMLElement = event.target;
+
+        if (!dropDown.contains(target) && !blockControls.contains(target)) {
+          setShowDropDown(false);
+        }
+      };
+      document.addEventListener('click', handle);
+
+      return () => {
+        document.removeEventListener('click', handle);
+      };
+    }
+  }, [blockControlsRef, setShowDropDown]);
+
+  const formatParagraph = () => {
+    if (blockType !== 'paragraph') {
+      editor.update((view) => {
+        const selection = view.getSelection();
+
+        if (selection !== null) {
+          const anchorNode = selection.getAnchorNode();
+          const parent = anchorNode.getParentBlockOrThrow();
+          const children = parent.getChildren();
+          const paragraph = createParagraphNode();
+          children.forEach((child) => paragraph.append(child));
+          parent.replace(paragraph);
+        }
+      });
+    }
+    setShowDropDown(false);
+  };
+
+  const formatLargeHeading = () => {
+    if (blockType !== 'h1') {
+      editor.update((view) => {
+        const selection = view.getSelection();
+
+        if (selection !== null) {
+          const anchorNode = selection.getAnchorNode();
+          const parent = anchorNode.getParentBlockOrThrow();
+          const children = parent.getChildren();
+          const paragraph = createHeadingNode('h1');
+          children.forEach((child) => paragraph.append(child));
+          parent.replace(paragraph);
+        }
+      });
+    }
+    setShowDropDown(false);
+  };
+
+  const formatSmallHeading = () => {
+    if (blockType !== 'h2') {
+      editor.update((view) => {
+        const selection = view.getSelection();
+
+        if (selection !== null) {
+          const anchorNode = selection.getAnchorNode();
+          const parent = anchorNode.getParentBlockOrThrow();
+          const children = parent.getChildren();
+          const paragraph = createHeadingNode('h2');
+          children.forEach((child) => paragraph.append(child));
+          parent.replace(paragraph);
+        }
+      });
+    }
+    setShowDropDown(false);
+  };
+
+  const formatBulletList = () => {
+    if (blockType !== 'ul') {
+      editor.update((view) => {
+        const selection = view.getSelection();
+
+        if (selection !== null) {
+          const anchorNode = selection.getAnchorNode();
+          const parent = anchorNode.getParentBlockOrThrow();
+          const children = parent.getChildren();
+          const list = createListNode('ul');
+          const listItem = createListItemNode();
+          list.append(listItem);
+          children.forEach((child) => listItem.append(child));
+          parent.replace(list);
+        }
+      });
+    }
+    setShowDropDown(false);
+  };
+
+  const formatNumberedList = () => {
+    if (blockType !== 'ol') {
+      editor.update((view) => {
+        const selection = view.getSelection();
+
+        if (selection !== null) {
+          const anchorNode = selection.getAnchorNode();
+          const parent = anchorNode.getParentBlockOrThrow();
+          const children = parent.getChildren();
+          const list = createListNode('ol');
+          const listItem = createListItemNode();
+          list.append(listItem);
+          children.forEach((child) => listItem.append(child));
+          parent.replace(list);
+        }
+      });
+    }
+    setShowDropDown(false);
+  };
+
+  const formatQuote = () => {
+    if (blockType !== 'quote') {
+      editor.update((view) => {
+        const selection = view.getSelection();
+
+        if (selection !== null) {
+          const anchorNode = selection.getAnchorNode();
+          const parent = anchorNode.getParentBlockOrThrow();
+          const children = parent.getChildren();
+          const paragraph = createQuoteNode();
+          children.forEach((child) => paragraph.append(child));
+          parent.replace(paragraph);
+        }
+      });
+    }
+    setShowDropDown(false);
+  };
+
+  const formatCode = () => {
+    if (blockType !== 'code') {
+      editor.update((view) => {
+        const selection = view.getSelection();
+
+        if (selection !== null) {
+          const anchorNode = selection.getAnchorNode();
+          const parent = anchorNode.getParentBlockOrThrow();
+          const children = parent.getChildren();
+          const paragraph = createCodeNode();
+          children.forEach((child) => paragraph.append(child));
+          parent.replace(paragraph);
+        }
+      });
+    }
+    setShowDropDown(false);
+  };
+
+  return (
+    <div className="dropdown" ref={dropDownRef}>
+      <button className="item" onClick={formatParagraph}>
+        <span className="icon paragraph" />
+        <span className="text">Normal</span>
+        {blockType === 'paragraph' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatLargeHeading}>
+        <span className="icon large-heading" />
+        <span className="text">Large Heading</span>
+        {blockType === 'h1' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatSmallHeading}>
+        <span className="icon small-heading" />
+        <span className="text">Small Heading</span>
+        {blockType === 'h2' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatBulletList}>
+        <span className="icon bullet-list" />
+        <span className="text">Bullet List</span>
+        {blockType === 'ul' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatNumberedList}>
+        <span className="icon numbered-list" />
+        <span className="text">Numbered List</span>
+        {blockType === 'ol' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatQuote}>
+        <span className="icon quote" />
+        <span className="text">Quote</span>
+        {blockType === 'quote' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatCode}>
+        <span className="icon code" />
+        <span className="text">Code Block</span>
+        {blockType === 'code' && <span className="active" />}
+      </button>
+    </div>
+  );
 }

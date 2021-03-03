4ec1990a14d88853c6755e5f3982cac6be580623
Steven Luscher <steveluscher@fb.com>
2021-03-03 13:33:23 -0800

    Created scripts to run all tests, just unit tests, or unit and e2e tests.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Outline
 
 Outline is a fast, light-weight, extensible library for building rich text editors on the web.
+
+## Running tests
+
+* `yarn test` runs all tests.
+* `yarn test:e2e` runs only e2e tests.
+* `yarn test:unit` runs only unit tests.
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,20 @@
 'use strict';
 
-module.exports = {
+const common = {
   modulePathIgnorePatterns: ['<rootDir>/packages/outline/npm'],
+};
+
+module.exports = {
+  projects: [
+    {
+      ...common,
+      displayName: 'e2e',
+      testPathIgnorePatterns: ['/__tests__/(?!e2e/)'],
+    },
+    {
+      ...common,
+      displayName: 'unit',
+      testPathIgnorePatterns: ['/__tests__/e2e/'],
+    },
+  ],
 };

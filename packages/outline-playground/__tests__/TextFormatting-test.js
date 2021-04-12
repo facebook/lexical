a6e6b7840bc404c5bfb5b9c461ecc2b16e23e9ee
Andy Wynkoop <andywynkoop@gmail.com>
2021-04-12 10:37:37 -0700

    Clean rebase (#207)
    Co-authored-by: Andy Wynkoop <andywynkoop@fb.com>

--- packages/outline-playground/__tests__/TextFormatting-test.js
+++ packages/outline-playground/__tests__/TextFormatting-test.js
@@ -18,257 +18,259 @@ import {
 
 describe('TextFormatting', () => {
   initializeE2E({chromium: true, webkit: true, firefox: true}, (e2e) => {
-    it(`Can create bold text using the shortcut`, async () => {
-      const {page} = e2e;
+    e2e.flaky(['webkit'], () => {
+      it(`Can create bold text using the shortcut`, async () => {
+        const {page} = e2e;
 
-      await focusEditor(page);
-      await page.keyboard.type('Hello');
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('b');
-      await keyUpCtrlOrMeta(page);
-      await page.keyboard.type(' World');
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 6,
-        focusPath: [0, 1, 0],
-        focusOffset: 6,
+        await focusEditor(page);
+        await page.keyboard.type('Hello');
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('b');
+        await keyUpCtrlOrMeta(page);
+        await page.keyboard.type(' World');
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 6,
+          focusPath: [0, 1, 0],
+          focusOffset: 6,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('b');
+        await keyUpCtrlOrMeta(page);
+        await page.keyboard.type('!');
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 2, 0],
+          anchorOffset: 1,
+          focusPath: [0, 2, 0],
+          focusOffset: 1,
+        });
       });
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('b');
-      await keyUpCtrlOrMeta(page);
-      await page.keyboard.type('!');
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 2, 0],
-        anchorOffset: 1,
-        focusPath: [0, 2, 0],
-        focusOffset: 1,
+      it(`Can create italic text using the shortcut`, async () => {
+        const {page} = e2e;
+
+        await focusEditor(page);
+        await page.keyboard.type('Hello');
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('i');
+        await keyUpCtrlOrMeta(page);
+        await page.keyboard.type(' World');
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 6,
+          focusPath: [0, 1, 0],
+          focusOffset: 6,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('i');
+        await keyUpCtrlOrMeta(page);
+        await page.keyboard.type('!');
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 2, 0],
+          anchorOffset: 1,
+          focusPath: [0, 2, 0],
+          focusOffset: 1,
+        });
       });
-    });
 
-    it(`Can create italic text using the shortcut`, async () => {
-      const {page} = e2e;
+      it(`Can select text and boldify it with the shortcut`, async () => {
+        const {page} = e2e;
 
-      await focusEditor(page);
-      await page.keyboard.type('Hello');
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('i');
-      await keyUpCtrlOrMeta(page);
-      await page.keyboard.type(' World');
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 6,
-        focusPath: [0, 1, 0],
-        focusOffset: 6,
-      });
-
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('i');
-      await keyUpCtrlOrMeta(page);
-      await page.keyboard.type('!');
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 2, 0],
-        anchorOffset: 1,
-        focusPath: [0, 2, 0],
-        focusOffset: 1,
-      });
-    });
-
-    it(`Can select text and boldify it with the shortcut`, async () => {
-      const {page} = e2e;
-
-      await focusEditor(page);
-      await page.keyboard.type('Hello world!');
-      await page.keyboard.press('ArrowLeft');
-      await page.keyboard.down('Shift');
-      await repeat(5, async () => {
+        await focusEditor(page);
+        await page.keyboard.type('Hello world!');
         await page.keyboard.press('ArrowLeft');
-      });
-      await page.keyboard.up('Shift');
-      await assertSelection(page, {
-        anchorPath: [0, 0, 0],
-        anchorOffset: 11,
-        focusPath: [0, 0, 0],
-        focusOffset: 6,
+        await page.keyboard.down('Shift');
+        await repeat(5, async () => {
+          await page.keyboard.press('ArrowLeft');
+        });
+        await page.keyboard.up('Shift');
+        await assertSelection(page, {
+          anchorPath: [0, 0, 0],
+          anchorOffset: 11,
+          focusPath: [0, 0, 0],
+          focusOffset: 6,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('b');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 0,
+          focusPath: [0, 1, 0],
+          focusOffset: 5,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('b');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 0, 0],
+          anchorOffset: 6,
+          focusPath: [0, 0, 0],
+          focusOffset: 11,
+        });
       });
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('b');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 0,
-        focusPath: [0, 1, 0],
-        focusOffset: 5,
-      });
+      it(`Can select text and italicify it with the shortcut`, async () => {
+        const {page} = e2e;
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('b');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 0, 0],
-        anchorOffset: 6,
-        focusPath: [0, 0, 0],
-        focusOffset: 11,
-      });
-    });
-
-    it(`Can select text and italicify it with the shortcut`, async () => {
-      const {page} = e2e;
-
-      await focusEditor(page);
-      await page.keyboard.type('Hello world!');
-      await page.keyboard.press('ArrowLeft');
-      await page.keyboard.down('Shift');
-      await repeat(5, async () => {
+        await focusEditor(page);
+        await page.keyboard.type('Hello world!');
         await page.keyboard.press('ArrowLeft');
-      });
-      await page.keyboard.up('Shift');
-      await assertSelection(page, {
-        anchorPath: [0, 0, 0],
-        anchorOffset: 11,
-        focusPath: [0, 0, 0],
-        focusOffset: 6,
+        await page.keyboard.down('Shift');
+        await repeat(5, async () => {
+          await page.keyboard.press('ArrowLeft');
+        });
+        await page.keyboard.up('Shift');
+        await assertSelection(page, {
+          anchorPath: [0, 0, 0],
+          anchorOffset: 11,
+          focusPath: [0, 0, 0],
+          focusOffset: 6,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('i');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 0,
+          focusPath: [0, 1, 0],
+          focusOffset: 5,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('i');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 0, 0],
+          anchorOffset: 6,
+          focusPath: [0, 0, 0],
+          focusOffset: 11,
+        });
       });
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('i');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 0,
-        focusPath: [0, 1, 0],
-        focusOffset: 5,
-      });
+      it(`Can select multiple text parts and format them with shortcuts`, async () => {
+        const {page} = e2e;
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('i');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 0, 0],
-        anchorOffset: 6,
-        focusPath: [0, 0, 0],
-        focusOffset: 11,
-      });
-    });
-
-    it(`Can select multiple text parts and format them with shortcuts`, async () => {
-      const {page} = e2e;
-
-      await focusEditor(page);
-      await page.keyboard.type('Hello world!');
-      await page.keyboard.press('ArrowLeft');
-      await page.keyboard.down('Shift');
-      await repeat(5, async () => {
+        await focusEditor(page);
+        await page.keyboard.type('Hello world!');
         await page.keyboard.press('ArrowLeft');
-      });
-      await page.keyboard.up('Shift');
-      await assertSelection(page, {
-        anchorPath: [0, 0, 0],
-        anchorOffset: 11,
-        focusPath: [0, 0, 0],
-        focusOffset: 6,
-      });
+        await page.keyboard.down('Shift');
+        await repeat(5, async () => {
+          await page.keyboard.press('ArrowLeft');
+        });
+        await page.keyboard.up('Shift');
+        await assertSelection(page, {
+          anchorPath: [0, 0, 0],
+          anchorOffset: 11,
+          focusPath: [0, 0, 0],
+          focusOffset: 6,
+        });
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('b');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 0,
-        focusPath: [0, 1, 0],
-        focusOffset: 5,
-      });
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('b');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 0,
+          focusPath: [0, 1, 0],
+          focusOffset: 5,
+        });
 
-      await page.keyboard.press('ArrowLeft');
-      await page.keyboard.press('ArrowRight');
-      await page.keyboard.down('Shift');
-      await page.keyboard.press('ArrowRight');
-      await page.keyboard.press('ArrowRight');
-      await page.keyboard.up('Shift');
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 1,
-        focusPath: [0, 1, 0],
-        focusOffset: 3,
-      });
-
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('i');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 2, 0],
-        anchorOffset: 0,
-        focusPath: [0, 2, 0],
-        focusOffset: 2,
-      });
-
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('b');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 2, 0],
-        anchorOffset: 0,
-        focusPath: [0, 2, 0],
-        focusOffset: 2,
-      });
-
-      await page.keyboard.press('ArrowLeft');
-      await page.keyboard.press('ArrowLeft');
-      await page.keyboard.down('Shift');
-      await repeat(5, async () => {
+        await page.keyboard.press('ArrowLeft');
         await page.keyboard.press('ArrowRight');
-      });
-      await page.keyboard.up('Shift');
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 0,
-        focusPath: [0, 3, 0],
-        focusOffset: 2,
-      });
+        await page.keyboard.down('Shift');
+        await page.keyboard.press('ArrowRight');
+        await page.keyboard.press('ArrowRight');
+        await page.keyboard.up('Shift');
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 1,
+          focusPath: [0, 1, 0],
+          focusOffset: 3,
+        });
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('b');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 0, 0],
-        anchorOffset: 6,
-        focusPath: [0, 2, 0],
-        focusOffset: 2,
-      });
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('i');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 2, 0],
+          anchorOffset: 0,
+          focusPath: [0, 2, 0],
+          focusOffset: 2,
+        });
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('i');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 1, 0],
-        anchorOffset: 0,
-        focusPath: [0, 1, 0],
-        focusOffset: 5,
-      });
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('b');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 2, 0],
+          anchorOffset: 0,
+          focusPath: [0, 2, 0],
+          focusOffset: 2,
+        });
 
-      await keyDownCtrlOrMeta(page);
-      await page.keyboard.press('i');
-      await keyUpCtrlOrMeta(page);
-      await assertHTMLSnapshot(page);
-      await assertSelection(page, {
-        anchorPath: [0, 0, 0],
-        anchorOffset: 6,
-        focusPath: [0, 0, 0],
-        focusOffset: 11,
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.down('Shift');
+        await repeat(5, async () => {
+          await page.keyboard.press('ArrowRight');
+        });
+        await page.keyboard.up('Shift');
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 0,
+          focusPath: [0, 3, 0],
+          focusOffset: 2,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('b');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 0, 0],
+          anchorOffset: 6,
+          focusPath: [0, 2, 0],
+          focusOffset: 2,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('i');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 1, 0],
+          anchorOffset: 0,
+          focusPath: [0, 1, 0],
+          focusOffset: 5,
+        });
+
+        await keyDownCtrlOrMeta(page);
+        await page.keyboard.press('i');
+        await keyUpCtrlOrMeta(page);
+        await assertHTMLSnapshot(page);
+        await assertSelection(page, {
+          anchorPath: [0, 0, 0],
+          anchorOffset: 6,
+          focusPath: [0, 0, 0],
+          focusOffset: 11,
+        });
       });
     });
   });

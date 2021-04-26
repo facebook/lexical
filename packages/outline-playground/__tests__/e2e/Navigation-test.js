64bceada04917e9f26d69c4e1d757af1558891c6
Andy Wynkoop <andywynkoop@fb.com>
2021-04-26 16:46:26 -0500

    Separate navigation test cases; skip paragraph shortcut tests on firefox

--- packages/outline-playground/__tests__/e2e/Navigation-test.js
+++ packages/outline-playground/__tests__/e2e/Navigation-test.js
@@ -19,107 +19,142 @@ import {
 
 describe('Keyboard Navigation', () => {
     initializeE2E({chromium: true, webkit: true, firefox: true}, (e2e) => {
+        async function typeParagraphs(page) {
+            await page.focus('div.editor');
+            await page.keyboard.type('Lorem Ipsum is simply dummy text of the printing and typesetting industry.');
+            await page.keyboard.press('Enter');
+            await sleep(500);
+            await page.keyboard.type('It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ');
+            await page.keyboard.press('Enter');
+            await sleep(500);
+            await page.keyboard.type('It was popularised in the 1960s with the release of Letraset sheets containing lorem ipsum passages.');
+        };
+
+        it('can type several paragraphs', async () => {
+            const {page} = e2e;
+            await typeParagraphs(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [2, 0, 0],
+                anchorOffset: 100,
+                focusPath: [2, 0, 0],
+                focusOffset: 100,
+            });
+        });
+
+        it('can move to the beginning of the current line, then back to the end of the current line', async () => {
+            const {page} = e2e;
+            await typeParagraphs(page);
+            await keyDownCtrlOrMeta(page);
+            await page.keyboard.press('ArrowLeft');
+            await keyUpCtrlOrMeta(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [2, 0, 0],
+                anchorOffset: 68,
+                focusPath: [2, 0, 0],
+                focusOffset: 68,
+            });
+            await keyDownCtrlOrMeta(page);
+            await page.keyboard.press('ArrowRight');
+            await keyUpCtrlOrMeta(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [2, 0, 0],
+                anchorOffset: 100,
+                focusPath: [2, 0, 0],
+                focusOffset: 100,
+            });
+        });
+
+        it('can move to the top of the editor', async () => {
+            const {page} = e2e;
+            await typeParagraphs(page);
+            await keyDownCtrlOrMeta(page);
+            await page.keyboard.press("ArrowUp");
+            await keyUpCtrlOrMeta(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [0, 0, 0],
+                anchorOffset: 0,
+                focusPath: [0, 0, 0],
+                focusOffset: 0,
+            });
+        });
+
+        it('can move one word to the right', async () => {
+            const {page} = e2e;
+            await typeParagraphs(page);
+            // go to the top of the editor
+            await keyDownCtrlOrMeta(page);
+            await page.keyboard.press("ArrowUp");
+            await keyUpCtrlOrMeta(page);
+            // move one word to the right
+            await keyDownCtrlOrAlt(page);
+            await page.keyboard.press("ArrowRight");
+            await keyUpCtrlOrAlt(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [0, 0, 0],
+                anchorOffset: 5,
+                focusPath: [0, 0, 0],
+                focusOffset: 5,
+            });
+        });
+
+        it('can move to the beginning of the previous word', async () => {
+            const {page} = e2e;
+            await typeParagraphs(page);
+            // go one word to the left
+            await keyDownCtrlOrAlt(page);
+            await page.keyboard.press("ArrowLeft");
+            await keyUpCtrlOrAlt(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [2, 0, 0],
+                anchorOffset: 91,
+                focusPath: [2, 0, 0],
+                focusOffset: 91,
+            });
+            // go another word to the left
+            await keyDownCtrlOrAlt(page);
+            await page.keyboard.press("ArrowLeft");
+            await keyUpCtrlOrAlt(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [2, 0, 0],
+                anchorOffset: 85,
+                focusPath: [2, 0, 0],
+                focusOffset: 85,
+            });
+        });
+
+        it('can move to the bottom of the editor', async () => {
+            const {page} = e2e;
+            await typeParagraphs(page);
+            // go to the top of the editor
+            await keyDownCtrlOrMeta(page);
+            await page.keyboard.press("ArrowUp");
+            await keyUpCtrlOrMeta(page);
+            // go to the end of the editor
+            await keyDownCtrlOrMeta(page);
+            await page.keyboard.press("ArrowDown");
+            await keyUpCtrlOrMeta(page);
+            await assertHTMLSnapshot(page);
+            await assertSelection(page, {
+                anchorPath: [2, 0, 0],
+                anchorOffset: 100,
+                focusPath: [2, 0, 0],
+                focusOffset: 100,
+            });
+        });
+
         e2e.skip(['firefox'], () => {
-            // TODO: Don't skip firefox once #272 is resolved (option + up/down not working)
-            it('can navigate a larger block of text using keyboard shortcuts', async () => {
+
+
+            it('can move to the beginning of the current paragraph', async () => {
                 const {page} = e2e;
-
-                await page.focus('div.editor');
-                await page.keyboard.type('Lorem Ipsum is simply dummy text of the printing and typesetting industry.');
-                await page.keyboard.press('Enter');
-                await sleep(500);
-                await page.keyboard.type('It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ');
-                await page.keyboard.press('Enter');
-                await sleep(500);
-                await page.keyboard.type('It was popularised in the 1960s with the release of Letraset sheets containing lorem ipsum passages.');
-
-                await assertHTMLSnapshot(page);
-                await assertSelection(page, {
-                    anchorPath: [2, 0, 0],
-                    anchorOffset: 100,
-                    focusPath: [2, 0, 0],
-                    focusOffset: 100,
-                });
-
-                //   Move to beginning of the current line
-                await keyDownCtrlOrMeta(page);
-                await page.keyboard.press('ArrowLeft');
-                await keyUpCtrlOrMeta(page);
-                await assertHTMLSnapshot(page);
-                await assertSelection(page, {
-                    anchorPath: [2, 0, 0],
-                    anchorOffset: 68,
-                    focusPath: [2, 0, 0],
-                    focusOffset: 68,
-                });
-                sleep(2000)
-
-                // Move back to end of current line
-                await keyDownCtrlOrMeta(page);
-                await page.keyboard.press('ArrowRight');
-                await keyUpCtrlOrMeta(page);
-                await assertHTMLSnapshot(page);
-                await assertSelection(page, {
-                    anchorPath: [2, 0, 0],
-                    anchorOffset: 100,
-                    focusPath: [2, 0, 0],
-                    focusOffset: 100,
-                });
-
-                // Move to the top of the edit field
-                await keyDownCtrlOrMeta(page);
-                await page.keyboard.press("ArrowUp");
-                await keyUpCtrlOrMeta(page);
-                await assertHTMLSnapshot(page);
-                await assertSelection(page, {
-                    anchorPath: [0, 0, 0],
-                    anchorOffset: 0,
-                    focusPath: [0, 0, 0],
-                    focusOffset: 0,
-                });
-
-                // Move one word to the right
-
-                await keyDownCtrlOrAlt(page);
-                await page.keyboard.press("ArrowRight");
-                await keyUpCtrlOrAlt(page);
-                await assertHTMLSnapshot(page);
-                await assertSelection(page, {
-                    anchorPath: [0, 0, 0],
-                    anchorOffset: 5,
-                    focusPath: [0, 0, 0],
-                    focusOffset: 5,
-                });
-
-                // Move to end of the current line, then move to the beginning of the last word
-
-                await keyDownCtrlOrMeta(page);
-                await page.keyboard.press("ArrowRight");
-                await keyUpCtrlOrMeta(page);
-                await keyDownCtrlOrAlt(page);
-                await page.keyboard.press("ArrowLeft");
-                await keyUpCtrlOrAlt(page);
-                await assertHTMLSnapshot(page);
-                await assertSelection(page, {
-                    anchorPath: [0, 0, 0],
-                    anchorOffset: 53,
-                    focusPath: [0, 0, 0],
-                    focusOffset: 53,
-                });
-
-                // Move to bottom of the edit field
-                await keyDownCtrlOrMeta(page);
-                await page.keyboard.press("ArrowDown");
-                await keyUpCtrlOrMeta(page);
-                await assertHTMLSnapshot(page);
-                await assertSelection(page, {
-                    anchorPath: [2, 0, 0],
-                    anchorOffset: 100,
-                    focusPath: [2, 0, 0],
-                    focusOffset: 100,
-                });
-
-                // Move to the beginning of the current paragraph
+                await typeParagraphs(page);
                 await keyDownCtrlOrAlt(page);
                 await page.keyboard.press("ArrowUp");
                 await keyUpCtrlOrAlt(page);
@@ -130,8 +165,11 @@ describe('Keyboard Navigation', () => {
                     focusPath: [2, 0, 0],
                     focusOffset: 0,
                 });
+            });
 
-                // Move to the top of the editor, then to the bottom of the current paragraph
+            it ('can move to the top of the editor, then to the bottom of the current paragraph', async () => {
+                const {page} = e2e;
+                await typeParagraphs(page);
                 await keyDownCtrlOrMeta(page);
                 await page.keyboard.press("ArrowUp");
                 await keyUpCtrlOrMeta(page);

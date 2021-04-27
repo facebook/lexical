c7c0507c5fb30fba3efd3654cb4b0434ae431ce4
Andy Wynkoop <andywynkoop@fb.com>
2021-04-27 16:19:26 -0500

    Refactor out keyboard navigation shortcuts from navigation tests

---
+++ packages/outline-playground/__tests__/keyboardShortcuts/index.js
@@ -0,0 +1,97 @@
+import {
+  IS_MAC,
+  keyDownCtrlOrAlt,
+  keyDownCtrlOrMeta,
+  keyUpCtrlOrAlt,
+  keyUpCtrlOrMeta,
+  E2E_BROWSER,
+} from '../utils';
+
+export async function moveToLineBeginning(page) {
+  if (IS_MAC) {
+    await keyDownCtrlOrMeta(page);
+    await page.keyboard.press('ArrowLeft');
+    await keyUpCtrlOrMeta(page);
+  } else {
+    await page.keyboard.press('Home');
+  }
+}
+
+export async function moveToLineEnd(page) {
+  if (IS_MAC) {
+    await keyDownCtrlOrMeta(page);
+    await page.keyboard.press('ArrowRight');
+    await keyUpCtrlOrMeta(page);
+  } else {
+    await page.keyboard.press('End');
+  }
+}
+
+export async function moveToEditorBeginning(page) {
+  if (IS_MAC) {
+    await keyDownCtrlOrMeta(page);
+    await page.keyboard.press('ArrowUp');
+    await keyUpCtrlOrMeta(page);
+  } else {
+    await page.keyboard.press('PageUp');
+    if (E2E_BROWSER === 'firefox') {
+      await page.keyboard.press('Home');
+    }
+  }
+}
+
+export async function moveToEditorEnd(page) {
+  if (IS_MAC) {
+    await keyDownCtrlOrMeta(page);
+    await page.keyboard.press('ArrowDown');
+    await keyUpCtrlOrMeta(page);
+  } else {
+    await page.keyboard.press('PageDown');
+    if (E2E_BROWSER === 'firefox') {
+      await page.keyboard.press('End');
+    }
+  }
+}
+
+export async function moveToNextWord(page) {
+  await keyDownCtrlOrAlt(page);
+  await page.keyboard.press('ArrowRight');
+  await keyUpCtrlOrAlt(page);
+}
+
+export async function moveToPrevWord(page) {
+  await keyDownCtrlOrAlt(page);
+  await page.keyboard.press('ArrowLeft');
+  await keyUpCtrlOrAlt(page);
+}
+
+export async function moveToParagraphBeginning(page) {
+  if (IS_MAC) {
+    await keyDownCtrlOrAlt(page);
+    await page.keyboard.press('ArrowUp');
+    // Firefox has a known bug with this key command, but if we press it again
+    // we can work around the bug.
+    if (E2E_BROWSER === 'firefox') {
+      await page.keyboard.press('ArrowUp');
+    }
+    await keyUpCtrlOrAlt(page);
+  } else {
+    await page.keyboard.press('ArrowUp');
+    await page.keyboard.press('Home');
+  }
+}
+
+export async function moveToParagraphEnd(page) {
+  if (IS_MAC) {
+    await keyDownCtrlOrAlt(page);
+    await page.keyboard.press('ArrowDown');
+    // Firefox has a known bug with this key command, but if we press it again
+    // we can work around the bug.
+    if (E2E_BROWSER === 'firefox') {
+      await page.keyboard.press('ArrowDown');
+    }
+    await keyUpCtrlOrMeta(page);
+  } else {
+    await page.keyboard.press('End');
+  }
+}

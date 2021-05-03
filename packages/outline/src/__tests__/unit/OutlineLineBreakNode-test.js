93ef6a168fe73e26de4d577363772a76861104d1
Denys Mikhalenko <denys@prontiol.com>
2021-05-02 14:11:04 -1000

    Unit test for #253

--- packages/outline/src/__tests__/unit/OutlineLineBreakNode-test.js
+++ packages/outline/src/__tests__/unit/OutlineLineBreakNode-test.js
@@ -65,6 +65,14 @@ describe('OutlineLineBreakNode tests', () => {
     });
   });
 
+  test('clone()', async () => {
+    await update(() => {
+      const lineBreakNode = Outline.createLineBreakNode();
+      const lineBreakNodeClone = lineBreakNode.clone()
+      expect(lineBreakNodeClone).toStrictEqual(lineBreakNode);
+    });
+  });
+
   test('createDOM()', async () => {
     await update(() => {
       const lineBreakNode = Outline.createLineBreakNode();

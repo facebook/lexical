f7764e51099cc9f23e238031948af4740ec386d3
Denys Mikhalenko <denys@prontiol.com>
2021-05-15 20:57:51 -1000

    Remove duplicate prettier option

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,7 +4,6 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   jsxBracketSameLine: true,
-  trailingComma: 'es5',
   printWidth: 80,
   parser: 'babel',
   trailingComma: 'all',

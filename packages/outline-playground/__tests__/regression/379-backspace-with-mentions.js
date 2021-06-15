d6a9528655304bc18739d3ae95261f8dd59546fc
Denys Mikhalenko <denys@prontiol.com>
2021-06-15 05:50:16 -0700

    Regression tests for #379 (#381)
    * Regression tests for #379
    
    * Remove BOM character from test
    
    * Tweak test
    
    * Fix test for Macs that dont use End
    
    Co-authored-by: Dominic Gannaway <dg@domgan.com>
    Co-authored-by: Dominic Gannaway <trueadm@users.noreply.github.com>

---
+++ packages/outline-playground/__tests__/regression/379-backspace-with-mentions.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {moveToEditorBeginning} from '../keyboardShortcuts';
+import {initializeE2E, assertHTML, assertSelection} from '../utils';
+
+describe('Regression test #379', () => {
+  initializeE2E((e2e) => {
+    it(`Is able to correctly handle backspace press at the line boundary`, async () => {
+      const {page} = e2e;
+      await page.focus('div.editor');
+      await page.keyboard.type('Luke');
+      await page.waitForSelector('#mentions-typeahead ul li');
+      await page.keyboard.press('Enter');
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph"><span></span><span class="mention" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span></span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 2, 0],
+        anchorOffset: 0,
+        focusPath: [0, 2, 0],
+        focusOffset: 0,
+      });
+      await moveToEditorBeginning(page);
+      await page.keyboard.press('Enter');
+      await page.keyboard.press('Backspace');
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph"><span></span><span class="mention" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span></span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 0,
+        focusPath: [0, 0, 0],
+        focusOffset: 0,
+      });
+    });
+  });
+});

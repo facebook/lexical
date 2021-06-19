41b44531078b5e9184ec93d2f6dc8b3b797e0201
Dominic Gannaway <dg@domgan.com>
2021-06-19 23:10:44 +0100

    Fix release

--- scripts/prepare-release.js
+++ scripts/prepare-release.js
@@ -7,7 +7,11 @@ const {exec} = require('child-process-promise');
 async function prepareOutlinePackage() {
   await exec(`rm -rf ./packages/outline/npm`);
   await exec(`mkdir ./packages/outline/npm`);
-  await exec(`cp -R ./packages/outline/dist ./packages/outline/npm`);
+  await exec(`mkdir ./packages/outline/npm/dist`);
+  await exec(
+    `cp -R ./packages/outline/dist/Outline.js ./packages/outline/npm/dist/`,
+  );
+  await exec(`cp -R ./packages/outline/dist/*.js ./packages/outline/npm`);
   await exec(`cp -R ./packages/outline/package.json ./packages/outline/npm`);
   await exec(`cp -R LICENSE ./packages/outline/npm`);
   await exec(`cp -R ./packages/outline/README.md ./packages/outline/npm`);
@@ -17,7 +21,7 @@ async function prepareOutlineReactPackage() {
   await exec(`rm -rf ./packages/outline-react/npm`);
   await exec(`mkdir ./packages/outline-react/npm`);
   await exec(
-    `cp -R ./packages/outline-react/dist ./packages/outline-react/npm`,
+    `cp -R ./packages/outline-react/dist/*.js ./packages/outline-react/npm`,
   );
   await exec(
     `cp -R ./packages/outline-react/package.json ./packages/outline-react/npm`,

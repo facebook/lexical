eefac15f4af227cbb83b0ee0a726ed4928e8ccdd
Dominic Gannaway <dg@domgan.com>
2021-06-20 00:06:49 +0100

    Fix release script

--- scripts/prepare-release.js
+++ scripts/prepare-release.js
@@ -3,15 +3,30 @@
 'use strict';
 
 const {exec} = require('child-process-promise');
+const fs = require('fs');
+const path = require('path');
 
 async function prepareOutlinePackage() {
   await exec(`rm -rf ./packages/outline/npm`);
   await exec(`mkdir ./packages/outline/npm`);
-  await exec(`mkdir ./packages/outline/npm/dist`);
-  await exec(
-    `cp -R ./packages/outline/dist/Outline.js ./packages/outline/npm/dist/`,
-  );
   await exec(`cp -R ./packages/outline/dist/*.js ./packages/outline/npm`);
+  const filenames = fs.readdirSync(path.resolve('./packages/outline/npm'));
+  filenames.forEach((filename) => {
+    if (filename === 'Outline.js') {
+      fs.renameSync(
+        path.resolve('./packages/outline/npm', filename),
+        path.resolve('./packages/outline/npm', filename.replace('Outline', 'index')),
+      );
+    } else if (
+      filename.indexOf('Helpers') === -1 &&
+      filename.indexOf('Outline') !== -1
+    ) {
+      fs.renameSync(
+        path.resolve('./packages/outline/npm', filename),
+        path.resolve('./packages/outline/npm', filename.replace('Outline', '')),
+      );
+    }
+  });
   await exec(`cp -R ./packages/outline/package.json ./packages/outline/npm`);
   await exec(`cp -R LICENSE ./packages/outline/npm`);
   await exec(`cp -R ./packages/outline/README.md ./packages/outline/npm`);

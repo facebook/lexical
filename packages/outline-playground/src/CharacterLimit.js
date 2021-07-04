451ea6b2cd5c5234d9a01281a043cb1bdcd63183
Dominic Gannaway <dg@domgan.com>
2021-07-04 13:08:48 +0100

    Revert accidental changes

--- packages/outline-playground/src/CharacterLimit.js
+++ packages/outline-playground/src/CharacterLimit.js
@@ -82,15 +82,13 @@ export default function CharacterLimit({
                   const offset = startOffset - existingOffset;
                   const [targetNode, nextOverflowNode] =
                     existingOverflowNode.splitText(offset);
-                  if (nextOverflowNode != null) {
-                    targetNode.toggleOverflowed();
-                    const parentBlock = targetNode.getParentBlockOrThrow();
-                    parentBlock.normalizeTextNodes(true);
-                    currentIntersectionRef.current = {
-                      nodeKey: nextOverflowNode.getKey(),
-                      offset: 0,
-                    };
-                  }
+                  targetNode.toggleOverflowed();
+                  const parentBlock = targetNode.getParentBlockOrThrow();
+                  parentBlock.normalizeTextNodes(true);
+                  currentIntersectionRef.current = {
+                    nodeKey: nextOverflowNode.getKey(),
+                    offset: 0,
+                  };
                 } else {
                   // Handle next siblings
                   const parentBlock = node.getParentBlockOrThrow();
@@ -135,12 +133,9 @@ export default function CharacterLimit({
             if (startOffset !== 0) {
               [, targetNode] = node.splitText(startOffset);
             }
-            if (targetNode != null && !targetNode.isOverflowed()) {
+            if (!targetNode.isOverflowed()) {
               targetNode.toggleOverflowed();
             }
-          }
-          if (targetNode == null) {
-            return;
           }
           // Handle next siblings
           const siblings = node.getNextSiblings();

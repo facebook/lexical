f7d638dae906d29c44fa4e2b3b77151f70cdec51
Dominic Gannaway <dg@domgan.com>
2021-07-11 18:58:00 +0100

    Add regression test

--- packages/outline-playground/__tests__/e2e/Mentions-test.js
+++ packages/outline-playground/__tests__/e2e/Mentions-test.js
@@ -7,6 +7,7 @@
  */
 
 import {initializeE2E, assertHTML, assertSelection} from '../utils';
+import {deleteNextWord, moveToEditorBeginning} from '../keyboardShortcuts'
 
 describe('Mentions', () => {
   initializeE2E((e2e) => {
@@ -180,6 +181,124 @@ describe('Mentions', () => {
       await assertHTML(
         page,
         '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true"><br></span></p><div contenteditable="false" class="editor-placeholder">Enter some rich text...</div>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 0,
+        focusPath: [0, 0, 0],
+        focusOffset: 0,
+      });
+    });
+
+    it(`Can enter multiple Luke Skywalker mentions and then delete them from start`, async () => {
+      const {page} = e2e;
+
+      await page.focus('div.editor');
+      await page.keyboard.type('Luke');
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 4,
+        focusPath: [0, 0, 0],
+        focusOffset: 4,
+      });
+
+      await page.waitForSelector('#mentions-typeahead ul li');
+      await page.keyboard.press('Enter');
+
+      await page.waitForSelector('.mention');
+
+      await page.keyboard.type(' ');
+
+      await page.keyboard.type('Luke');
+
+      await page.waitForSelector('#mentions-typeahead ul li');
+      await page.keyboard.press('Enter');
+
+      await page.waitForSelector('.mention');
+
+      await page.keyboard.type(' ');
+
+      await page.keyboard.type('Luke');
+
+      await page.waitForSelector('#mentions-typeahead ul li');
+      await page.keyboard.press('Enter');
+
+      await page.waitForSelector('.mention');
+
+      await page.keyboard.type(' ');
+
+      await page.keyboard.type('Luke');
+
+      await page.waitForSelector('#mentions-typeahead ul li');
+      await page.keyboard.press('Enter');
+
+      await page.waitForSelector('.mention');
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph"><span data-outline-text="true">⁠</span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true">⁠</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 8, 0],
+        anchorOffset: 0,
+        focusPath: [0, 8, 0],
+        focusOffset: 0,
+      });
+
+      await moveToEditorBeginning(page);
+
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 0,
+        focusPath: [0, 0, 0],
+        focusOffset: 0,
+      });
+
+      await deleteNextWord(page);
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true"> Skywalker </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true">⁠</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 0,
+        focusPath: [0, 0, 0],
+        focusOffset: 0,
+      });
+
+      await deleteNextWord(page);
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true">⁠</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 0,
+        focusPath: [0, 0, 0],
+        focusOffset: 0,
+      });
+
+      await deleteNextWord(page);
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true"> Skywalker </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true"> </span><span class="mention" data-outline-text="true" style="background-color: rgba(24, 119, 232, 0.2);">Luke Skywalker</span><span data-outline-text="true">⁠</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 0,
+        focusPath: [0, 0, 0],
+        focusOffset: 0,
+      });
+
+      await deleteNextWord(page);
+      await deleteNextWord(page);
+      await deleteNextWord(page);
+      await deleteNextWord(page);
+      await deleteNextWord(page);
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph"><span data-outline-text="true">⁠<br></span></p>',
       );
       await assertSelection(page, {
         anchorPath: [0, 0, 0],

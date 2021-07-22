8db8c94fd93ebebd817aa73f4deb997d405ed230
Gerard Rovira <zurfyx@users.noreply.github.com>
2021-07-22 12:32:34 +0100

    Editor focus should not trigger on page load E2E (#504)

---
+++ packages/outline-playground/__tests__/e2e/Selection-test.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {initializeE2E, sleep} from '../utils';
+
+describe('Selection', () => {
+  initializeE2E((e2e) => {
+    it('does not focus the editor on load', async () => {
+      const editorHasFocus = async () =>
+        await page.evaluate(() => {
+          const editorElement = document.querySelector('div.editor');
+          return document.activeElement === editorElement;
+        });
+      const {page} = e2e;
+      expect(await editorHasFocus()).toEqual(false);
+      await sleep(500);
+      expect(await editorHasFocus()).toEqual(false);
+    });
+  });
+});

f884d9b67968940aea450b65b17b2e381220ed16
Dominic Gannaway <trueadm@users.noreply.github.com>
2021-07-23 12:43:51 +0100

    Improve e2e coverage + playground features (#509)
    * Improve e2e coverage + playground features
    
    * Fix lint
    
    * Remove .only
    
    * Disable image tests in plain text

---
+++ packages/outline-playground/__tests__/e2e/Keywords-test.js
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {initializeE2E, assertHTML, assertSelection, repeat} from '../utils';
+
+describe('Keywords', () => {
+  initializeE2E((e2e) => {
+    it(`Can create a decorator and move selection around it`, async () => {
+      const {page} = e2e;
+
+      await page.focus('div.editor');
+      await page.keyboard.type('congrats');
+
+      await page.waitForSelector('.keyword');
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true">⁠</span><span class="keyword" data-outline-text="true" style="cursor: default;">congrats</span><span data-outline-text="true">⁠</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 2, 0],
+        anchorOffset: 0,
+        focusPath: [0, 2, 0],
+        focusOffset: 0,
+      });
+
+      await page.keyboard.type('c');
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true">congratsc</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 9,
+        focusPath: [0, 0, 0],
+        focusOffset: 9,
+      });
+
+      await page.keyboard.press('ArrowLeft');
+      await page.keyboard.press('Space');
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true">⁠</span><span class="keyword" data-outline-text="true" style="cursor: default;">congrats</span><span data-outline-text="true"> c</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 2, 0],
+        anchorOffset: 1,
+        focusPath: [0, 2, 0],
+        focusOffset: 1,
+      });
+
+      await page.keyboard.press('ArrowRight');
+      await page.keyboard.type('ongrats');
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true">⁠</span><span class="keyword" data-outline-text="true" style="cursor: default;">congrats</span><span data-outline-text="true"> </span><span class="keyword" data-outline-text="true" style="cursor: default;">congrats</span><span data-outline-text="true">⁠</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 4, 0],
+        anchorOffset: 0,
+        focusPath: [0, 4, 0],
+        focusOffset: 0,
+      });
+
+      await repeat(8, async () => {
+        await page.keyboard.press('ArrowLeft');
+      });
+      await assertSelection(page, {
+        anchorPath: [0, 2, 0],
+        anchorOffset: 1,
+        focusPath: [0, 2, 0],
+        focusOffset: 1,
+      });
+
+      await page.keyboard.press('Backspace');
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true">congratscongrats</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 0, 0],
+        anchorOffset: 8,
+        focusPath: [0, 0, 0],
+        focusOffset: 8,
+      });
+
+      await page.keyboard.press('Space');
+
+      await assertHTML(
+        page,
+        '<p class="editor-paragraph" dir="ltr"><span data-outline-text="true">⁠</span><span class="keyword" data-outline-text="true" style="cursor: default;">congrats</span><span data-outline-text="true"> </span><span class="keyword" data-outline-text="true" style="cursor: default;">congrats</span><span data-outline-text="true">⁠</span></p>',
+      );
+      await assertSelection(page, {
+        anchorPath: [0, 2, 0],
+        anchorOffset: 1,
+        focusPath: [0, 2, 0],
+        focusOffset: 1,
+      });
+    });
+  });
+});
--- packages/outline-playground/src/useKeywords.js
+++ packages/outline-playground/src/useKeywords.js
@@ -7,67 +7,166 @@
  * @flow strict-local
  */
 
-import type {OutlineEditor, NodeKey, TextNode} from 'outline';
+import type {
+  OutlineEditor,
+  NodeKey,
+  EditorThemeClasses,
+  OutlineNode,
+  View,
+} from 'outline';
 
-import * as React from 'react';
 import {useEffect} from 'react';
-import {DecoratorNode} from 'outline';
+import {createTextNode, isTextNode, TextNode} from 'outline';
 
-const keywords = new Set(['congrats', 'congratulations']);
+const keywordsRegex =
+  /(^|$|[^A-Za-zªµºÀ-ÖØ-öø-ˁˆ-ˑˠ-ˤˬˮͰ-ʹͶͷͺ-ͽΆΈ-ΊΌΎ-ΡΣ-ϵϷ-ҁҊ-ԧԱ-Ֆՙա-ևא-תװ-ײؠ-يٮٯٱ-ۓەۥۦۮۯۺ-ۼۿܐܒ-ܯݍ-ޥޱߊ-ߪߴߵߺࠀ-ࠕࠚࠤࠨࡀ-ࡘࢠࢢ-ࢬऄ-हऽॐक़-ॡॱ-ॷॹ-ॿঅ-ঌএঐও-নপ-রলশ-হঽৎড়ঢ়য়-ৡৰৱਅ-ਊਏਐਓ-ਨਪ-ਰਲਲ਼ਵਸ਼ਸਹਖ਼-ੜਫ਼ੲ-ੴઅ-ઍએ-ઑઓ-નપ-રલળવ-હઽૐૠૡଅ-ଌଏଐଓ-ନପ-ରଲଳଵ-ହଽଡ଼ଢ଼ୟ-ୡୱஃஅ-ஊஎ-ஐஒ-கஙசஜஞடணதந-பம-ஹௐఅ-ఌఎ-ఐఒ-నప-ళవ-హఽౘౙౠౡಅ-ಌಎ-ಐಒ-ನಪ-ಳವ-ಹಽೞೠೡೱೲഅ-ഌഎ-ഐഒ-ഺഽൎൠൡൺ-ൿඅ-ඖක-නඳ-රලව-ෆก-ะาำเ-ๆກຂຄງຈຊຍດ-ທນ-ຟມ-ຣລວສຫອ-ະາຳຽເ-ໄໆໜ-ໟༀཀ-ཇཉ-ཬྈ-ྌက-ဪဿၐ-ၕၚ-ၝၡၥၦၮ-ၰၵ-ႁႎႠ-ჅჇჍა-ჺჼ-ቈቊ-ቍቐ-ቖቘቚ-ቝበ-ኈኊ-ኍነ-ኰኲ-ኵኸ-ኾዀዂ-ዅወ-ዖዘ-ጐጒ-ጕጘ-ፚᎀ-ᎏᎠ-Ᏼᐁ-ᙬᙯ-ᙿᚁ-ᚚᚠ-ᛪᜀ-ᜌᜎ-ᜑᜠ-ᜱᝀ-ᝑᝠ-ᝬᝮ-ᝰក-ឳៗៜᠠ-ᡷᢀ-ᢨᢪᢰ-ᣵᤀ-ᤜᥐ-ᥭᥰ-ᥴᦀ-ᦫᧁ-ᧇᨀ-ᨖᨠ-ᩔᪧᬅ-ᬳᭅ-ᭋᮃ-ᮠᮮᮯᮺ-ᯥᰀ-ᰣᱍ-ᱏᱚ-ᱽᳩ-ᳬᳮ-ᳱᳵᳶᴀ-ᶿḀ-ἕἘ-Ἕἠ-ὅὈ-Ὅὐ-ὗὙὛὝὟ-ώᾀ-ᾴᾶ-ᾼιῂ-ῄῆ-ῌῐ-ΐῖ-Ίῠ-Ῥῲ-ῴῶ-ῼⁱⁿₐ-ₜℂℇℊ-ℓℕℙ-ℝℤΩℨK-ℭℯ-ℹℼ-ℿⅅ-ⅉⅎↃↄⰀ-Ⱞⰰ-ⱞⱠ-ⳤⳫ-ⳮⳲⳳⴀ-ⴥⴧⴭⴰ-ⵧⵯⶀ-ⶖⶠ-ⶦⶨ-ⶮⶰ-ⶶⶸ-ⶾⷀ-ⷆⷈ-ⷎⷐ-ⷖⷘ-ⷞⸯ々〆〱-〵〻〼ぁ-ゖゝ-ゟァ-ヺー-ヿㄅ-ㄭㄱ-ㆎㆠ-ㆺㇰ-ㇿ㐀-䶵一-鿌ꀀ-ꒌꓐ-ꓽꔀ-ꘌꘐ-ꘟꘪꘫꙀ-ꙮꙿ-ꚗꚠ-ꛥꜗ-ꜟꜢ-ꞈꞋ-ꞎꞐ-ꞓꞠ-Ɦꟸ-ꠁꠃ-ꠅꠇ-ꠊꠌ-ꠢꡀ-ꡳꢂ-ꢳꣲ-ꣷꣻꤊ-ꤥꤰ-ꥆꥠ-ꥼꦄ-ꦲꧏꨀ-ꨨꩀ-ꩂꩄ-ꩋꩠ-ꩶꩺꪀ-ꪯꪱꪵꪶꪹ-ꪽꫀꫂꫛ-ꫝꫠ-ꫪꫲ-ꫴꬁ-ꬆꬉ-ꬎꬑ-ꬖꬠ-ꬦꬨ-ꬮꯀ-ꯢ가-힣ힰ-ퟆퟋ-ퟻ豈-舘並-龎ﬀ-ﬆﬓ-ﬗיִײַ-ﬨשׁ-זּטּ-לּמּנּסּףּפּצּ-ﮱﯓ-ﴽﵐ-ﶏﶒ-ﷇﷰ-ﷻﹰ-ﹴﹶ-ﻼＡ-Ｚａ-ｚｦ-ﾾￂ-ￇￊ-ￏￒ-ￗￚ-ￜ])(congrats|congratulations|gratuluju|gratuluji|gratulujeme|blahopřeju|blahopřeji|blahopřejeme|Til lykke|Tillykke|Glückwunsch|Gratuliere|felicitaciones|enhorabuena|paljon onnea|onnittelut|Félicitations|gratula|gratulálok|gratulálunk|congratulazioni|complimenti|おめでとう|おめでとうございます|축하해|축하해요|gratulerer|Gefeliciteerd|gratulacje|Parabéns|parabéns|felicitações|felicitări|мои поздравления|поздравляем|поздравляю|gratulujem|blahoželám|ยินดีด้วย|ขอแสดงความยินดี|tebrikler|tebrik ederim|恭喜|祝贺你|恭喜你|恭喜|恭喜|baie geluk|veels geluk|অভিনন্দন|Čestitam|Čestitke|Čestitamo|Συγχαρητήρια|Μπράβο|અભિનંદન|badhai|बधाई|अभिनंदन|Честитам|Свака част|hongera|வாழ்த்துகள்|வாழ்த்துக்கள்|అభినందనలు|അഭിനന്ദനങ്ങൾ|Chúc mừng|מזל טוב|mazel tov|mazal tov)(^|$|[^A-Za-zªµºÀ-ÖØ-öø-ˁˆ-ˑˠ-ˤˬˮͰ-ʹͶͷͺ-ͽΆΈ-ΊΌΎ-ΡΣ-ϵϷ-ҁҊ-ԧԱ-Ֆՙա-ևא-תװ-ײؠ-يٮٯٱ-ۓەۥۦۮۯۺ-ۼۿܐܒ-ܯݍ-ޥޱߊ-ߪߴߵߺࠀ-ࠕࠚࠤࠨࡀ-ࡘࢠࢢ-ࢬऄ-हऽॐक़-ॡॱ-ॷॹ-ॿঅ-ঌএঐও-নপ-রলশ-হঽৎড়ঢ়য়-ৡৰৱਅ-ਊਏਐਓ-ਨਪ-ਰਲਲ਼ਵਸ਼ਸਹਖ਼-ੜਫ਼ੲ-ੴઅ-ઍએ-ઑઓ-નપ-રલળવ-હઽૐૠૡଅ-ଌଏଐଓ-ନପ-ରଲଳଵ-ହଽଡ଼ଢ଼ୟ-ୡୱஃஅ-ஊஎ-ஐஒ-கஙசஜஞடணதந-பம-ஹௐఅ-ఌఎ-ఐఒ-నప-ళవ-హఽౘౙౠౡಅ-ಌಎ-ಐಒ-ನಪ-ಳವ-ಹಽೞೠೡೱೲഅ-ഌഎ-ഐഒ-ഺഽൎൠൡൺ-ൿඅ-ඖක-නඳ-රලව-ෆก-ะาำเ-ๆກຂຄງຈຊຍດ-ທນ-ຟມ-ຣລວສຫອ-ະາຳຽເ-ໄໆໜ-ໟༀཀ-ཇཉ-ཬྈ-ྌက-ဪဿၐ-ၕၚ-ၝၡၥၦၮ-ၰၵ-ႁႎႠ-ჅჇჍა-ჺჼ-ቈቊ-ቍቐ-ቖቘቚ-ቝበ-ኈኊ-ኍነ-ኰኲ-ኵኸ-ኾዀዂ-ዅወ-ዖዘ-ጐጒ-ጕጘ-ፚᎀ-ᎏᎠ-Ᏼᐁ-ᙬᙯ-ᙿᚁ-ᚚᚠ-ᛪᜀ-ᜌᜎ-ᜑᜠ-ᜱᝀ-ᝑᝠ-ᝬᝮ-ᝰក-ឳៗៜᠠ-ᡷᢀ-ᢨᢪᢰ-ᣵᤀ-ᤜᥐ-ᥭᥰ-ᥴᦀ-ᦫᧁ-ᧇᨀ-ᨖᨠ-ᩔᪧᬅ-ᬳᭅ-ᭋᮃ-ᮠᮮᮯᮺ-ᯥᰀ-ᰣᱍ-ᱏᱚ-ᱽᳩ-ᳬᳮ-ᳱᳵᳶᴀ-ᶿḀ-ἕἘ-Ἕἠ-ὅὈ-Ὅὐ-ὗὙὛὝὟ-ώᾀ-ᾴᾶ-ᾼιῂ-ῄῆ-ῌῐ-ΐῖ-Ίῠ-Ῥῲ-ῴῶ-ῼⁱⁿₐ-ₜℂℇℊ-ℓℕℙ-ℝℤΩℨK-ℭℯ-ℹℼ-ℿⅅ-ⅉⅎↃↄⰀ-Ⱞⰰ-ⱞⱠ-ⳤⳫ-ⳮⳲⳳⴀ-ⴥⴧⴭⴰ-ⵧⵯⶀ-ⶖⶠ-ⶦⶨ-ⶮⶰ-ⶶⶸ-ⶾⷀ-ⷆⷈ-ⷎⷐ-ⷖⷘ-ⷞⸯ々〆〱-〵〻〼ぁ-ゖゝ-ゟァ-ヺー-ヿㄅ-ㄭㄱ-ㆎㆠ-ㆺㇰ-ㇿ㐀-䶵一-鿌ꀀ-ꒌꓐ-ꓽꔀ-ꘌꘐ-ꘟꘪꘫꙀ-ꙮꙿ-ꚗꚠ-ꛥꜗ-ꜟꜢ-ꞈꞋ-ꞎꞐ-ꞓꞠ-Ɦꟸ-ꠁꠃ-ꠅꠇ-ꠊꠌ-ꠢꡀ-ꡳꢂ-ꢳꣲ-ꣷꣻꤊ-ꤥꤰ-ꥆꥠ-ꥼꦄ-ꦲꧏꨀ-ꨨꩀ-ꩂꩄ-ꩋꩠ-ꩶꩺꪀ-ꪯꪱꪵꪶꪹ-ꪽꫀꫂꫛ-ꫝꫠ-ꫪꫲ-ꫴꬁ-ꬆꬉ-ꬎꬑ-ꬖꬠ-ꬦꬨ-ꬮꯀ-ꯢ가-힣ힰ-ퟆퟋ-ퟻ豈-舘並-龎ﬀ-ﬆﬓ-ﬗיִײַ-ﬨשׁ-זּטּ-לּמּנּסּףּפּצּ-ﮱﯓ-ﴽﵐ-ﶏﶒ-ﷇﷰ-ﷻﹰ-ﹴﹶ-ﻼＡ-Ｚａ-ｚｦ-ﾾￂ-ￇￊ-ￏￒ-ￗￚ-ￜ])/i;
+
+function isCharacterBetweenValid(char: string): boolean {
+  return /[\s\d.,\\/!$%^&*;:{}=\-`~()]/.test(char);
+}
 
 export default function useKeywords(editor: OutlineEditor): void {
   useEffect(() => {
     editor.registerNodeType('keyword', KeywordNode);
-    return editor.addTextNodeTransform((node: TextNode) => {
+    return editor.addTextNodeTransform((node: TextNode, view: View) => {
+      const text = node.getTextContent();
       if (!node.isSimpleText()) {
+        const prevSibling = node.getPreviousSibling();
+        if (isKeywordNode(prevSibling)) {
+          convertKeywordNodeToPlainTextNode(prevSibling);
+        }
+        const nextSibling = node.getNextSibling();
+        if (isKeywordNode(nextSibling)) {
+          convertKeywordNodeToPlainTextNode(nextSibling);
+        }
+        return;
+      } else if (text.length === 0) {
+        const prevSibling = node.getPreviousSibling();
+        const nextSibling = node.getNextSibling();
+        if (
+          isTextNode(prevSibling) &&
+          !prevSibling.isSimpleText() &&
+          isKeywordNode(nextSibling)
+        ) {
+          convertKeywordNodeToPlainTextNode(nextSibling);
+        }
+        if (
+          isTextNode(nextSibling) &&
+          !nextSibling.isSimpleText() &&
+          isKeywordNode(prevSibling)
+        ) {
+          convertKeywordNodeToPlainTextNode(prevSibling);
+        }
         return;
       }
-      const text = node.getTextContent();
-      // TODO this only works if there is only the keyword in the editor.
-      // we should make it work more effectively.
-      if (keywords.has(text)) {
-        const keywordNode = createKeywordNode(text);
-        node.replace(keywordNode);
-        keywordNode.selectNext(0, 0);
+      const selection = view.getSelection();
+
+      if (!isCharacterBetweenValid(text[0])) {
+        // Handle when a text node occurs after a keyword, but doesn't include a space.
+        const prevSibling = node.getPreviousSibling();
+        if (isKeywordNode(prevSibling)) {
+          convertKeywordNodeToPlainTextNode(prevSibling);
+        }
+      }
+
+      let currentNode = node;
+      let targetNode = undefined;
+      let prefixLength = 0;
+      let matchArr;
+
+      while (
+        (matchArr = keywordsRegex.exec(text.substr(prefixLength))) !== null
+      ) {
+        if (matchArr === null) {
+          break;
+        }
+        const prefix = matchArr[1];
+        const startOffset = matchArr.index + prefix.length;
+        const endOffset = startOffset + matchArr[2].length;
+        const lastTargetNode = targetNode;
+        prefixLength += endOffset;
+        // Ensure there is no prefix or that one ends in a valid character
+        if (
+          prefix !== '' &&
+          !isCharacterBetweenValid(prefix[prefix.length - 1])
+        ) {
+          continue;
+        }
+        if (startOffset === 0) {
+          const prevSibling = currentNode.getPreviousSibling();
+          // If the prev sibling is a text node and the start offset is 0, then
+          // that means we shouldn't be trying to make a delight at the start.
+          // Otherwise, we will not have a space.
+          if (isTextNode(prevSibling)) {
+            continue;
+          }
+          [targetNode, currentNode] = currentNode.splitText(endOffset);
+        } else {
+          [, targetNode, currentNode] = currentNode.splitText(
+            startOffset,
+            endOffset,
+          );
+        }
+        if (currentNode) {
+          const nextText = currentNode.getTextContent();
+          if (nextText.length > 0 && !isCharacterBetweenValid(nextText[0])) {
+            targetNode = lastTargetNode;
+            continue;
+          }
+        }
+        const keywordText = targetNode.getTextContent();
+
+        const keywordNode = createKeywordNode(keywordText);
+        targetNode.replace(keywordNode);
+        targetNode = keywordNode;
+      }
+
+      // Restore selection if we no longer have it
+      if (
+        selection !== null &&
+        (!selection.getAnchorNode().isAttached() ||
+          !selection.getFocusNode().isAttached())
+      ) {
+        if (targetNode !== undefined) {
+          targetNode.selectNext(0, 0);
+        } else if (currentNode !== undefined) {
+          currentNode.select(0, 0);
+        }
       }
     });
   }, [editor]);
 }
 
-function Keyword({children}: {children: string}): React.MixedElement {
-  return <span className="keyword">{children}</span>;
-}
-
-class KeywordNode extends DecoratorNode {
+class KeywordNode extends TextNode {
   __keyword: string;
 
   constructor(keyword: string, key?: NodeKey) {
-    super(key);
+    super(keyword, key);
     this.__type = 'keyword';
-    this.__keyword = keyword;
   }
-  static deserialize(data: {__keyword: string}): KeywordNode {
-    return new KeywordNode(data.__keyword);
+  static deserialize(data: {__text: string}): KeywordNode {
+    return new KeywordNode(data.__text);
   }
   clone(): KeywordNode {
-    return new KeywordNode(this.__keyword, this.__key);
+    return new KeywordNode(this.__text, this.__key);
   }
-  getTextContent(): string {
-    return this.__keyword;
-  }
-  createDOM(): HTMLElement {
-    const dom = document.createElement('span');
+  createDOM(editorThemeClasses: EditorThemeClasses): HTMLElement {
+    const dom = super.createDOM(editorThemeClasses);
     dom.style.cursor = 'default';
+    dom.className = 'keyword';
     return dom;
-  }
-  updateDOM(): boolean {
-    return false;
-  }
-  decorate() {
-    return <Keyword>{this.__keyword}</Keyword>;
   }
 }
 
 function createKeywordNode(keyword: string): KeywordNode {
-  return new KeywordNode(keyword);
+  return new KeywordNode(keyword).makeSegmented();
+}
+
+function isKeywordNode(node: null | OutlineNode): boolean %checks {
+  return node instanceof KeywordNode;
+}
+
+function convertKeywordNodeToPlainTextNode(node: KeywordNode): void {
+  const textNode = createTextNode(node.getTextContent());
+  node.replace(textNode);
 }

de5c00e9e0b607654a2ae24e37756e97c9053172
Dominic Gannaway <dg@domgan.com>
2021-09-16 11:23:40 +0100

    Fix flow type

--- packages/outline/src/core/OutlineNode.js
+++ packages/outline/src/core/OutlineNode.js
@@ -743,7 +743,7 @@ export class OutlineNode {
     }
     return writableSelf;
   }
-  selectPrevious(anchorOffset?: number, focusOffset?: number): Selection | null {
+  selectPrevious(anchorOffset?: number, focusOffset?: number): Selection {
     errorOnReadOnly();
     const prevSibling = this.getPreviousSibling();
     const parent = this.getParentBlockOrThrow();
@@ -756,7 +756,7 @@ export class OutlineNode {
     }
     return prevSibling.select(anchorOffset, focusOffset);
   }
-  selectNext(anchorOffset?: number, focusOffset?: number): Selection | null {
+  selectNext(anchorOffset?: number, focusOffset?: number): Selection {
     errorOnReadOnly();
     const nextSibling = this.getNextSibling();
     const parent = this.getParentBlockOrThrow();

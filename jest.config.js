0c393446ffd78b55b43cb14a85b7ecab43a706fd
Gerard Rovira <gerardrovira@fb.com>
2021-09-23 10:10:31 +0100

    dfs

--- jest.config.js
+++ jest.config.js
@@ -45,6 +45,8 @@ module.exports = {
           '<rootDir>/packages/outline/src/helpers/OutlineTextHelpers.js',
         '^outline/KeyHelpers$':
           '<rootDir>/packages/outline/src/helpers/OutlineKeyHelpers.js',
+        '^outline/NodeHelpers$':
+          '<rootDir>/packages/outline/src/helpers/OutlineNodeHelpers.js',
         '^shared/getDOMTextNodeFromElement$':
           '<rootDir>/packages/shared/src/getDOMTextNodeFromElement.js',
         '^shared/isImmutableOrInert$':

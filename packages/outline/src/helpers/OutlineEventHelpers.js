61536f4d8206b445bb914ffa2ed0b3313e534ed5
Dominic Gannaway <dg@domgan.com>
2021-10-14 00:01:22 +0100

    Move comment

--- packages/outline/src/helpers/OutlineEventHelpers.js
+++ packages/outline/src/helpers/OutlineEventHelpers.js
@@ -465,6 +465,11 @@ export function onCompositionEnd(
   }, 'onCompositionEnd');
 }
 
+// This is a work-around is mainly Chrome specific bug where if you select
+// the contents of an empty block, you cannot easily unselect anything.
+// This results in a tiny selection box that looks buggy/broken. This can
+// also help other browsers when selection might "appear" lost, when it
+// really isn't.
 export function onClick(event: MouseEvent, editor: OutlineEditor): void {
   editor.update((view) => {
     const selection = view.getSelection();
@@ -472,11 +477,6 @@ export function onClick(event: MouseEvent, editor: OutlineEditor): void {
       return;
     }
     const anchor = selection.anchor;
-    // This is a work-around is mainly Chrome specific bug where if you select
-    // the contents of an empty block, you cannot easily unselect anything.
-    // This results in a tiny selection box that looks buggy/broken. This can
-    // also help other browsers when selection might "appear" lost, when it
-    // really isn't.
     if (
       selection.isCollapsed() &&
       anchor.type === 'block' &&

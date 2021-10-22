24187fb64fdb5b7419cb216f411eb098065f2334
Dominic Gannaway <dg@domgan.com>
2021-10-22 16:23:18 +0100

    Revise

--- packages/outline-playground/__tests__/utils/index.js
+++ packages/outline-playground/__tests__/utils/index.js
@@ -32,7 +32,7 @@ export function initializeE2E(runTests, config: Config = {}) {
     appSettings.isRichText = process.env.E2E_EDITOR_MODE !== 'plain-text';
   }
   if (appSettings.disableBeforeInput === undefined) {
-    appSettings.disableBeforeInput = process.env.E2E_LEGACY_EVENTS === 'true';
+    appSettings.disableBeforeInput = process.env.E2E_EVENTS_MODE === 'legacy-events';
   }
   const urlParams = appSettingsToURLParams(appSettings);
   const e2e = {

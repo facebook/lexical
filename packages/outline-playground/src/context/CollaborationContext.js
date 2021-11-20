ee01ccfb94031308eb22e2c486c9975d048c37f9
Dominic Gannaway <trueadm@users.noreply.github.com>
2021-11-20 23:09:04 +0000

    Improve collab cursors (#873)

--- packages/outline-playground/src/context/CollaborationContext.js
+++ packages/outline-playground/src/context/CollaborationContext.js
@@ -17,13 +17,33 @@ type CollaborationContextType = {
   color: string,
 };
 
-const colors = ['255,165,0', '0,200,55', '160,0,200', '0,172,200'];
+const entries = [
+  ['Cat', '255,165,0'],
+  ['Dog', '0,200,55'],
+  ['Rabbit', '160,0,200'],
+  ['Frog', '0,172,200'],
+  ['Fox', '197,200,0'],
+  ['Hedgehog', '31,200,0'],
+  ['Pigeon', '200,0,0'],
+  ['Squirrel', '200,0,148'],
+  ['Bear', '255,235,0'],
+  ['Tiger', '86,255,0'],
+  ['Leopard', '0,255,208'],
+  ['Zebra', '0,243,255'],
+  ['Wolf', '0,102,255'],
+  ['Owl', '147,0,255'],
+  ['Gull', '255,0,153'],
+  ['Squid', '0,220,255'],
+];
+
+const randomEntry =
+  entries[Math.floor(Math.random() * (entries.length - 1 - 0 + 1) + 0)];
 
 export const CollaborationContext: React$Context<CollaborationContextType> =
   createContext({
     yjsDocMap: new Map(),
-    name: 'Guest' + Math.floor(Math.random() * 100),
-    color: colors[Math.floor(Math.random() * (colors.length - 1 - 0 + 1) + 0)],
+    name: randomEntry[0],
+    color: randomEntry[1],
   });
 
 export function useCollaborationContext(): CollaborationContextType {

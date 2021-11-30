1d142efb04fe7d3f76bf3c4d04b1d6b594476958
Dominic Gannaway <dg@domgan.com>
2021-11-30 20:10:02 +0000

    Improve treeshaking with Rollup

--- scripts/build.js
+++ scripts/build.js
@@ -247,7 +247,7 @@ ${source}`;
     exports: 'auto',
   };
   if (isWatchMode) {
-    const watcher = rollup.watch({...inputOptions, output: outputOptions});
+    const watcher = rollup.watch({...inputOptions, output: outputOptions, treeshake: 'smallest'});
     watcher.on('event', async (event) => {
       switch (event.code) {
         case 'BUNDLE_START':

b9806dd448273f700c642a8eec69f13fb349ca96
Dominic Gannaway <dg@domgan.com>
2021-12-09 20:52:48 +0000

    Improve history tagging

--- packages/outline-playground/src/plugins/ToolbarPlugin.js
+++ packages/outline-playground/src/plugins/ToolbarPlugin.js
@@ -136,8 +136,6 @@ function FloatingLinkEditor({editor}: {editor: OutlineEditor}): React$Node {
     }
     const rootElement = editor.getRootElement();
 
-    debugger;
-
     if (
       selection !== null &&
       !nativeSelection.isCollapsed &&

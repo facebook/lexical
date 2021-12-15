97154fd023794c87242f156ff0580a1d44bb0d78
Gerard Rovira <zurfyx@users.noreply.github.com>
2021-12-15 21:11:23 +0000

    E2E can tab out of the editor (#1025)

---
+++ packages/outline-playground/__tests__/e2e/Focus-test.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {initializeE2E, focusEditor} from '../utils';
+
+describe('Focus', () => {
+  initializeE2E((e2e) => {
+    it(`can tab out of the editor`, async () => {
+      const {page, isRichText} = e2e;
+      if (isRichText) {
+        return;
+      }
+      await focusEditor(page);
+
+      await page.keyboard.press('Tab');
+      const isEditorFocused = await page.evaluate(() => {
+        const editor = document.querySelector('div.editor');
+        return editor === document.activeElement;
+      });
+
+      expect(isEditorFocused).toBe(false);
+    });
+  });
+});

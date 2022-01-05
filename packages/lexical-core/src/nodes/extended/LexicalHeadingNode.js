ac35579670796c77ab9892f435bef12ee94192cf
“kraisler” <kraisler@fb.com>
2022-01-05 09:50:57 -0800

    Initial checkin markdown infra.

--- packages/lexical-core/src/nodes/extended/LexicalHeadingNode.js
+++ packages/lexical-core/src/nodes/extended/LexicalHeadingNode.js
@@ -14,7 +14,7 @@ import {addClassNamesToElement} from '@lexical/helpers/elements';
 import {ElementNode} from '@lexical/core';
 import {$createParagraphNode} from '@lexical/core/ParagraphNode';
 
-type HeadingTagType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
+export type HeadingTagType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
 
 export class HeadingNode extends ElementNode {
   __tag: HeadingTagType;

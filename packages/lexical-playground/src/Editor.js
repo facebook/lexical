d4fad6b94cba94472f64ee6e426a2ddef38531b9
Dominic Gannaway <dg@domgan.com>
2022-01-06 18:25:50 +0000

    Fix e2e test

--- packages/lexical-playground/src/Editor.js
+++ packages/lexical-playground/src/Editor.js
@@ -147,6 +147,7 @@ export default function Editor({
           <PlainTextPlugin
             contentEditable={contentEditable}
             placeholder={placeholder}
+            initEditorState={true}
           />
         )}
         {(isCharLimit || isCharLimitUtf8) && (

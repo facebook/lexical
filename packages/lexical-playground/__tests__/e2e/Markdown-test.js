3f2073ff5f702f919c65a61cff0af05a540e9512
“kraisler” <kraisler@fb.com>
2022-01-19 13:40:46 -0800

    Resolved feedback

--- packages/lexical-playground/__tests__/e2e/Markdown-test.js
+++ packages/lexical-playground/__tests__/e2e/Markdown-test.js
@@ -10,7 +10,7 @@ import {undo, redo} from '../keyboardShortcuts';
 import {initializeE2E, assertHTML, focusEditor, IS_COLLAB} from '../utils';
 
 describe('Markdown', () => {
-  async function validateTest(
+  async function checkHTMLExpectationsIncludingUndoRedo(
     page: any,
     forwardHTML: String,
     undoHTML: string,
@@ -26,36 +26,62 @@ describe('Markdown', () => {
     await assertHTML(page, forwardHTML);
   }
 
-  // Maintain a pairing between triggers and forwardHTMLExpectations.
+  const triggersAndExpectations = [
+    {
+      expectation:
+        '<h1 class="PlaygroundEditorTheme__h1 qntmu8s7 ftqqwnbv tes86rjd m8h3af8h l7ghb35v kmwttqpk qjfq86k5 srn514ro oxkhqvkx rl78xhln nch0832m"><br></h1>',
 
-  const triggers = [
-    '# ', // H1.
-    '## ', // H2.
-    '### ', // H3.
-    '``` ', // Code block.
-    '> ', // Block quote.
-    '* ', // Unordered.
-    '- ', // Unordered.
-    '321. ', // Ordered.
-  ];
+      trigger: '# ', // H1.
+    },
+    {
+      expectation:
+        '<h2 class="PlaygroundEditorTheme__h2 k1z55t6l cu002yh1 o48pnaf2 l7ghb35v kjdc1dyq kmwttqpk jenc4j3g srn514ro oxkhqvkx rl78xhln nch0832m sxswz4zx"><br></h2>',
 
-  const forwardHTMLExpectations = [
-    '<h1 class="PlaygroundEditorTheme__h1 qntmu8s7 ftqqwnbv tes86rjd m8h3af8h l7ghb35v kmwttqpk qjfq86k5 srn514ro oxkhqvkx rl78xhln nch0832m"><br></h1>',
-    '<h2 class="PlaygroundEditorTheme__h2 k1z55t6l cu002yh1 o48pnaf2 l7ghb35v kjdc1dyq kmwttqpk jenc4j3g srn514ro oxkhqvkx rl78xhln nch0832m sxswz4zx"><br></h2>',
-    '<h3><br></h3>',
-    '<code class="PlaygroundEditorTheme__code igcfgt1w ne4oaoub b6ax4al1 q46jt4gp b0eko5f3 r5g9zsuq fwlpnqze l9mvetk9 f6xnxolp l7ghb35v kmwttqpk th51lws0 mfn553m3 fxyi2ncp" spellcheck="false"><br></code>',
-    '<blockquote class="PlaygroundEditorTheme__quote m8h3af8h l7ghb35v kjdc1dyq kmwttqpk ilmd4f5g k1z55t6l cu002yh1 e9731kn7 jf6b6iuc tcghtb95 q5qw6xdq"><br></blockquote>',
-    '<ul class="PlaygroundEditorTheme__ul srn514ro oxkhqvkx rl78xhln nch0832m m8h3af8h l7ghb35v kjdc1dyq kmwttqpk i2mu9gw5"><li value="1" class="PlaygroundEditorTheme__listItem th51lws0 r26s8xbz mfn553m3 gug11x0k"><br></li></ul>',
-    '<ul class="PlaygroundEditorTheme__ul srn514ro oxkhqvkx rl78xhln nch0832m m8h3af8h l7ghb35v kjdc1dyq kmwttqpk i2mu9gw5"><li value="1" class="PlaygroundEditorTheme__listItem th51lws0 r26s8xbz mfn553m3 gug11x0k"><br></li></ul>',
-    '<ol start="321" class="PlaygroundEditorTheme__ol1 srn514ro oxkhqvkx rl78xhln nch0832m m8h3af8h l7ghb35v kjdc1dyq kmwttqpk i2mu9gw5"><li value="321" class="PlaygroundEditorTheme__listItem th51lws0 r26s8xbz mfn553m3 gug11x0k"><br></li></ol>',
+      trigger: '## ', // H2.
+    },
+    {
+      expectation: '<h3><br></h3>',
+
+      trigger: '### ', // H3.
+    },
+    {
+      expectation:
+        '<code class="PlaygroundEditorTheme__code igcfgt1w ne4oaoub b6ax4al1 q46jt4gp b0eko5f3 r5g9zsuq fwlpnqze l9mvetk9 f6xnxolp l7ghb35v kmwttqpk th51lws0 mfn553m3 fxyi2ncp" spellcheck="false"><br></code>',
+
+      trigger: '``` ', // Code block.
+    },
+    {
+      expectation:
+        '<blockquote class="PlaygroundEditorTheme__quote m8h3af8h l7ghb35v kjdc1dyq kmwttqpk ilmd4f5g k1z55t6l cu002yh1 e9731kn7 jf6b6iuc tcghtb95 q5qw6xdq"><br></blockquote>',
+
+      trigger: '> ', // Block quote.
+    },
+    {
+      expectation:
+        '<ul class="PlaygroundEditorTheme__ul srn514ro oxkhqvkx rl78xhln nch0832m m8h3af8h l7ghb35v kjdc1dyq kmwttqpk i2mu9gw5"><li value="1" class="PlaygroundEditorTheme__listItem th51lws0 r26s8xbz mfn553m3 gug11x0k"><br></li></ul>',
+
+      trigger: '* ', // Unordered.
+    },
+    {
+      expectation:
+        '<ul class="PlaygroundEditorTheme__ul srn514ro oxkhqvkx rl78xhln nch0832m m8h3af8h l7ghb35v kjdc1dyq kmwttqpk i2mu9gw5"><li value="1" class="PlaygroundEditorTheme__listItem th51lws0 r26s8xbz mfn553m3 gug11x0k"><br></li></ul>',
+
+      trigger: '- ', // Unordered.
+    },
+    {
+      expectation:
+        '<ol start="321" class="PlaygroundEditorTheme__ol1 srn514ro oxkhqvkx rl78xhln nch0832m m8h3af8h l7ghb35v kjdc1dyq kmwttqpk i2mu9gw5"><li value="321" class="PlaygroundEditorTheme__listItem th51lws0 r26s8xbz mfn553m3 gug11x0k"><br></li></ol>',
+
+      trigger: '321. ', // Ordered.
+    },
   ];
 
   initializeE2E((e2e) => {
-    const count = triggers.length;
+    const count = triggersAndExpectations.length;
     for (let i = 0; i < count; ++i) {
-      const trigger = triggers[i];
+      const trigger = triggersAndExpectations[i].trigger;
 
-      it(`(${trigger}) Should test markdown with current trigger. Should include undo and redo.`, async () => {
+      it(`Should test markdown with the (${trigger}) trigger. Should include undo and redo.`, async () => {
         const {isRichText, page} = e2e;
 
         if (!isRichText) {
@@ -66,11 +92,15 @@ describe('Markdown', () => {
 
         await page.keyboard.type(trigger);
 
-        const forwardHTML = forwardHTMLExpectations[i];
+        const forwardHTML = triggersAndExpectations[i].expectation;
 
         const undoHTML = `<p class="PlaygroundEditorTheme__paragraph m8h3af8h l7ghb35v kmwttqpk mfn553m3 om3e55n1"><span data-lexical-text="true">${trigger}</span></p>`;
 
-        await validateTest(page, forwardHTML, undoHTML);
+        await checkHTMLExpectationsIncludingUndoRedo(
+          page,
+          forwardHTML,
+          undoHTML,
+        );
       });
     }
   });

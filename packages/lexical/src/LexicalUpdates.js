dccb169499ddc3d330b1071966cf0e6107cf7a1e
Dominic Gannaway <trueadm@users.noreply.github.com>
2022-01-29 00:04:03 +0000

    Fix empty check regression (#1183)

--- packages/lexical/src/LexicalUpdates.js
+++ packages/lexical/src/LexicalUpdates.js
@@ -560,7 +560,7 @@ function beginUpdate(
     processNestedUpdates(editor, deferred);
     applySelectionTransforms(pendingEditorState, editor);
     if (editor._dirtyType !== NO_DIRTY_NODES) {
-      if (pendingEditorState.isEmpty()) {
+      if (!skipEmptyCheck && pendingEditorState.isEmpty()) {
         invariant(
           false,
           'updateEditor: the pending editor state is empty. Ensure the root not never becomes empty from an update.',

b6fd8e62973eedc548260b2e39a07249f164cb11
Russ Zhurba <rulik@fb.com>
2022-02-02 09:17:41 -0500

    Fix comments

--- packages/lexical-playground/src/nodes/ExcalidrawNode/ExcalidrawImage.js
+++ packages/lexical-playground/src/nodes/ExcalidrawNode/ExcalidrawImage.js
@@ -1,9 +1,10 @@
 /**
- * (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
- * @emails oncall+excalidraw
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
  * @flow strict
- * @format
  */
 
 // $FlowFixMe: node modules are ignored by flow

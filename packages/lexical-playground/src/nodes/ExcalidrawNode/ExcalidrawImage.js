0e1b78d2f9ae22b07ba6d20c038fd4fe1d359cbd
rulikrulit <rulik@fb.com>
2022-02-03 12:59:01 -0500

    Add Excalidraw plugin (#1212)
    * Add sample Excalidraw node
    
    * Configure excalidraw behaviors
    
    * Remove excalidraw from flowconfig
    
    * Fix comments
    
    * Add icon, move dependencies
    
    * Packages update
    
    * Fix icon, remove console.log
    
    * Remove changes for package-lock.json

---
+++ packages/lexical-playground/src/nodes/ExcalidrawNode/ExcalidrawImage.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+// $FlowFixMe: node modules are ignored by flow
+import {exportToSvg} from '@excalidraw/excalidraw';
+import * as React from 'react';
+import {useEffect, useState} from 'react';
+
+type ImageType = 'svg' | 'canvas';
+
+type Props = {
+  /**
+   * The Excalidraw elements to be rendered as an image
+   */
+  elements: $ReadOnlyArray<{...}>,
+  /**
+   * The type of image to be rendered
+   */
+  imageType?: ImageType,
+  /**
+   * The css class applied to image to be rendered
+   */
+  className?: string,
+  /**
+   * The height of the image to be rendered
+   */
+  height?: number | null,
+  /**
+   * The width of the image to be rendered
+   */
+  width?: number | null,
+  /**
+   * Configures the export setting for SVG/Canvas
+   */
+  appState?: mixed,
+  /**
+   * The css class applied to the root element of this component
+   */
+  rootClassName?: string | null,
+};
+
+// exportToSvg has fonts from excalidraw.com
+// We don't want them to be used in open source
+const removeStyleFromSvg_HACK = (svg) => {
+  const styleTag = svg?.firstElementChild?.firstElementChild;
+  if (styleTag && styleTag.tagName === 'style') {
+    styleTag.remove();
+  }
+};
+
+/**
+ * @explorer-desc
+ * A component for rendering Excalidraw elements as a static image
+ */
+export default function ExcalidrawImage({
+  elements,
+  className = '',
+  height = null,
+  width = null,
+  appState = null,
+  rootClassName = null,
+}: Props): React.MixedElement {
+  const [Svg, setSvg] = useState<Element | null>(null);
+
+  useEffect(() => {
+    const setContent = async () => {
+      const svg: Element = await exportToSvg({
+        elements,
+        appState,
+      });
+      removeStyleFromSvg_HACK(svg);
+      setSvg(svg);
+    };
+    setContent();
+  }, [elements, appState]);
+
+  return (
+    <div
+      className={rootClassName}
+      dangerouslySetInnerHTML={{__html: Svg?.outerHTML}}
+    />
+  );
+}

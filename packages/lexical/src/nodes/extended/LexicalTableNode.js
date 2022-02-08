5040a90599eb74b114c1be89ff8de535de370328
Tyler Bainbridge <tbainbridge@fb.com>
2022-02-08 12:12:40 -0500

    Tables: Prevent Table Cell Deletion From Backspace offset 0 (#1232)
    * Clear Selection + Add empty paragraph node when cell is cleared
    
    * Remove package lock changes
    
    * Prevent Table Cell Deletion From Backspace
    
    * Only when in a Table Cell
    
    * Remove redundant check

--- packages/lexical/src/nodes/extended/LexicalTableNode.js
+++ packages/lexical/src/nodes/extended/LexicalTableNode.js
@@ -23,10 +23,12 @@ import {
   $getNearestNodeFromDOMNode,
   $isElementNode,
   $createSelection,
+  $createParagraphNode,
   $getSelection,
   $setSelection,
 } from 'lexical';
-import {$createParagraphNode} from 'lexical/ParagraphNode';
+import {$findMatchingParent} from '@lexical/helpers/nodes';
+import {$isTableCellNode} from 'lexical/TableCellNode';
 
 type Cell = {
   elem: HTMLElement,
@@ -194,17 +196,20 @@ function updateCells(
   return highlighted;
 }
 
-function applyCellSelection(
+function applyCustomTableHandlers(
   tableNode: TableNode,
   tableElement: HTMLElement,
   editor: LexicalEditor,
 ): void {
-  const grid = tableNode.getGrid();
-
   const rootElement = editor.getRootElement();
   if (rootElement === null) {
     return;
   }
+
+  trackTableGrid(tableNode, tableElement, editor);
+
+  const grid = tableNode.getGrid();
+
   let isSelected = false;
   let isHighlightingCells = false;
   let startX = -1;
@@ -405,6 +410,40 @@ function applyCellSelection(
       });
     }
   });
+
+  editor.addListener(
+    'command',
+    (type, payload) => {
+      const selection = $getSelection();
+
+      if (selection == null) {
+        return false;
+      }
+
+      const tableCellNode = $findMatchingParent(
+        selection.anchor.getNode(),
+        (n) => $isTableCellNode(n),
+      );
+
+      if (!$isTableCellNode(tableCellNode)) {
+        return false;
+      }
+
+      if (type === 'deleteCharacter') {
+        if (
+          highlightedCells.length === 0 &&
+          selection != null &&
+          selection.isCollapsed() &&
+          selection.anchor.offset === 0
+        ) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+    LowPriority,
+  );
 }
 
 export class TableNode extends ElementNode {
@@ -442,8 +481,7 @@ export class TableNode extends ElementNode {
 
     addClassNamesToElement(element, config.theme.table);
 
-    trackTableGrid(this, element, editor);
-    applyCellSelection(this, element, editor);
+    applyCustomTableHandlers(this, element, editor);
 
     return element;
   }

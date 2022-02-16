2e151be3903a9a715ec19ea46a5b7fca4503779a
Dominic Gannaway <trueadm@users.noreply.github.com>
2022-02-16 10:41:45 +0000

    Revert "After triggering markdown to bold text, the caret should appear after the space character. (#1293)" (#1303)
    This reverts commit e217ca81131813034ab71bca8f9d26108c89742b.

--- packages/lexical/src/LexicalUtils.js
+++ packages/lexical/src/LexicalUtils.js
@@ -339,7 +339,7 @@ export function markAllNodesAsDirty(editor: LexicalEditor, type: string): void {
     true,
     editor._pendingEditorState === null
       ? {
-          tag: 'history-merge',
+          tag: 'without-history',
         }
       : undefined,
   );

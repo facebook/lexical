042b5457a5b03265c56e6750b1acbeddb97d3850
Tyler Bainbridge <tyler@tylerbainbridge.com>
2022-02-17 18:23:58 -0500

    Move advanced logic from node to plugin

--- packages/lexical-table/src/LexicalTableNode.js
+++ packages/lexical-table/src/LexicalTableNode.js
@@ -8,556 +8,15 @@
  */
 
 import type {TableCellNode} from './LexicalTableCellNode';
-import type {
-  CommandListenerCriticalPriority,
-  CommandListenerLowPriority,
-  EditorConfig,
-  LexicalEditor,
-  LexicalNode,
-  NodeKey,
-  TextFormatType,
-} from 'lexical';
+import type {Cell, Grid, SelectionShape} from './lexicalTableCoreHelpers';
+import type {EditorConfig, LexicalEditor, LexicalNode, NodeKey} from 'lexical';
 
 import {addClassNamesToElement} from '@lexical/helpers/elements';
-import {$findMatchingParent} from '@lexical/helpers/nodes';
-import {
-  $createParagraphNode,
-  $createRangeSelection,
-  $createTextNode,
-  $getNearestNodeFromDOMNode,
-  $getSelection,
-  $isElementNode,
-  $isRangeSelection,
-  $setSelection,
-  GridNode,
-} from 'lexical';
+import {$getNearestNodeFromDOMNode, GridNode} from 'lexical';
 import invariant from 'shared/invariant';
 
 import {$isTableCellNode} from './LexicalTableCellNode';
-
-type Cell = {
-  elem: HTMLElement,
-  highlighted: boolean,
-  x: number,
-  y: number,
-};
-
-type Cells = Array<Array<Cell>>;
-
-type Grid = {
-  cells: Cells,
-  columns: number,
-  rows: number,
-};
-
-type SelectionShape = {
-  fromX: number,
-  fromY: number,
-  toX: number,
-  toY: number,
-};
-
-const LowPriority: CommandListenerLowPriority = 1;
-const CriticalPriority: CommandListenerCriticalPriority = 4;
-
-const removeHighlightStyle = document.createElement('style');
-removeHighlightStyle.appendChild(
-  document.createTextNode('::selection{background-color: transparent}'),
-);
-
-function getCellFromTarget(node: Node): Cell | null {
-  let currentNode = node;
-  while (currentNode != null) {
-    const nodeName = currentNode.nodeName;
-    if (nodeName === 'TD' || nodeName === 'TH') {
-      // $FlowFixMe: internal field
-      const cell = currentNode._cell;
-      if (cell === undefined) {
-        return null;
-      }
-      return cell;
-    }
-    currentNode = currentNode.parentNode;
-  }
-  return null;
-}
-
-export function trackTableGrid(
-  tableNode: TableNode,
-  tableElement: HTMLElement,
-  editor: LexicalEditor,
-): Grid {
-  const cells: Cells = [];
-  const grid = {
-    cells,
-    columns: 0,
-    rows: 0,
-  };
-  const observer = new MutationObserver((records) => {
-    editor.update(() => {
-      let currentNode = tableElement.firstChild;
-      let x = 0;
-      let y = 0;
-
-      let gridNeedsRedraw = false;
-      for (let i = 0; i < records.length; i++) {
-        const record = records[i];
-        const target = record.target;
-        const nodeName = target.nodeName;
-        if (nodeName === 'TABLE' || nodeName === 'TR') {
-          gridNeedsRedraw = true;
-          break;
-        }
-      }
-      if (!gridNeedsRedraw) {
-        return;
-      }
-      cells.length = 0;
-
-      while (currentNode != null) {
-        const nodeMame = currentNode.nodeName;
-        if (nodeMame === 'TD' || nodeMame === 'TH') {
-          // $FlowFixMe: TD is always an HTMLElement
-          const elem: HTMLElement = currentNode;
-          const cell = {
-            elem,
-            highlighted: false,
-            x,
-            y,
-          };
-          // $FlowFixMe: internal field
-          currentNode._cell = cell;
-          if (cells[y] === undefined) {
-            cells[y] = [];
-          }
-          cells[y][x] = cell;
-        } else {
-          const child = currentNode.firstChild;
-          if (child != null) {
-            currentNode = child;
-            continue;
-          }
-        }
-        const sibling = currentNode.nextSibling;
-        if (sibling != null) {
-          x++;
-          currentNode = sibling;
-          continue;
-        }
-        const parent = currentNode.parentNode;
-        if (parent != null) {
-          const parentSibling = parent.nextSibling;
-          if (parentSibling == null) {
-            break;
-          }
-          y++;
-          x = 0;
-          currentNode = parentSibling;
-        }
-      }
-      grid.columns = x + 1;
-      grid.rows = y + 1;
-
-      tableNode.setGrid(grid);
-    });
-  });
-
-  observer.observe(tableElement, {
-    childList: true,
-    subtree: true,
-  });
-
-  tableNode.setGrid(grid);
-
-  return grid;
-}
-
-function updateCells(
-  fromX: number,
-  toX: number,
-  fromY: number,
-  toY: number,
-  cells: Cells,
-): Array<Cell> {
-  const highlighted = [];
-  for (let y = 0; y < cells.length; y++) {
-    const row = cells[y];
-    for (let x = 0; x < row.length; x++) {
-      const cell = row[x];
-      const elemStyle = cell.elem.style;
-      if (x >= fromX && x <= toX && y >= fromY && y <= toY) {
-        if (!cell.highlighted) {
-          cell.highlighted = true;
-          elemStyle.setProperty('background-color', 'rgb(163, 187, 255)');
-          elemStyle.setProperty('caret-color', 'transparent');
-        }
-        highlighted.push(cell);
-      } else if (cell.highlighted) {
-        cell.highlighted = false;
-        elemStyle.removeProperty('background-color');
-        elemStyle.removeProperty('caret-color');
-
-        if (!cell.elem.getAttribute('style')) {
-          cell.elem.removeAttribute('style');
-        }
-      }
-    }
-  }
-  return highlighted;
-}
-
-function applyCustomTableHandlers(
-  tableNode: TableNode,
-  tableElement: HTMLElement,
-  editor: LexicalEditor,
-): void {
-  const rootElement = editor.getRootElement();
-  if (rootElement === null) {
-    return;
-  }
-
-  trackTableGrid(tableNode, tableElement, editor);
-
-  const grid = tableNode.getGrid();
-
-  let isSelected = false;
-  let isHighlightingCells = false;
-  let startX = -1;
-  let startY = -1;
-  let currentX = -1;
-  let currentY = -1;
-  let highlightedCells = [];
-
-  if (grid == null) {
-    throw new Error('Table grid not found.');
-  }
-
-  tableElement.addEventListener('mousemove', (event: MouseEvent) => {
-    if (isSelected) {
-      // $FlowFixMe: event.target is always a Node on the DOM
-      const cell = getCellFromTarget(event.target);
-      if (cell !== null) {
-        const cellX = cell.x;
-        const cellY = cell.y;
-        if (!isHighlightingCells && (startX !== cellX || startY !== cellY)) {
-          event.preventDefault();
-          const windowSelection = window.getSelection();
-          // Collapse the selection
-          windowSelection.setBaseAndExtent(
-            windowSelection.anchorNode,
-            0,
-            windowSelection.anchorNode,
-            0,
-          );
-          isHighlightingCells = true;
-          if (document.body) {
-            document.body.appendChild(removeHighlightStyle);
-          }
-          if (deleteCharacterListener === null) {
-            deleteCharacterListener = editor.addListener(
-              'command',
-              (type, payload) => {
-                if (type === 'deleteCharacter') {
-                  if (highlightedCells.length === grid.columns * grid.rows) {
-                    tableNode.selectPrevious();
-                    // Delete entire table
-                    tableNode.remove();
-                    clearHighlight();
-                    return true;
-                  }
-                  highlightedCells.forEach(({elem}) => {
-                    const cellNode = $getNearestNodeFromDOMNode(elem);
-
-                    if ($isElementNode(cellNode)) {
-                      const paragraphNode = $createParagraphNode();
-                      const textNode = $createTextNode();
-                      paragraphNode.append(textNode);
-                      cellNode.append(paragraphNode);
-
-                      cellNode.getChildren().forEach((child) => {
-                        if (child !== paragraphNode) {
-                          child.remove();
-                        }
-                      });
-                    }
-                  });
-                  tableNode.setSelectionState(null);
-                  $setSelection(null);
-                  return true;
-                } else if (type === 'formatText') {
-                  formatCells(payload);
-                  return true;
-                } else if (type === 'insertText') {
-                  clearHighlight();
-                  return false;
-                }
-                return false;
-              },
-              LowPriority,
-            );
-          }
-        } else if (cellX === currentX && cellY === currentY) {
-          return;
-        }
-        currentX = cellX;
-        currentY = cellY;
-
-        if (isHighlightingCells) {
-          const fromX = Math.min(startX, currentX);
-          const toX = Math.max(startX, currentX);
-          const fromY = Math.min(startY, currentY);
-          const toY = Math.max(startY, currentY);
-
-          editor.update(() => {
-            highlightedCells = tableNode.setSelectionState({
-              fromX,
-              fromY,
-              toX,
-              toY,
-            });
-          });
-        }
-      }
-    }
-  });
-
-  const clearHighlight = () => {
-    editor.update(() => {
-      isHighlightingCells = false;
-      isSelected = false;
-      startX = -1;
-      startY = -1;
-      currentX = -1;
-      currentY = -1;
-
-      editor.update(() => {
-        tableNode.setSelectionState(null);
-      });
-
-      highlightedCells = [];
-      if (deleteCharacterListener !== null) {
-        deleteCharacterListener();
-        deleteCharacterListener = null;
-      }
-      const parent = removeHighlightStyle.parentNode;
-      if (parent != null) {
-        parent.removeChild(removeHighlightStyle);
-      }
-    });
-  };
-
-  tableElement.addEventListener('mouseleave', (event: MouseEvent) => {
-    if (isSelected) {
-      return;
-    }
-  });
-
-  const formatCells = (type: TextFormatType) => {
-    let selection = $getSelection();
-    if (!$isRangeSelection(selection)) {
-      selection = $createRangeSelection();
-    }
-    // This is to make Flow play ball.
-    const formatSelection = selection;
-    const anchor = formatSelection.anchor;
-    const focus = formatSelection.focus;
-    highlightedCells.forEach((highlightedCell) => {
-      const cellNode = $getNearestNodeFromDOMNode(highlightedCell.elem);
-      if ($isElementNode(cellNode) && cellNode.getTextContentSize() !== 0) {
-        anchor.set(cellNode.getKey(), 0, 'element');
-        focus.set(cellNode.getKey(), cellNode.getChildrenSize(), 'element');
-        formatSelection.formatText(type);
-      }
-    });
-    // Collapse selection
-    selection.anchor.set(
-      selection.anchor.key,
-      selection.anchor.offset,
-      selection.anchor.type,
-    );
-    selection.focus.set(
-      selection.anchor.key,
-      selection.anchor.offset,
-      selection.anchor.type,
-    );
-    $setSelection(selection);
-  };
-
-  let deleteCharacterListener = null;
-
-  tableElement.addEventListener('mousedown', (event: MouseEvent) => {
-    if (isSelected) {
-      if (isHighlightingCells) {
-        clearHighlight();
-      }
-      return;
-    }
-    setTimeout(() => {
-      if (isHighlightingCells) {
-        clearHighlight();
-      }
-      // $FlowFixMe: event.target is always a Node on the DOM
-      const cell = getCellFromTarget(event.target);
-      if (cell !== null) {
-        isSelected = true;
-        startX = cell.x;
-        startY = cell.y;
-
-        document.addEventListener(
-          'mouseup',
-          () => {
-            isSelected = false;
-          },
-          {
-            capture: true,
-            once: true,
-          },
-        );
-      }
-    }, 0);
-  });
-
-  window.addEventListener('click', (e) => {
-    if (
-      highlightedCells.length > 0 &&
-      !tableElement.contains(e.target) &&
-      rootElement.contains(e.target)
-    ) {
-      editor.update(() => {
-        tableNode.setSelectionState(null);
-      });
-    }
-  });
-
-  const selectGridNodeInDirection = (
-    x: number,
-    y: number,
-    direction: 'backward' | 'forward' | 'up' | 'down',
-  ): boolean => {
-    switch (direction) {
-      case 'backward':
-      case 'forward': {
-        const isForward = direction === 'forward';
-
-        if (y !== (isForward ? grid.columns - 1 : 0)) {
-          tableNode.getCellNodeFromCords(x, y + (isForward ? 1 : -1)).select();
-        } else {
-          if (x !== (isForward ? grid.rows - 1 : 0)) {
-            tableNode
-              .getCellNodeFromCords(
-                x + (isForward ? 1 : -1),
-                isForward ? 0 : grid.columns - 1,
-              )
-              .select();
-          } else if (!isForward) {
-            tableNode.selectPrevious();
-          } else {
-            tableNode.selectNext();
-          }
-        }
-        return true;
-      }
-
-      case 'up': {
-        if (x !== 0) {
-          tableNode.getCellNodeFromCords(x - 1, y).select();
-        } else {
-          tableNode.selectPrevious();
-        }
-        return true;
-      }
-
-      case 'down': {
-        if (x !== grid.rows - 1) {
-          tableNode.getCellNodeFromCords(x + 1, y).select();
-        } else {
-          tableNode.selectNext();
-        }
-        return true;
-      }
-    }
-
-    return false;
-  };
-
-  editor.addListener(
-    'command',
-    (type, payload) => {
-      const selection = $getSelection();
-
-      if (!$isRangeSelection(selection)) {
-        return false;
-      }
-
-      const tableCellNode = $findMatchingParent(
-        selection.anchor.getNode(),
-        (n) => $isTableCellNode(n),
-      );
-
-      if (!$isTableCellNode(tableCellNode)) {
-        return false;
-      }
-
-      if (type === 'deleteCharacter') {
-        if (
-          highlightedCells.length === 0 &&
-          selection.isCollapsed() &&
-          selection.anchor.offset === 0 &&
-          selection.anchor.getNode().getPreviousSiblings().length === 0
-        ) {
-          return true;
-        }
-      }
-
-      if (type === 'indentContent' || type === 'outdentContent') {
-        if (selection.isCollapsed() && highlightedCells.length === 0) {
-          const currentCords = tableNode.getCordsFromCellNode(tableCellNode);
-
-          selectGridNodeInDirection(
-            currentCords.x,
-            currentCords.y,
-            type === 'indentContent' ? 'forward' : 'backward',
-          );
-
-          return true;
-        }
-      }
-
-      if (type === 'keyArrowDown' || type === 'keyArrowUp') {
-        const event: KeyboardEvent = payload;
-
-        if (selection.isCollapsed() && highlightedCells.length === 0) {
-          const currentCords = tableNode.getCordsFromCellNode(tableCellNode);
-          const elementParentNode = $findMatchingParent(
-            selection.anchor.getNode(),
-            (n) => $isElementNode(n),
-          );
-
-          if (
-            (type === 'keyArrowUp' &&
-              elementParentNode === tableCellNode.getFirstChild()) ||
-            (type === 'keyArrowDown' &&
-              elementParentNode === tableCellNode.getLastChild())
-          ) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
-
-            selectGridNodeInDirection(
-              currentCords.x,
-              currentCords.y,
-              type === 'keyArrowUp' ? 'up' : 'down',
-            );
-
-            return true;
-          }
-        }
-      }
-
-      return false;
-    },
-    CriticalPriority,
-  );
-}
+import {updateCells} from './lexicalTableCoreHelpers';
 
 export class TableNode extends GridNode {
   __selectionShape: ?SelectionShape;
@@ -596,8 +55,6 @@ export class TableNode extends GridNode {
 
     addClassNamesToElement(element, config.theme.table);
 
-    applyCustomTableHandlers(this, element, editor);
-
     return element;
   }
 
@@ -618,10 +75,12 @@ export class TableNode extends GridNode {
 
     self.__selectionShape = selectionShape;
 
-    if (this.__grid == null) return [];
+    const grid = this.getGrid();
+
+    if (grid == null) return [];
 
     if (!selectionShape) {
-      return updateCells(-1, -1, -1, -1, this.__grid.cells);
+      return updateCells(-1, -1, -1, -1, grid.cells);
     }
 
     return updateCells(
@@ -629,31 +88,33 @@ export class TableNode extends GridNode {
       selectionShape.toX,
       selectionShape.fromY,
       selectionShape.toY,
-      this.__grid.cells,
+      grid.cells,
     );
   }
 
   getSelectionState(): ?SelectionShape {
-    return this.__selectionShape;
+    return this.getLatest().__selectionShape;
   }
 
   getCordsFromCellNode(tableCellNode: TableCellNode): {x: number, y: number} {
-    invariant(this.__grid, 'Grid not found.');
+    const grid = this.getGrid();
 
-    const {rows, cells} = this.__grid;
+    invariant(grid, 'Grid not found.');
 
-    for (let x = 0; x < rows; x++) {
-      const row = cells[x];
+    const {rows, cells} = grid;
+
+    for (let y = 0; y < rows; y++) {
+      const row = cells[y];
       if (row == null) {
-        throw new Error(`Row not found at x:${x}`);
+        throw new Error(`Row not found at y:${y}`);
       }
 
-      const y = row.findIndex(({elem}) => {
+      const x = row.findIndex(({elem}) => {
         const cellNode = $getNearestNodeFromDOMNode(elem);
         return cellNode === tableCellNode;
       });
 
-      if (y !== -1) {
+      if (x !== -1) {
         return {x, y};
       }
     }
@@ -662,20 +123,22 @@ export class TableNode extends GridNode {
   }
 
   getCellNodeFromCords(x: number, y: number): TableCellNode {
-    invariant(this.__grid, 'Grid not found.');
+    const grid = this.getGrid();
 
-    const {cells} = this.__grid;
+    invariant(grid, 'Grid not found.');
 
-    const row = cells[x];
+    const {cells} = grid;
+
+    const row = cells[y];
 
     if (row == null) {
-      throw new Error(`Table row x:"${x}" not found.`);
+      throw new Error(`Table row x:"${y}" not found.`);
     }
 
-    const cell = row[y];
+    const cell = row[x];
 
     if (cell == null) {
-      throw new Error(`Table cell y:"${y}" in row x:"${x}" not found.`);
+      throw new Error(`Table cell y:"${x}" in row x:"${y}" not found.`);
     }
 
     const node = $getNearestNodeFromDOMNode(cell.elem);
@@ -687,13 +150,14 @@ export class TableNode extends GridNode {
     throw new Error('Node at cords not TableCellNode.');
   }
 
-  setGrid(grid: ?Grid): void {
+  setGrid(grid: ?Grid): TableNode {
     const self = this.getWritable();
     self.__grid = grid;
+    return self;
   }
 
   getGrid(): ?Grid {
-    return this.__grid;
+    return this.getLatest().__grid;
   }
 
   canSelectBefore(): true {

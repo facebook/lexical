91fa6ce3c80b059cc91da9c27bf2cafd9b4c2b05
acywatson <acy.watson@gmail.com>
2022-02-25 15:15:51 -0800

    various licensing fixes

--- scripts/npm/packages.js
+++ scripts/npm/packages.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
 'use strict';
 
 const LEXICAL_PKG = 'lexical';

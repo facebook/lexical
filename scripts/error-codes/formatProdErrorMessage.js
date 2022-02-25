ff88a743880992cc5d0075831fe3b7767f9a0fe4
Dominic Gannaway <dg@domgan.com>
2022-02-25 00:28:05 +0000

    Revert bad merge

---
+++ scripts/error-codes/formatProdErrorMessage.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+'use strict';
+
+// Do not require this module directly! Use normal `invariant` calls with
+// template literal strings. The messages will be replaced with error codes
+// during build.
+
+function formatProdErrorMessage(code) {
+  throw Error(
+    `Minified Lexical error #${code}; see codes.json for the full message or ` +
+      'use the non-minified dev environment for full errors and additional ' +
+      'helpful warnings.',
+  );
+}
+
+module.exports = formatProdErrorMessage;

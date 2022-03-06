9783a62317159ad9237256bde1eb15791259cdbd
acywatson <acy.watson@gmail.com>
2022-03-06 14:05:22 -0800

    skip collab test

--- packages/lexical-playground/__tests__/regression/1384-insert-nodes.js
+++ packages/lexical-playground/__tests__/regression/1384-insert-nodes.js
@@ -17,46 +17,50 @@ import {selectCharacters} from '../keyboardShortcuts';
 
 describe('Regression test #1384', () => {
   initializeE2E((e2e) => {
-    it.skipIf(e2e.isPlainText, `Properly pastes in code blocks`, async () => {
-      const {page} = e2e;
-      await focusEditor(page);
-      await page.keyboard.type('``` alert(1);');
-      await page.keyboard.press('Enter');
-      await page.keyboard.type('alert(2);');
-      await page.keyboard.press('Enter');
-      await page.keyboard.type('alert(3);');
-      await page.keyboard.press('ArrowUp');
-      await page.keyboard.press('ArrowUp');
-      await page.keyboard.press('ArrowLeft');
-      await selectCharacters(page, 'left', 8);
-      const clipboard = await copyToClipboard(page);
-      await page.keyboard.press('ArrowLeft');
-      await pasteFromClipboard(page, clipboard);
-      await expect(page).toMatchEditorInlineSnapshot(`
-        <code spellcheck="false" dir="ltr">
-          <span data-lexical-text="true">alert</span>
-          <span data-lexical-text="true">(</span>
-          <span data-lexical-text="true">1</span>
-          <span data-lexical-text="true">)</span>
-          <span data-lexical-text="true">alert</span>
-          <span data-lexical-text="true">(</span>
-          <span data-lexical-text="true">1</span>
-          <span data-lexical-text="true">)</span>
-          <span data-lexical-text="true">;</span>
-          <br />
-          <span data-lexical-text="true">alert</span>
-          <span data-lexical-text="true">(</span>
-          <span data-lexical-text="true">2</span>
-          <span data-lexical-text="true">)</span>
-          <span data-lexical-text="true">;</span>
-          <br />
-          <span data-lexical-text="true">alert</span>
-          <span data-lexical-text="true">(</span>
-          <span data-lexical-text="true">3</span>
-          <span data-lexical-text="true">)</span>
-          <span data-lexical-text="true">;</span>
-        </code>
-    `);
-    });
+    it.skipIf(
+      e2e.isPlainText || e2e.isCollab,
+      `Properly pastes in code blocks`,
+      async () => {
+        const {page} = e2e;
+        await focusEditor(page);
+        await page.keyboard.type('``` alert(1);');
+        await page.keyboard.press('Enter');
+        await page.keyboard.type('alert(2);');
+        await page.keyboard.press('Enter');
+        await page.keyboard.type('alert(3);');
+        await page.keyboard.press('ArrowUp');
+        await page.keyboard.press('ArrowUp');
+        await page.keyboard.press('ArrowLeft');
+        await selectCharacters(page, 'left', 8);
+        const clipboard = await copyToClipboard(page);
+        await page.keyboard.press('ArrowLeft');
+        await pasteFromClipboard(page, clipboard);
+        await expect(page).toMatchEditorInlineSnapshot(`
+              <code spellcheck="false" dir="ltr">
+                <span data-lexical-text="true">alert</span>
+                <span data-lexical-text="true">(</span>
+                <span data-lexical-text="true">1</span>
+                <span data-lexical-text="true">)</span>
+                <span data-lexical-text="true">alert</span>
+                <span data-lexical-text="true">(</span>
+                <span data-lexical-text="true">1</span>
+                <span data-lexical-text="true">)</span>
+                <span data-lexical-text="true">;</span>
+                <br />
+                <span data-lexical-text="true">alert</span>
+                <span data-lexical-text="true">(</span>
+                <span data-lexical-text="true">2</span>
+                <span data-lexical-text="true">)</span>
+                <span data-lexical-text="true">;</span>
+                <br />
+                <span data-lexical-text="true">alert</span>
+                <span data-lexical-text="true">(</span>
+                <span data-lexical-text="true">3</span>
+                <span data-lexical-text="true">)</span>
+                <span data-lexical-text="true">;</span>
+              </code>
+            `);
+      },
+    );
   });
 });

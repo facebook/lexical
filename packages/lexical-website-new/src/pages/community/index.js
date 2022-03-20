03afd4ccd28eae0c7f7e4823f59e827eb4931192
acywatson <acy.watson@gmail.com>
2022-03-19 18:15:57 -0700

    add licenses

--- packages/lexical-website-new/src/pages/community/index.js
+++ packages/lexical-website-new/src/pages/community/index.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ */
+
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import React from 'react';

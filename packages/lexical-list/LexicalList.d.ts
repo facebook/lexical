63f71f096e4679292da312d9c4f409f3ccf02221
Acy Watson <acy.watson@gmail.com>
2022-03-21 00:39:06 -0700

    Add missing types (#1492)
    * add missing types
    
    * fix directory structure and imports
    
    * fix ci?

---
+++ packages/lexical-list/LexicalList.d.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+ import { ListNodeTagType } from "./src/LexicalListNode";
+ import { ElementNode, LexicalNode, LexicalEditor, ParagraphNode, RangeSelection } from 'lexical';
+
+export function $createListItemNode(): ListItemNode;
+export function $createListNode(tag: ListNodeTagType, start?: number): ListNode;
+export function $getListDepth(listNode: ListNode): number;
+export function $handleListInsertParagraph(): boolean;
+export function $isListItemNode(node?: LexicalNode): boolean;
+export function $isListNode(node?: LexicalNode): boolean;
+export function indentList(): boolean;
+export function insertList(editor: LexicalEditor, listType: 'ul' | 'ol'): void;
+export declare class ListItemNode extends ElementNode {
+    append(...nodes: LexicalNode[]): ListItemNode;
+    replace<N extends LexicalNode>(replaceWithNode: N): N;
+    insertAfter(node: LexicalNode): LexicalNode;
+    insertNewAfter(): ListItemNode | ParagraphNode;
+    collapseAtStart(selection: RangeSelection): true;
+    getIndent(): number;
+    setIndent(indent: number): this;
+    insertBefore(nodeToInsert: LexicalNode): LexicalNode;
+    canInsertAfter(node: LexicalNode): boolean;
+    canReplaceWith(replacement: LexicalNode): boolean;
+    canMergeWith(node: LexicalNode): boolean;
+ }
+export declare class ListNode extends ElementNode {
+    canBeEmpty(): false;
+    append(...nodesToAppend: LexicalNode[]): ListNode;
+    getTag(): ListNodeTagType;
+}
+export function outdentList(): boolean;
+export function removeList(editor: LexicalEditor): boolean;

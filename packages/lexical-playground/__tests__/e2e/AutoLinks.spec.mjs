d494cfef730804482d61c9cf26c659e8c3e028cc
Maksim Horbachevsky <fantactuka@gmail.com>
2022-03-23 12:08:54 -0400

    Prevent auto-link within link (#1504)
    * Replace browser-based formatting with prettier, fix assertion
    
    * Prevent auto-link within link

--- packages/lexical-playground/__tests__/e2e/AutoLinks.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/AutoLinks.spec.mjs
@@ -6,88 +6,112 @@
  *
  */
 
-import {moveToLineBeginning, moveToLineEnd} from '../keyboardShortcuts/index.mjs';
 import {
+  moveLeft,
+  moveRight,
+  moveToLineBeginning,
+  moveToLineEnd,
+  selectAll,
+} from '../keyboardShortcuts/index.mjs';
+import {
+  applyLink,
   assertHTML,
   focusEditor,
   initialize,
   pasteFromClipboard,
-  test
+  test,
 } from '../utils/index.mjs';
 
 test.describe('Auto Links', () => {
-    test.beforeEach(({isCollab, page }) => initialize({ isCollab, page }));
-    test(
-      'Can convert url-like text into links',
-      async ({page, isPlainText}) => {
-        test.skip(isPlainText);
-        await focusEditor(page);
-        await page.keyboard.type(
-          'Hello http://example.com and https://example.com/path?with=query#and-hash and www.example.com',
-        );
-        await assertHTML(
-          page,
-          '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">Hello </span><a href="http://example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com</span></a><span data-lexical-text="true"> and </span><a href="https://example.com/path?with=query#and-hash" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">https://example.com/path?with=query#and-hash</span></a><span data-lexical-text="true"> and </span><a href="www.example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">www.example.com</span></a></p>',
-        );
-      },
+  test.beforeEach(({isCollab, page}) => initialize({isCollab, page}));
+
+  test('Can convert url-like text into links', async ({page, isPlainText}) => {
+    test.skip(isPlainText);
+    await focusEditor(page);
+    await page.keyboard.type(
+      'Hello http://example.com and https://example.com/path?with=query#and-hash and www.example.com',
     );
-
-    test(
-      'Can destruct links if add non-spacing text in front or right after it',
-      async ({page, isPlainText}) => {
-        test.skip(isPlainText);
-        const htmlWithLink =
-          '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><a href="http://example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com</span></a></p>';
-
-        await focusEditor(page);
-        await page.keyboard.type('http://example.com');
-        await assertHTML(page, htmlWithLink);
-
-        // Add non-url text after the link
-        await page.keyboard.type('!');
-        await assertHTML(
-          page,
-          '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com!</span></p>',
-        );
-        await page.keyboard.press('Backspace');
-        await assertHTML(page, htmlWithLink);
-
-        // Add non-url text before the link
-        await moveToLineBeginning(page);
-        await page.keyboard.type('!');
-        await assertHTML(
-          page,
-          '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">!http://example.com</span></p>',
-        );
-        await page.keyboard.press('Backspace');
-        await assertHTML(page, htmlWithLink);
-
-        // Add newline after link
-        await moveToLineEnd(page);
-        await page.keyboard.press('Enter');
-        await assertHTML(
-          page,
-          htmlWithLink +
-            '<p class="PlaygroundEditorTheme__paragraph"><br /></p>',
-        );
-        await page.keyboard.press('Backspace');
-        await assertHTML(page, htmlWithLink);
-      },
+    await assertHTML(
+      page,
+      '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">Hello </span><a href="http://example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com</span></a><span data-lexical-text="true"> and </span><a href="https://example.com/path?with=query#and-hash" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">https://example.com/path?with=query#and-hash</span></a><span data-lexical-text="true"> and </span><a href="www.example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">www.example.com</span></a></p>',
     );
+  });
 
-    test(
-      'Can create link when pasting text with urls',
-      async ({page, isPlainText}) => {
-        test.skip(isPlainText);
-        await focusEditor(page);
-        await pasteFromClipboard(page, {
-          'text/plain':
-            'Hello http://example.com and https://example.com/path?with=query#and-hash and www.example.com',
-        });
-        await assertHTML(
-          page,
-          '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">Hello </span><a href="http://example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com</span></a><span data-lexical-text="true"> and </span><a href="https://example.com/path?with=query#and-hash" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">https://example.com/path?with=query#and-hash</span></a><span data-lexical-text="true"> and </span><a href="www.example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">www.example.com</span></a></p>',
-        );
-      },
+  test('Can destruct links if add non-spacing text in front or right after it', async ({
+    page,
+    isPlainText,
+  }) => {
+    test.skip(isPlainText);
+    const htmlWithLink =
+      '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><a href="http://example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com</span></a></p>';
+
+    await focusEditor(page);
+    await page.keyboard.type('http://example.com');
+    await assertHTML(page, htmlWithLink);
+
+    // Add non-url text after the link
+    await page.keyboard.type('!');
+    await assertHTML(
+      page,
+      '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com!</span></p>',
     );
+    await page.keyboard.press('Backspace');
+    await assertHTML(page, htmlWithLink);
+
+    // Add non-url text before the link
+    await moveToLineBeginning(page);
+    await page.keyboard.type('!');
+    await assertHTML(
+      page,
+      '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">!http://example.com</span></p>',
+    );
+    await page.keyboard.press('Backspace');
+    await assertHTML(page, htmlWithLink);
+
+    // Add newline after link
+    await moveToLineEnd(page);
+    await page.keyboard.press('Enter');
+    await assertHTML(
+      page,
+      htmlWithLink + '<p class="PlaygroundEditorTheme__paragraph"><br /></p>',
+    );
+    await page.keyboard.press('Backspace');
+    await assertHTML(page, htmlWithLink);
+  });
+
+  test('Can create link when pasting text with urls', async ({
+    page,
+    isPlainText,
+  }) => {
+    test.skip(isPlainText);
+    await focusEditor(page);
+    await pasteFromClipboard(page, {
+      'text/plain':
+        'Hello http://example.com and https://example.com/path?with=query#and-hash and www.example.com',
+    });
+    await assertHTML(
+      page,
+      '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">Hello </span><a href="http://example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">http://example.com</span></a><span data-lexical-text="true"> and </span><a href="https://example.com/path?with=query#and-hash" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">https://example.com/path?with=query#and-hash</span></a><span data-lexical-text="true"> and </span><a href="www.example.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">www.example.com</span></a></p>',
+    );
+  });
+
+  test('Does not create redundant auto-link', async ({page, isPlainText}) => {
+    test.skip(isPlainText);
+    await focusEditor(page);
+    await page.keyboard.type('hm');
+
+    await selectAll(page);
+    await applyLink(page);
+    await assertHTML(
+      page,
+      '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><a href="https://facebook.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">hm</span></a></p>',
+    );
+    await moveLeft(page, 1);
+    await moveRight(page, 1);
+    await page.keyboard.type('ttps://facebook.co');
+    await assertHTML(
+      page,
+      '<p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><a href="https://facebook.com" class="PlaygroundEditorTheme__link PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">https://facebook.com</span></a></p>',
+    );
+  });
 });

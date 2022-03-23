b86623336b504cdfcb98b62115a51bfb8c0f428c
John Flockton <thegreatercurve@users.noreply.github.com>
2022-03-23 18:44:42 +0000

    Add twitter node (#1493)
    * Fix missing pre-commit linting checks in JSX files (#1494)
    
    * Fix linting in JSX files
    
    * Fix a couple more bugs
    
    * Add mjs files back
    
    * Fix linting errors for having onclicks on divs
    
    * Fix another small linting error
    
    * formatting change
    
    * Get tweet ID from URL

---
+++ packages/lexical-playground/src/nodes/TweetNode.jsx
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+import type {DecoratorMap, LexicalNode, NodeKey} from 'lexical';
+
+import {DecoratorNode} from 'lexical';
+import * as React from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
+
+const WIDGET_SCRIPT_URL = 'https://platform.twitter.com/widgets.js';
+
+const getHasScriptCached = () =>
+  document.querySelector(`script[src="${WIDGET_SCRIPT_URL}"]`);
+
+type TweetComponentProps = $ReadOnly<{
+  loadingComponent?: React$Node,
+  onError?: (error?: Error) => void,
+  onLoad?: (error?: Error) => void,
+  tweetID: string,
+}>;
+
+function TweetComponent({
+  loadingComponent,
+  onError,
+  onLoad,
+  tweetID,
+}: TweetComponentProps) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const previousTweetIDRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const createTweet = useCallback(async () => {
+    try {
+      const element = await window.twttr.widgets.createTweet(
+        tweetID,
+        containerRef.current,
+      );
+
+      setIsLoading(false);
+      onLoad?.(element);
+    } catch (e) {
+      onError?.(e);
+    }
+  }, [onError, onLoad, tweetID]);
+
+  useEffect(() => {
+    if (tweetID !== previousTweetIDRef.current) {
+      setIsLoading(true);
+
+      if (!getHasScriptCached()) {
+        const script = document.createElement('script');
+        script.src = WIDGET_SCRIPT_URL;
+        script.async = true;
+        document.body?.appendChild(script);
+        script.onload = createTweet;
+        script.onerror = onError;
+      } else {
+        createTweet();
+      }
+
+      previousTweetIDRef.current = tweetID;
+    }
+  }, [createTweet, onError, tweetID]);
+
+  return (
+    <>
+      {isLoading ? loadingComponent : null}
+      <div ref={containerRef} />
+    </>
+  );
+}
+
+export class TweetNode extends DecoratorNode<React$Node> {
+  __id: string;
+
+  static getType(): string {
+    return 'tweet';
+  }
+
+  static clone(node: TweetNode): TweetNode {
+    return new TweetNode(node.__id, node.__state, node.__key);
+  }
+
+  constructor(id: string, state?: DecoratorMap, key?: NodeKey) {
+    super(state, key);
+
+    this.__id = id;
+  }
+
+  createDOM(): HTMLElement {
+    return document.createElement('div');
+  }
+
+  updateDOM(): false {
+    return false;
+  }
+
+  decorate(): React$Node {
+    return <TweetComponent loadingComponent="Loading..." tweetID={this.__id} />;
+  }
+}
+
+export function $createTweetNode(tweetID: string): TweetNode {
+  return new TweetNode(tweetID);
+}
+
+export function $isTweetNode(node: ?LexicalNode): boolean %checks {
+  return node instanceof TweetNode;
+}

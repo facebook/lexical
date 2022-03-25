885405ae88e1f94e27b2989711a13a0d102d006a
Yangshun Tay <tay.yang.shun@gmail.com>
2022-03-26 02:01:41 +0800

    website: improve styling for homepage (#1545)
    * website: improve styling for homepage
    
    * feat: use accessible tabs
    
    * fix package-lock
    
    Co-authored-by: acywatson <acy.watson@gmail.com>

--- packages/lexical-website-new/src/components/HomepageExamples/index.js
+++ packages/lexical-website-new/src/components/HomepageExamples/index.js
@@ -4,106 +4,120 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- *
  */
 
 import Link from '@docusaurus/Link';
+import * as Tabs from '@radix-ui/react-tabs';
+import clsx from 'clsx';
 import React, {useState} from 'react';
 
 import styles from './styles.module.css';
 
-function Pills({pills, activeIndex, onClick}) {
-  return (
-    <div role="tablist" className="pills">
-      {pills.map((pill, index) => {
-        const classNames = ['pills__item'];
-        const isSelected = activeIndex === index;
-        if (isSelected) {
-          classNames.push('pills__item--active');
-          classNames.push(styles.tabSelected);
-        }
-        return (
-          <a
-            key={pill.id}
-            className={`${classNames.join(' ')} ${styles.tabAnchor}`}
-            aria-selected={isSelected}
-            tabIndex="0"
-            role="tab"
-            id={`${pill.id}-tab`}
-            onClick={() => {
-              onClick(index);
-            }}
-          >
-            {pill.label}
-          </a>
-        );
-      })}
-    </div>
-  );
-}
+const EXAMPLES = [
+  {
+    content: (
+      <>
+        Feature 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.
+      </>
+    ),
+    id: 'example-feature-1',
+    label: 'Easy Setup',
+    src: 'https://codesandbox.io/embed/lexical-plain-text-example-g932e?fontsize=12&hidenavigation=1&module=%2Fsrc%2FEditor.js&theme=dark&view=editor',
+  },
+  {
+    content: (
+      <>
+        Feature 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.
+      </>
+    ),
+    id: 'example-feature-2',
+    label: 'Powerful Transforms',
+    src: 'https://codesandbox.io/embed/lexical-plain-text-example-forked-qdxhy?fontsize=12&hidenavigation=1&module=%2Fsrc%2FEmoticonPlugin.js&theme=dark&view=editor',
+  },
+  {
+    content: (
+      <>
+        Feature 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.
+      </>
+    ),
+    id: 'example-feature-3',
+    label: 'Works Anywhere',
+    src: 'https://codesandbox.io/embed/lexical-plain-text-example-g932e?fontsize=12&hidenavigation=1&module=%2Fsrc%2FEditor.js&theme=dark&view=editor',
+  },
+];
 
 export default function HomepageExamples() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const pills = [
-    {
-      content: `Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content
-      Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 contentFeature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content
-      Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 contentFeature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content Feature 1 content`,
-      id: 'example-feature-1',
-      label: 'Easy Setup',
-      src: 'https://codesandbox.io/embed/lexical-plain-text-example-g932e?fontsize=12&hidenavigation=1&module=%2Fsrc%2FEditor.js&theme=dark&view=editor',
-    },
-    {
-      content: `Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content
-      Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 contentFeature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content
-      Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 contentFeature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content Feature 2 content`,
-      id: 'example-feature-2',
-      label: 'Powerful Transforms',
-      src: 'https://codesandbox.io/embed/lexical-plain-text-example-forked-qdxhy?fontsize=12&hidenavigation=1&module=%2Fsrc%2FEmoticonPlugin.js&theme=dark&view=editor',
-    },
-    {
-      content: `Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content
-      Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 contentFeature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content
-      Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 contentFeature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content Feature 3 content`,
-      id: 'example-feature-3',
-      label: 'Works Anywhere',
-      src: 'https://codesandbox.io/embed/lexical-plain-text-example-g932e?fontsize=12&hidenavigation=1&module=%2Fsrc%2FEditor.js&theme=dark&view=editor',
-    },
-  ];
-  const activePill = pills[activeIndex];
+  const [activeItemID, setActiveItemID] = useState(EXAMPLES[0].id);
+
   return (
-    <div className="container">
-      <div className="row">
-        <Pills
-          pills={pills}
-          activeIndex={activeIndex}
-          onClick={setActiveIndex}
-        />
+    <Tabs.Root
+      value={activeItemID}
+      orientation="horizontal"
+      onValueChange={setActiveItemID}
+    >
+      <div className="container">
+        <Tabs.List asChild={true} className="pills" loop={true}>
+          <ul>
+            {EXAMPLES.map(({id, label}) => (
+              <Tabs.Trigger asChild={true} value={id} key={id}>
+                <li
+                  className={clsx(
+                    'pills__item',
+                    activeItemID === id && 'pills__item--active',
+                  )}
+                  tabIndex={0}
+                  role="tab"
+                >
+                  {label}
+                </li>
+              </Tabs.Trigger>
+            ))}
+          </ul>
+        </Tabs.List>
+        {EXAMPLES.map(({id, content, src}) => (
+          <Tabs.Content asChild={true} value={id} key={id}>
+            <div className="row">
+              <div className="col col--4">
+                {content}
+                <div>
+                  <Link
+                    className="button button--primary margin-top--md"
+                    to="/docs/intro"
+                  >
+                    Get Started
+                  </Link>
+                </div>
+              </div>
+              <div className="col col--8">
+                <iframe
+                  className={styles.codesandbox}
+                  src={src}
+                  title="lexical-plain-text-example"
+                  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+                />
+              </div>
+            </div>
+          </Tabs.Content>
+        ))}
       </div>
-      <div
-        className={`row margin-vert--lg ${styles.tabContent}`}
-        id={activePill.id}
-        role="tabpanel"
-        aria-labelledby={`example-tab-${activeIndex}`}
-      >
-        <div className="col col--4">
-          {activePill.content}
-          <Link
-            className="button button--primary margin-top--md"
-            to="/docs/intro"
-          >
-            Get Started
-          </Link>
-        </div>
-        <div className="col col--8">
-          <iframe
-            className={styles.codesandbox}
-            src={activePill.src}
-            title="lexical-plain-text-example"
-            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-          />
-        </div>
-      </div>
-    </div>
+    </Tabs.Root>
   );
 }

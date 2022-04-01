facebook/lexical

commit e1b67d08977de51306bcbcc208b9339c0d9c0280

Move command type out of listeners to `registerCommandListener` (#1551)
* Move command type out of listener callback to registerCommandListener outer method

* Remove optional chaining

* correct boolean values returned from some command callbacks

* Fix bug with priorty ordering

* clarify type

* Fix failing E2E

* Fix E2E bug

* Wrap mutiple commands in withSubscriptions

* update docs

* Address small issues and add unit test

* remove console logs

* Fix E2E bugs

* Fix E2E bugs

* Remove flow fixmes and add invariant

packages/lexical-react/src/shared/useYjsCollaboration.jsx
@@ -11,6 +11,7 @@ import type {Binding, Provider} from '@lexical/yjs';
 import type {CommandListenerEditorPriority, LexicalEditor} from 'lexical';
 import type {Doc} from 'yjs';
 
+import withSubscriptions from '@lexical/react/withSubscriptions';
 import {
   createBinding,
   createUndoManager,
@@ -166,8 +167,9 @@ export function useYjsCollaboration(
   }, [binding]);
 
   useEffect(() => {
-    return editor.registerCommandListener((type, payload) => {
-      if (type === 'toggleConnect') {
+    return editor.registerCommandListener(
+      'toggleConnect',
+      (payload) => {
         if (connect !== undefined && disconnect !== undefined) {
           const shouldConnect = payload;
           if (shouldConnect) {
@@ -180,9 +182,10 @@ export function useYjsCollaboration(
             disconnect();
           }
         }
-      }
-      return false;
-    }, EditorPriority);
+        return true;
+      },
+      EditorPriority,
+    );
   }, [connect, disconnect, editor]);
 
   return [cursorsContainer, binding];
@@ -195,14 +198,24 @@ export function useYjsFocusTracking(
   color: string,
 ) {
   useEffect(() => {
-    return editor.registerCommandListener((type, payload) => {
-      if (type === 'focus') {
-        setLocalStateFocus(provider, name, color, true);
-      } else if (type === 'blur') {
-        setLocalStateFocus(provider, name, color, false);
-      }
-      return false;
-    }, EditorPriority);
+    return withSubscriptions(
+      editor.registerCommandListener(
+        'focus',
+        (payload) => {
+          setLocalStateFocus(provider, name, color, false);
+          return true;
+        },
+        EditorPriority,
+      ),
+      editor.registerCommandListener(
+        'blur',
+        (payload) => {
+          setLocalStateFocus(provider, name, color, false);
+          return true;
+        },
+        EditorPriority,
+      ),
+    );
   }, [color, editor, name, provider]);
 }
 
@@ -219,24 +232,28 @@ export function useYjsHistory(
     const undo = () => {
       undoManager.undo();
     };
-
     const redo = () => {
       undoManager.redo();
     };
 
-    const applyCommand = (type) => {
-      if (type === 'undo') {
-        undo();
-        return true;
-      }
-      if (type === 'redo') {
-        redo();
-        return true;
-      }
-      return false;
-    };
-
-    return editor.registerCommandListener(applyCommand, EditorPriority);
+    return withSubscriptions(
+      editor.registerCommandListener(
+        'undo',
+        () => {
+          undo();
+          return true;
+        },
+        EditorPriority,
+      ),
+      editor.registerCommandListener(
+        'redo',
+        () => {
+          redo();
+          return true;
+        },
+        EditorPriority,
+      ),
+    );
   });
 
   const clearHistory = useCallback(() => {

packages/lexical-code/src/index.js
@@ -752,12 +752,23 @@ export function registerCodeHighlighting(
       textNodeTransform(node, editor),
     ),
     editor.registerCommandListener(
-      (type, payload): boolean =>
-        type === 'indentContent' || type === 'outdentContent'
-          ? handleMultilineIndent(type)
-          : type === 'keyArrowUp' || type === 'keyArrowDown'
-          ? handleShiftLines(type, payload)
-          : false,
+      'indentContent',
+      (payload): boolean => handleMultilineIndent('indentContent'),
+      1,
+    ),
+    editor.registerCommandListener(
+      'outdentContent',
+      (payload): boolean => handleMultilineIndent('outdentContent'),
+      1,
+    ),
+    editor.registerCommandListener(
+      'keyArrowUp',
+      (payload): boolean => handleShiftLines('keyArrowUp', payload),
+      1,
+    ),
+    editor.registerCommandListener(
+      'keyArrowDown',
+      (payload): boolean => handleShiftLines('keyArrowDown', payload),
       1,
     ),
   ];

packages/lexical-history/src/index.js
@@ -19,6 +19,7 @@ import type {
   RangeSelection,
 } from 'lexical';
 
+import withSubscriptions from '@lexical/react/withSubscriptions';
 import {
   $getSelection,
   $isRangeSelection,
@@ -362,29 +363,42 @@ export function registerHistory(
     };
   };
 
-  const applyCommand = (type) => {
-    switch (type) {
-      case 'undo':
+  const unregisterCommandListener = withSubscriptions(
+    editor.registerCommandListener(
+      'undo',
+      () => {
         undo(editor, historyState);
         return true;
-      case 'redo':
+      },
+      EditorPriority,
+    ),
+    editor.registerCommandListener(
+      'redo',
+      () => {
         redo(editor, historyState);
         return true;
-      case 'clearEditor':
+      },
+      EditorPriority,
+    ),
+    editor.registerCommandListener(
+      'clearEditor',
+      () => {
         clearHistory(historyState);
         return false;
-      case 'clearHistory':
+      },
+      EditorPriority,
+    ),
+    editor.registerCommandListener(
+      'clearHistory',
+      () => {
         clearHistory(historyState);
         return true;
-      default:
-        return false;
-    }
-  };
-
-  const unregisterCommandListener = editor.registerCommandListener(
-    applyCommand,
-    EditorPriority,
+      },
+      EditorPriority,
+    ),
+    editor.registerUpdateListener(applyChange),
   );
+
   const unregisterUpdateListener = editor.registerUpdateListener(applyChange);
 
   return () => {

packages/lexical-plain-text/src/index.js
@@ -7,16 +7,13 @@
  * @flow strict
  */
 
-import type {
-  CommandListenerEditorPriority,
-  EditorState,
-  LexicalEditor,
-} from 'lexical';
+import type {EditorState, LexicalEditor} from 'lexical';
 
 import {
   $insertDataTransferForPlainText,
   getHtmlContent,
 } from '@lexical/clipboard';
+import withSubscriptions from '@lexical/react/withSubscriptions';
 import {
   $moveCharacter,
   $shouldOverrideDefaultCharacterSelection,
@@ -130,125 +127,327 @@ export function registerPlainText(
   editor: LexicalEditor,
   initialEditorState?: InitialEditorStateType,
 ): () => void {
-  const removeListener = editor.registerCommandListener(
-    (type, payload): boolean => {
-      const selection = $getSelection();
-      if (!$isRangeSelection(selection)) {
-        return false;
-      }
-      switch (type) {
-        case 'deleteCharacter': {
-          const isBackward: boolean = payload;
-          selection.deleteCharacter(isBackward);
-          return true;
+  const removeListener = withSubscriptions(
+    editor.registerCommandListener(
+      'deleteCharacter',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
         }
-        case 'deleteWord': {
-          const isBackward: boolean = payload;
-          selection.deleteWord(isBackward);
-          return true;
+        const isBackward: boolean = payload;
+        selection.deleteCharacter(isBackward);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'deleteWord',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
         }
-        case 'deleteLine': {
-          const isBackward: boolean = payload;
-          selection.deleteLine(isBackward);
-          return true;
+        const isBackward: boolean = payload;
+        selection.deleteWord(isBackward);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'deleteLine',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
         }
-        case 'insertText': {
-          const eventOrText: InputEvent | string = payload;
-          if (typeof eventOrText === 'string') {
-            selection.insertText(eventOrText);
+        const isBackward: boolean = payload;
+        selection.deleteLine(isBackward);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'insertText',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const eventOrText: InputEvent | string = payload;
+        if (typeof eventOrText === 'string') {
+          selection.insertText(eventOrText);
+        } else {
+          const dataTransfer = eventOrText.dataTransfer;
+          if (dataTransfer != null) {
+            $insertDataTransferForPlainText(dataTransfer, selection);
           } else {
-            const dataTransfer = eventOrText.dataTransfer;
-            if (dataTransfer != null) {
-              $insertDataTransferForPlainText(dataTransfer, selection);
-            } else {
-              const data = eventOrText.data;
-              if (data) {
-                selection.insertText(data);
-              }
+            const data = eventOrText.data;
+            if (data) {
+              selection.insertText(data);
             }
           }
-          return true;
         }
-        case 'removeText':
-          selection.removeText();
-          return true;
-        case 'insertLineBreak':
-          const selectStart: boolean = payload;
-          selection.insertLineBreak(selectStart);
-          return true;
-        case 'insertParagraph':
-          selection.insertLineBreak();
-          return true;
-        case 'indentContent':
-        case 'outdentContent':
-        case 'insertHorizontalRule':
-        case 'insertImage':
-        case 'insertTable':
-        case 'formatElement':
-        case 'formatText': {
-          return true;
-        }
-        case 'keyArrowLeft': {
-          const event: KeyboardEvent = payload;
-          const isHoldingShift = event.shiftKey;
-          if ($shouldOverrideDefaultCharacterSelection(selection, true)) {
-            event.preventDefault();
-            $moveCharacter(selection, isHoldingShift, true);
-            return true;
-          }
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'removeText',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
           return false;
         }
-        case 'keyArrowRight': {
-          const event: KeyboardEvent = payload;
-          const isHoldingShift = event.shiftKey;
-          if ($shouldOverrideDefaultCharacterSelection(selection, false)) {
-            event.preventDefault();
-            $moveCharacter(selection, isHoldingShift, false);
-            return true;
-          }
+        selection.removeText();
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'insertLineBreak',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
           return false;
         }
-        case 'keyBackspace': {
-          const event: KeyboardEvent = payload;
-          event.preventDefault();
-          return editor.execCommand('deleteCharacter', true);
+        const selectStart: boolean = payload;
+        selection.insertLineBreak(selectStart);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'insertParagraph',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
         }
-        case 'keyDelete': {
-          const event: KeyboardEvent = payload;
-          event.preventDefault();
-          return editor.execCommand('deleteCharacter', false);
+        selection.insertLineBreak();
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'indentContent',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
         }
-        case 'keyEnter': {
-          const event: KeyboardEvent = payload;
-          event.preventDefault();
-          return editor.execCommand('insertLineBreak');
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'outdentContent',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
         }
-        case 'copy': {
-          const event: ClipboardEvent = payload;
-          onCopyForPlainText(event, editor);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'insertHorizontalRule',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'insertImage',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'insertTable',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'formatElement',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'formatText',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'keyArrowLeft',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const event: KeyboardEvent = payload;
+        const isHoldingShift = event.shiftKey;
+        if ($shouldOverrideDefaultCharacterSelection(selection, true)) {
+          event.preventDefault();
+          $moveCharacter(selection, isHoldingShift, true);
           return true;
         }
-        case 'cut': {
-          const event: ClipboardEvent = payload;
-          onCutForPlainText(event, editor);
-          return true;
+        return false;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'keyArrowRight',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
         }
-        case 'paste': {
-          const event: ClipboardEvent = payload;
-          onPasteForPlainText(event, editor);
-          return true;
-        }
-        case 'drop':
-        case 'dragstart': {
-          // TODO: Make drag and drop work at some point.
-          const event: DragEvent = payload;
+        const event: KeyboardEvent = payload;
+        const isHoldingShift = event.shiftKey;
+        if ($shouldOverrideDefaultCharacterSelection(selection, false)) {
           event.preventDefault();
+          $moveCharacter(selection, isHoldingShift, false);
           return true;
         }
-      }
-      return false;
-    },
-    (0: CommandListenerEditorPriority),
+        return false;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'keyBackspace',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const event: KeyboardEvent = payload;
+        event.preventDefault();
+        return editor.execCommand('deleteCharacter', true);
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'keyDelete',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const event: KeyboardEvent = payload;
+        event.preventDefault();
+        return editor.execCommand('deleteCharacter', false);
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'keyEnter',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const event: KeyboardEvent = payload;
+        event.preventDefault();
+        return editor.execCommand('insertLineBreak');
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'copy',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const event: ClipboardEvent = payload;
+        onCopyForPlainText(event, editor);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'cut',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const event: ClipboardEvent = payload;
+        onCutForPlainText(event, editor);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'paste',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        const event: ClipboardEvent = payload;
+        onPasteForPlainText(event, editor);
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'drop',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        // TODO: Make drag and drop work at some point.
+        const event: DragEvent = payload;
+        event.preventDefault();
+        return true;
+      },
+      0,
+    ),
+    editor.registerCommandListener(
+      'dragstart',
+      (payload) => {
+        const selection = $getSelection();
+        if (!$isRangeSelection(selection)) {
+          return false;
+        }
+        // TODO: Make drag and drop work at some point.
+        const event: DragEvent = payload;
+        event.preventDefault();
+        return true;
+      },
+      0,
+    ),
   );
   initializeEditor(editor, initialEditorState);
   return removeListener;

packages/lexical-playground/src/nodes/EquationNode.jsx
@@ -16,6 +16,7 @@ import type {
 } from 'lexical';
 
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import withSubscriptions from '@lexical/react/withSubscriptions';
 import {$getNodeByKey, DecoratorNode} from 'lexical';
 import * as React from 'react';
 import {useCallback, useEffect, useRef, useState} from 'react';
@@ -59,19 +60,33 @@ function EquationComponent({
 
   useEffect(() => {
     if (showEquationEditor) {
-      return editor.registerCommandListener((type, payload) => {
-        const activeElement = document.activeElement;
-        const inputElem = inputRef.current;
-
-        if (type === 'selectionChange' && inputElem !== activeElement) {
-          onHide();
-        } else if (type === 'keyEscape' && inputElem === activeElement) {
-          onHide(true);
-          return true;
-        }
-
-        return false;
-      }, HighPriority);
+      return withSubscriptions(
+        editor.registerCommandListener(
+          'selectionChange',
+          (payload) => {
+            const activeElement = document.activeElement;
+            const inputElem = inputRef.current;
+            if (inputElem !== activeElement) {
+              onHide();
+            }
+            return false;
+          },
+          HighPriority,
+        ),
+        editor.registerCommandListener(
+          'keyEscape',
+          (payload) => {
+            const activeElement = document.activeElement;
+            const inputElem = inputRef.current;
+            if (inputElem === activeElement) {
+              onHide(true);
+              return true;
+            }
+            return false;
+          },
+          HighPriority,
+        ),
+      );
     }
   }, [editor, onHide, showEquationEditor]);
 

packages/lexical-playground/src/nodes/ImageNode.jsx
@@ -32,6 +32,7 @@ import RichTextPlugin from '@lexical/react/LexicalRichTextPlugin';
 import TablesPlugin from '@lexical/react/LexicalTablePlugin';
 import useLexicalDecoratorMap from '@lexical/react/useLexicalDecoratorMap';
 import useLexicalNodeSelection from '@lexical/react/useLexicalNodeSelection';
+import withSubscriptions from '@lexical/react/withSubscriptions';
 import {
   $getNodeByKey,
   $getSelection,
@@ -309,32 +310,9 @@ function ImageComponent({
   );
   const [selection, setSelection] = useState(null);
 
-  useEffect(() => {
-    return editor.registerUpdateListener(({editorState}) => {
-      setSelection(editorState.read(() => $getSelection()));
-    });
-  }, [editor]);
-
-  useEffect(() => {
-    return editor.registerCommandListener((type, payload) => {
-      if (type === 'click') {
-        const event: MouseEvent = payload;
-
-        if (isResizing) {
-          return true;
-        }
-        if (event.target === ref.current) {
-          if (!event.shiftKey) {
-            clearSelection();
-          }
-          setSelected(!isSelected);
-          return true;
-        }
-      } else if (
-        isSelected &&
-        $isNodeSelection($getSelection()) &&
-        (type === 'keyDelete' || type === 'keyBackspace')
-      ) {
+  const onDelete = useCallback(
+    (payload) => {
+      if (isSelected && $isNodeSelection($getSelection())) {
         const event: KeyboardEvent = payload;
         event.preventDefault();
         editor.update(() => {
@@ -346,8 +324,47 @@ function ImageComponent({
         });
       }
       return false;
-    }, LowPriority);
-  }, [clearSelection, editor, isResizing, isSelected, nodeKey, setSelected]);
+    },
+    [editor, isSelected, nodeKey, setSelected],
+  );
+
+  useEffect(() => {
+    return withSubscriptions(
+      editor.registerUpdateListener(({editorState}) => {
+        setSelection(editorState.read(() => $getSelection()));
+      }),
+      editor.registerCommandListener(
+        'click',
+        (payload) => {
+          const event: MouseEvent = payload;
+
+          if (isResizing) {
+            return true;
+          }
+          if (event.target === ref.current) {
+            if (!event.shiftKey) {
+              clearSelection();
+            }
+            setSelected(!isSelected);
+            return true;
+          }
+
+          return false;
+        },
+        LowPriority,
+      ),
+      editor.registerCommandListener('keyDelete', onDelete, LowPriority),
+      editor.registerCommandListener('keyBackspace', onDelete, LowPriority),
+    );
+  }, [
+    clearSelection,
+    editor,
+    isResizing,
+    isSelected,
+    nodeKey,
+    onDelete,
+    setSelected,
+  ]);
 
   const setShowCaption = useCallback(() => {
     editor.update(() => {

packages/lexical-playground/src/plugins/ActionsPlugin.jsx
@@ -12,6 +12,7 @@ import type {CommandListenerEditorPriority} from 'lexical';
 import {exportFile, importFile} from '@lexical/file';
 import {useCollaborationContext} from '@lexical/react/LexicalCollaborationPlugin';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import withSubscriptions from '@lexical/react/withSubscriptions';
 import {$getRoot} from 'lexical';
 import * as React from 'react';
 import {useCallback, useEffect, useState} from 'react';
@@ -34,16 +35,26 @@ export default function ActionsPlugins({
   const isCollab = yjsDocMap.get('main') !== undefined;
 
   useEffect(() => {
-    return editor.registerCommandListener((type, payload) => {
-      if (type === 'readOnly') {
-        const readOnly = payload;
-        setIsReadyOnly(readOnly);
-      } else if (type === 'connected') {
-        const isConnected = payload;
-        setConnected(isConnected);
-      }
-      return false;
-    }, EditorPriority);
+    return withSubscriptions(
+      editor.registerCommandListener(
+        'readOnly',
+        (payload) => {
+          const readOnly = payload;
+          setIsReadyOnly(readOnly);
+          return false;
+        },
+        EditorPriority,
+      ),
+      editor.registerCommandListener(
+        'connected',
+        (payload) => {
+          const isConnected = payload;
+          setConnected(isConnected);
+          return false;
+        },
+        EditorPriority,
+      ),
+    );
   }, [editor]);
 
   const insertSticky = useCallback(() => {

packages/lexical-playground/src/plugins/CharacterStylesPopupPlugin.jsx
@@ -103,12 +103,14 @@ function FloatingCharacterStylesEditor({
         });
       }),
 
-      editor.registerCommandListener((type) => {
-        if (type === 'selectionChange') {
+      editor.registerCommandListener(
+        'selectionChange',
+        () => {
           updateCharacterStylesEditor();
-        }
-        return false;
-      }, 1),
+          return false;
+        },
+        1,
+      ),
     );
   }, [editor, updateCharacterStylesEditor]);
 
@@ -119,7 +121,8 @@ function FloatingCharacterStylesEditor({
           editor.execCommand('formatText', 'bold');
         }}
         className={'popup-item spaced ' + (isBold ? 'active' : '')}
-        aria-label="Format Bold">
+        aria-label="Format Bold"
+      >
         <i className="format bold" />
       </button>
       <button
@@ -127,7 +130,8 @@ function FloatingCharacterStylesEditor({
           editor.execCommand('formatText', 'italic');
         }}
         className={'popup-item spaced ' + (isItalic ? 'active' : '')}
-        aria-label="Format Italics">
+        aria-label="Format Italics"
+      >
         <i className="format italic" />
       </button>
       <button
@@ -135,7 +139,8 @@ function FloatingCharacterStylesEditor({
           editor.execCommand('formatText', 'underline');
         }}
         className={'popup-item spaced ' + (isUnderline ? 'active' : '')}
-        aria-label="Format Underline">
+        aria-label="Format Underline"
+      >
         <i className="format underline" />
       </button>
       <button
@@ -143,7 +148,8 @@ function FloatingCharacterStylesEditor({
           editor.execCommand('formatText', 'strikethrough');
         }}
         className={'popup-item spaced ' + (isStrikethrough ? 'active' : '')}
-        aria-label="Format Strikethrough">
+        aria-label="Format Strikethrough"
+      >
         <i className="format strikethrough" />
       </button>
       <button
@@ -151,13 +157,15 @@ function FloatingCharacterStylesEditor({
           editor.execCommand('formatText', 'code');
         }}
         className={'popup-item spaced ' + (isCode ? 'active' : '')}
-        aria-label="Insert Code">
+        aria-label="Insert Code"
+      >
         <i className="format code" />
       </button>
       <button
         onClick={insertLink}
         className={'popup-item spaced ' + (isLink ? 'active' : '')}
-        aria-label="Insert Link">
+        aria-label="Insert Link"
+      >
         <i className="format link" />
       </button>
     </div>

commit 20d98c8ffd3d45867638927723564ee81f4a9d84

fix the cursor missing caused a typo in pr #1551

packages/lexical-react/src/shared/useYjsCollaboration.jsx
@@ -212,7 +212,7 @@ export function useYjsFocusTracking(
       editor.registerCommand(
         FOCUS_COMMAND,
         (payload) => {
-          setLocalStateFocus(provider, name, color, false);
+          setLocalStateFocus(provider, name, color, true);
           return true;
         },
         EditorPriority,
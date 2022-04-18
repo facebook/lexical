c08a5585a134b4ec8d235854a94ca0a69e90499b
Zach Wily <zach@zwily.com>
2022-04-18 11:08:24 -0600

    Fix collab selections possibly doubling up when spanning text nodes

--- packages/lexical-yjs/src/SyncCursors.js
+++ packages/lexical-yjs/src/SyncCursors.js
@@ -285,6 +285,26 @@ function updateCursor(
 
   for (let i = 0; i < selectionRectsLength; i++) {
     const selectionRect = selectionRects[i];
+
+    // Exclude a rect that is the exact same as the last rect. getClientRects() can return
+    // the same rect twice for some elements. A more sophisticated thing to do here is to
+    // merge all the rects together into a set of rects that don't overlap, so we don't
+    // generate backgrounds that are too dark.
+    if (i > 0) {
+      const prevRect = selectionRects[i - 1];
+      if (
+        prevRect.top === selectionRect.top &&
+        prevRect.left === selectionRect.left &&
+        prevRect.width === selectionRect.width &&
+        prevRect.height === selectionRect.height &&
+        prevRect.height === selectionRect.height
+      ) {
+        selectionRects.splice(i--, 1);
+        selectionRectsLength--;
+        continue;
+      }
+    }
+
     if (selectionRect.width + rootPadding === rootRect.width) {
       // Exclude selections that span the entire element
       selectionRects.splice(i--, 1);

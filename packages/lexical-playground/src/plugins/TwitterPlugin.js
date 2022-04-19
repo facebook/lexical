9e8a127b86136aed3e23f65b5dad8647c9d82bc4
John Flockton <thegreatercurve@users.noreply.github.com>
2022-04-19 15:43:22 +0100

    Fix bug with alignment for root level decorator nodes v2 (#1867)
    * wip
    
    * Add __format to  node state of YouTubeNode and TweetNode

--- packages/lexical-playground/src/plugins/TwitterPlugin.js
+++ packages/lexical-playground/src/plugins/TwitterPlugin.js
@@ -11,15 +11,27 @@ import type {LexicalCommand} from 'lexical';
 
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {
+  $getNearestBlockElementAncestorOrThrow,
+  mergeRegister,
+} from '@lexical/utils';
+import {
   $createParagraphNode,
   $getSelection,
+  $isNodeSelection,
   $isRangeSelection,
   COMMAND_PRIORITY_EDITOR,
+  COMMAND_PRIORITY_LOW,
   createCommand,
+  FORMAT_ELEMENT_COMMAND,
 } from 'lexical';
 import {useEffect} from 'react';
 
-import {$createTweetNode, TweetNode} from '../nodes/TweetNode.jsx';
+import {
+  $createTweetNode,
+  $isTweetNode,
+  TweetNode,
+} from '../nodes/TweetNode.jsx';
+import {$isYouTubeNode} from '../nodes/YouTubeNode.jsx';
 
 export const INSERT_TWEET_COMMAND: LexicalCommand<string> = createCommand();
 
@@ -31,26 +43,48 @@ export default function TwitterPlugin(): React$Node {
       throw new Error('TwitterPlugin: TweetNode not registered on editor');
     }
 
-    return editor.registerCommand(
-      INSERT_TWEET_COMMAND,
-      (payload) => {
-        const selection = $getSelection();
-        if ($isRangeSelection(selection)) {
-          const focusNode = selection.focus.getNode();
-          if (focusNode !== null) {
-            const tweetNode = $createTweetNode(payload);
-            selection.focus
-              .getNode()
-              .getTopLevelElementOrThrow()
-              .insertAfter(tweetNode);
-            const paragraphNode = $createParagraphNode();
-            tweetNode.insertAfter(paragraphNode);
-            paragraphNode.select();
+    return mergeRegister(
+      editor.registerCommand(
+        INSERT_TWEET_COMMAND,
+        (payload) => {
+          const selection = $getSelection();
+          if ($isRangeSelection(selection)) {
+            const focusNode = selection.focus.getNode();
+            if (focusNode !== null) {
+              const tweetNode = $createTweetNode(payload);
+              selection.focus
+                .getNode()
+                .getTopLevelElementOrThrow()
+                .insertAfter(tweetNode);
+              const paragraphNode = $createParagraphNode();
+              tweetNode.insertAfter(paragraphNode);
+              paragraphNode.select();
+            }
           }
-        }
-        return true;
-      },
-      COMMAND_PRIORITY_EDITOR,
+          return true;
+        },
+        COMMAND_PRIORITY_EDITOR,
+      ),
+      editor.registerCommand(
+        FORMAT_ELEMENT_COMMAND,
+        (format) => {
+          const selection = $getSelection();
+          if (!$isRangeSelection(selection) && !$isNodeSelection(selection)) {
+            return false;
+          }
+          const nodes = selection.getNodes();
+          for (const node of nodes) {
+            if ($isTweetNode(node) || $isYouTubeNode(node)) {
+              node.setFormat(format);
+            } else {
+              const element = $getNearestBlockElementAncestorOrThrow(node);
+              element.setFormat(format);
+            }
+          }
+          return true;
+        },
+        COMMAND_PRIORITY_LOW,
+      ),
     );
   }, [editor]);
   return null;

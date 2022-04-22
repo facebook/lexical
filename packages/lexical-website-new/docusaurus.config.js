2f0e5b2b96784d56123d840e2ccc62970a0d7690
harish-sethuraman <harish.sethuraman@freshworks.com>
2022-04-22 21:47:16 +0530

    Update docusaurus.config.js

--- packages/lexical-website-new/docusaurus.config.js
+++ packages/lexical-website-new/docusaurus.config.js
@@ -60,24 +60,24 @@ const config = {
     ({
       algolia: {
         // Public API key: it is safe to commit it
-        apiKey: 'YOUR_SEARCH_API_KEY',
+        apiKey: '00b99bc61a623e1abd819b1d655da918',
 
         // The application ID provided by Algolia
-        appId: 'YOUR_APP_ID',
+        appId: 'YRGKJK6OMH',
 
         // Optional: see doc section below
         contextualSearch: true,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'external\\.com|domain\\.com',
+        // externalUrlRegex: 'external\\.com|domain\\.com',
 
-        indexName: 'YOUR_INDEX_NAME',
+        indexName: 'lexical',
 
         // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
+        // searchPagePath: 'search',
 
         // Optional: Algolia search parameters
-        searchParameters: {},
+        // searchParameters: {},
 
         //... other Algolia params
       },

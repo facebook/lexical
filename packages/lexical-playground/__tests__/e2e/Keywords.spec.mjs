bfba0949404d9bbb3c625950da9477e84807f627
Dominic Gannaway <dg@domgan.com>
2022-04-22 11:20:39 +0100

    Fix test

--- packages/lexical-playground/__tests__/e2e/Keywords.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/Keywords.spec.mjs
@@ -320,21 +320,12 @@ test.describe('Keywords', () => {
         </p>
       `,
     );
-    if (browserName === 'firefox') {
-      await assertSelection(page, {
-        anchorOffset: 8,
-        anchorPath: [0, 0, 0],
-        focusOffset: 8,
-        focusPath: [0, 0, 0],
-      });
-    } else {
-      await assertSelection(page, {
-        anchorOffset: 0,
-        anchorPath: [0, 1, 0],
-        focusOffset: 0,
-        focusPath: [0, 1, 0],
-      });
-    }
+    await assertSelection(page, {
+      anchorOffset: 8,
+      anchorPath: [0, 0, 0],
+      focusOffset: 8,
+      focusPath: [0, 0, 0],
+    });
 
     await page.keyboard.press('Space');
 

cdb55157c601ad4a88c68cf13eddb7b4e873b74f
John Flockton <thegreatercurve@users.noreply.github.com>
2022-05-07 13:02:20 +0100

    Migrate playground to TypeScript (#2092)
    * Migrate playground to typescript
    
    * Remove new line
    
    * remove eslint ignore rules
    
    * Fix failing integrity lint rules
    
    * Fix more CI linting errors
    
    * Committed in error

---
+++ packages/lexical-playground/src/nodes/EmojiNode.tsx
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import type {EditorConfig, LexicalNode, NodeKey} from 'lexical';
+
+import {TextNode} from 'lexical';
+
+export class EmojiNode extends TextNode {
+  __className: string;
+
+  static getType(): string {
+    return 'emoji';
+  }
+
+  static clone(node: EmojiNode): EmojiNode {
+    return new EmojiNode(node.__className, node.__text, node.__key);
+  }
+
+  constructor(className: string, text: string, key: void | NodeKey) {
+    super(text, key);
+    this.__className = className;
+  }
+
+  createDOM(config: EditorConfig): HTMLElement {
+    const dom = document.createElement('span');
+    const inner = super.createDOM(config);
+    dom.className = this.__className;
+    inner.className = 'emoji-inner';
+    dom.appendChild(inner);
+    return dom;
+  }
+
+  updateDOM(
+    prevNode: TextNode,
+    dom: HTMLElement,
+    config: EditorConfig,
+  ): boolean {
+    // $FlowFixMe: this will always be an element or null
+    const inner: null | HTMLElement = dom.firstChild;
+    if (inner === null) {
+      return true;
+    }
+    super.updateDOM(prevNode, inner, config);
+    return false;
+  }
+}
+
+export function $isEmojiNode(node: LexicalNode | null): node is EmojiNode {
+  return node instanceof EmojiNode;
+}
+
+export function $createEmojiNode(
+  className: string,
+  emojiText: string,
+): EmojiNode {
+  return new EmojiNode(className, emojiText).setMode('token');
+}
---
+++ packages/lexical-playground/src/plugins/ImagesPlugin.ts
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import type {LexicalCommand} from 'lexical';
+
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import {
+  $getSelection,
+  $isRangeSelection,
+  $isRootNode,
+  COMMAND_PRIORITY_EDITOR,
+  createCommand,
+} from 'lexical';
+import {useEffect} from 'react';
+
+import {$createImageNode, ImageNode} from '../nodes/ImageNode';
+
+export type InsertImagePayload = Readonly<{
+  altText: string;
+  src: string;
+}>;
+export const INSERT_IMAGE_COMMAND: LexicalCommand<InsertImagePayload> =
+  createCommand();
+export default function ImagesPlugin(): JSX.Element {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    if (!editor.hasNodes([ImageNode])) {
+      throw new Error('ImagesPlugin: ImageNode not registered on editor');
+    }
+
+    return editor.registerCommand(
+      INSERT_IMAGE_COMMAND,
+      (payload) => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+          if ($isRootNode(selection.anchor.getNode())) {
+            selection.insertParagraph();
+          }
+
+          const imageNode = $createImageNode(payload.src, payload.altText, 500);
+          selection.insertNodes([imageNode]);
+        }
+
+        return true;
+      },
+      COMMAND_PRIORITY_EDITOR,
+    );
+  }, [editor]);
+
+  return null;
+}

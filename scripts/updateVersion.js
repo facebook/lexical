7d85d7770f8fc3b11cb783cf54871051d85c57a3
fantactuka <fantactuka@gmail.com>
2022-05-09 19:26:55 -0400

    Add missing references for headless package

--- scripts/updateVersion.js
+++ scripts/updateVersion.js
@@ -15,6 +15,7 @@ const packages = {
   '@lexical/dragon': 'lexical-dragon',
   '@lexical/file': 'lexical-file',
   '@lexical/hashtag': 'lexical-hashtag',
+  '@lexical/headless': 'lexical-headless',
   '@lexical/history': 'lexical-history',
   '@lexical/link': 'lexical-link',
   '@lexical/list': 'lexical-list',

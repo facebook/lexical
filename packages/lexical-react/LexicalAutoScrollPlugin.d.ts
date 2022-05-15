596f50bc9f15dbfc24adff5d9471c8388e09a8e5
John Flockton <thegreatercurve@users.noreply.github.com>
2022-05-15 19:36:57 +0100

    Remove double headers

--- packages/lexical-react/LexicalAutoScrollPlugin.d.ts
+++ packages/lexical-react/LexicalAutoScrollPlugin.d.ts
@@ -6,14 +6,6 @@
  *
  */
 
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 type Props = Readonly<{
   scrollRef: {current: HTMLElement | null};
 }>;

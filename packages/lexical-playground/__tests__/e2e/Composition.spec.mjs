c943356970753f3ff92008731554c629b55fbad3
Dominic Gannaway <dg@domgan.com>
2022-05-17 20:48:15 +0100

    Revert files

--- packages/lexical-playground/__tests__/e2e/Composition.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/Composition.spec.mjs
@@ -175,7 +175,7 @@ test.describe('Composition', () => {
   });
 
   test.describe('IME', () => {
-    test.only('Can type Hiragana via IME', async ({page, browserName}) => {
+    test('Can type Hiragana via IME', async ({page, browserName}) => {
       // We don't yet support FF.
       test.skip(browserName === 'firefox');
 

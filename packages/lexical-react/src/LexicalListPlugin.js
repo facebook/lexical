89409e56e6fb5af1f369edebd3667be7a37f45ab
acywatson <acy.watson@gmail.com>
2022-05-17 12:05:36 -0700

    add runtime check for list node and list item node

--- packages/lexical-react/src/LexicalListPlugin.js
+++ packages/lexical-react/src/LexicalListPlugin.js
@@ -7,12 +7,23 @@
  * @flow strict
  */
 
+import {ListItemNode,ListNode} from '@lexical/list';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import {useEffect} from 'react';
 
 import {useList} from './shared/useList';
 
 export function ListPlugin(): null {
   const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    if (!editor.hasNodes([ListNode, ListItemNode])) {
+      throw new Error(
+        'ListPlugin: ListNode and/or ListItemNode not registered on editor',
+      );
+    }
+  }, [editor]);
+
   useList(editor);
 
   return null;

b7ce7df746e551a12cb97fbc48a40c26e0de1f3c
Ben Ray <bray@fb.com>
2022-05-19 12:39:05 -0700

    Fixing Placeholder Bug found on Web

--- packages/lexical-react/src/shared/useCanShowPlaceholder.js
+++ packages/lexical-react/src/shared/useCanShowPlaceholder.js
@@ -22,7 +22,7 @@ export function useCanShowPlaceholder(editor: LexicalEditor): boolean {
 
   useLayoutEffect(() => {
     const isComposing = editor.isComposing();
-    const currentCanShowPlaceholder = editorState.getEditorState().read(
+    const currentCanShowPlaceholder = editor.getEditorState().read(
       $canShowPlaceholderCurry(isComposing),
     );
     setCanShowPlaceholder(currentCanShowPlaceholder);

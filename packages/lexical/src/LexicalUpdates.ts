6e5d354ac5ff0bfcdaf4986286534682de7befa7
Dominic Gannaway <dg@domgan.com>
2022-05-23 13:08:19 +0100

    Fix cyles

--- packages/lexical/src/LexicalUpdates.ts
+++ packages/lexical/src/LexicalUpdates.ts
@@ -334,11 +334,9 @@ export function parseEditorState(
   const previousActiveEditor = activeEditor;
   const previousDirtyElements = editor._dirtyElements;
   const previousDirtyLeaves = editor._dirtyLeaves;
-  const previousNormalizedNodes = editor._normalizedNodes;
   const previousCloneNotNeeded = editor._cloneNotNeeded;
   editor._dirtyElements = new Map();
   editor._dirtyLeaves = new Set();
-  editor._normalizedNodes = new Set();
   editor._cloneNotNeeded = new Set();
   activeEditorState = editorState;
   isReadOnlyMode = false;
@@ -362,7 +360,6 @@ export function parseEditorState(
   } finally {
     editor._dirtyElements = previousDirtyElements;
     editor._dirtyLeaves = previousDirtyLeaves;
-    editor._normalizedNodes = previousNormalizedNodes;
     editor._cloneNotNeeded = previousCloneNotNeeded;
     activeEditorState = previousActiveEditorState;
     isReadOnlyMode = previousReadOnlyMode;

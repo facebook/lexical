8815b1a22536deb22688ad5c11273c51a33919c4
Dominic Gannaway <dg@domgan.com>
2022-05-26 16:38:51 +0100

    Add comment

--- packages/lexical-react/src/LexicalAutoFocusPlugin.ts
+++ packages/lexical-react/src/LexicalAutoFocusPlugin.ts
@@ -14,9 +14,18 @@ export function AutoFocusPlugin(): null {
 
   useEffect(() => {
     editor.focus(() => {
+      // If we try and move selection to the same point with setBaseAndExtent, it won't
+      // trigger a re-focus on the element. So in the case this occurs, we'll need to correct it.
+      // Normally this is fine, Selection API !== Focus API, but fore the intents of the naming
+      // of this plugin, which should preserve focus too.
       const activeElement = document.activeElement;
       const rootElement = editor.getRootElement() as HTMLDivElement;
-      if (rootElement !== null && !rootElement.contains(activeElement)) {
+      if (
+        rootElement !== null ||
+        activeElement === null ||
+        !rootElement.contains(activeElement)
+      ) {
+        // Note: preventScroll won't work in Webkit.
         rootElement.focus({preventScroll: true});
       }
     });

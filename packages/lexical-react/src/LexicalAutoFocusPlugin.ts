2f78eeb41daa8ee4a60c47107ff67953bcfeb8df
Dominic Gannaway <trueadm@users.noreply.github.com>
2022-05-26 18:59:25 +0100

    Improve scroll plugin (#2282)
    * Expose $getUpdateTags and $addUpdateTag
    
    * Improve scroll plugin
    
    * Add comment

--- packages/lexical-react/src/LexicalAutoFocusPlugin.ts
+++ packages/lexical-react/src/LexicalAutoFocusPlugin.ts
@@ -13,7 +13,22 @@ export function AutoFocusPlugin(): null {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
-    editor.focus();
+    editor.focus(() => {
+      // If we try and move selection to the same point with setBaseAndExtent, it won't
+      // trigger a re-focus on the element. So in the case this occurs, we'll need to correct it.
+      // Normally this is fine, Selection API !== Focus API, but fore the intents of the naming
+      // of this plugin, which should preserve focus too.
+      const activeElement = document.activeElement;
+      const rootElement = editor.getRootElement() as HTMLDivElement;
+      if (
+        rootElement !== null ||
+        activeElement === null ||
+        !rootElement.contains(activeElement)
+      ) {
+        // Note: preventScroll won't work in Webkit.
+        rootElement.focus({preventScroll: true});
+      }
+    });
   }, [editor]);
 
   return null;

23bfa4953f5203908df0c464d3e3f437da390523
Dominic Gannaway <dg@domgan.com>
2022-05-26 16:23:41 +0100

    Improve scroll plugin

--- packages/lexical-react/src/LexicalAutoFocusPlugin.ts
+++ packages/lexical-react/src/LexicalAutoFocusPlugin.ts
@@ -13,7 +13,13 @@ export function AutoFocusPlugin(): null {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
-    editor.focus();
+    editor.focus(() => {
+      const activeElement = document.activeElement;
+      const rootElement = editor.getRootElement() as HTMLDivElement;
+      if (rootElement !== null && !rootElement.contains(activeElement)) {
+        rootElement.focus({preventScroll: true});
+      }
+    });
   }, [editor]);
 
   return null;

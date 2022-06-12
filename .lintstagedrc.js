f54491cf7b34be3b2b63f6cc8af82da6ad366305
Maksim Horbachevsky <fantactuka@gmail.com>
2022-06-12 13:23:58 -0400

    Update .lintstagedrc.js
    Co-authored-by: John Flockton <thegreatercurve@users.noreply.github.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,4 @@
 module.exports = {
   '*.(js|mjs|jsx|css|html|d.ts|js.flow|ts|tsx)': 'prettier --write',
-  '*.(js|mjs|jsx)': ['flow focus-check', 'eslint --fix'],
-  '*.(ts|tsx)': ['eslint --fix'],
+  '*.(js|mjs|jsx|ts|tsx)': ['eslint --fix'],
 };

eda3df9ba61060bbcf595103aac38438cd6b969b
fantactuka <fantactuka@gmail.com>
2022-06-12 12:35:39 -0400

    Prevent dispatching redundant can undo/redo commands

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-  '*.(js|mjs|jsx|css|html|d.ts|js.flow)': 'prettier --write',
+  '*.(js|mjs|jsx|css|html|d.ts|js.flow|ts|tsx)': 'prettier --write',
   '*.(js|mjs|jsx)': ['flow focus-check', 'eslint --fix'],
+  '*.(ts|tsx)': ['eslint --fix'],
 };

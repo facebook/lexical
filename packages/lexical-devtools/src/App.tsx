73ba9caece04bc7138110478e3c89c2bb2d3dcf2
Will <will.gutierrez@gmail.com>
2022-06-13 02:08:35 -0700

    Create DevTools Browser Extension Scaffold (#2388)
    * create browser extension scaffold
    
    * pass linter
    
    * correct favicon URL
    
    * changes to enable npm build script
    
    * delete package-lock.json to pass CI? 🤷
    
    * add build instructions to README
    
    * inherit from base tsconfig.json
    
    Co-authored-by: John Flockton <thegreatercurve@users.noreply.github.com>
    
    * add missing comma
    
    Co-authored-by: John Flockton <thegreatercurve@users.noreply.github.com>

---
+++ packages/lexical-devtools/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+import React from 'react';
+
+import logo from './apple-touch-icon.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Lexical Developer Tools</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

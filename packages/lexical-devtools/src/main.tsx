96ff7541cb60f9280b0793abc7a069d5480959b8
John Flockton <thegreatercurve@users.noreply.github.com>
2022-06-15 19:01:02 +0100

    Fix small issues with Vite

--- packages/lexical-devtools/src/main.tsx
+++ packages/lexical-devtools/src/main.tsx
@@ -13,10 +13,7 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,

89762af972f5a7c205a8225838c119723be6b9ea
John Flockton <thegreatercurve@users.noreply.github.com>
2022-06-16 14:18:11 +0100

    Keep precommit checks

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.(js|mjs|jsx|css|html|d.ts|js.flow)': 'prettier --write',
-  '*.(js|mjs|jsx)': ['flow focus-check', 'eslint --fix'],
+  '*.(js|mjs|jsx|css|html|d.ts|js.flow|ts|tsx)': 'prettier --write',
+  '*.(js|mjs|jsx|ts|tsx)': ['eslint --fix'],
 };

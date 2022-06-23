eeaa79272d90eecf4ce7b04a65d77c5fb4d556ac
John Flockton <thegreatercurve@users.noreply.github.com>
2022-06-23 14:26:54 +0100

    ChangeType should be COMPOSING_CHARACTER when beforeInput is fired

--- packages/lexical-playground/__tests__/e2e/History.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/History.spec.mjs
@@ -10,6 +10,7 @@ import {moveLeft, redo, toggleBold, undo} from '../keyboardShortcuts/index.mjs';
 import {
   assertHTML,
   assertSelection,
+  enableCompositionKeyEvents,
   focusEditor,
   html,
   initialize,
@@ -555,5 +556,124 @@ test.describe('History', () => {
     await assertHTML(page, step2HTML);
     await redo(page);
     await assertHTML(page, step1HTML);
+  });
+
+  test('Can undo composed Hirigana via IME after composition ends (#2479)', async ({
+    page,
+    browserName,
+  }) => {
+    // We don't yet support FF.
+    test.skip(browserName === 'firefox');
+
+    await focusEditor(page);
+    await enableCompositionKeyEvents(page);
+
+    await page.keyboard.imeSetComposition('ｓ', 1, 1);
+    await page.keyboard.imeSetComposition('す', 1, 1);
+    await page.keyboard.imeSetComposition('すｓ', 2, 2);
+    await page.keyboard.imeSetComposition('すｓｈ', 3, 3);
+    await page.keyboard.imeSetComposition('すし', 2, 2);
+    await page.keyboard.insertText('すし');
+    await page.keyboard.type(' ');
+    await page.keyboard.imeSetComposition('m', 1, 1);
+    await page.keyboard.imeSetComposition('も', 1, 1);
+    await page.keyboard.imeSetComposition('もj', 2, 2);
+    await page.keyboard.imeSetComposition('もじ', 2, 2);
+    await page.keyboard.imeSetComposition('もじあ', 3, 3);
+    await page.keyboard.insertText('もじあ');
+
+    await assertHTML(
+      page,
+      html`
+        <p
+          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+          dir="ltr">
+          <span data-lexical-text="true">すし もじあ</span>
+        </p>
+      `,
+    );
+
+    await assertSelection(page, {
+      anchorOffset: 6,
+      anchorPath: [0, 0, 0],
+      focusOffset: 6,
+      focusPath: [0, 0, 0],
+    });
+
+    await undo(page);
+
+    await assertHTML(
+      page,
+      html`
+        <p
+          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+          dir="ltr">
+          <span data-lexical-text="true">すし</span>
+        </p>
+      `,
+    );
+
+    await assertSelection(page, {
+      anchorOffset: 3,
+      anchorPath: [0, 0, 0],
+      focusOffset: 3,
+      focusPath: [0, 0, 0],
+    });
+
+    await undo(page);
+
+    await assertHTML(
+      page,
+      html`
+        <p
+          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+          dir="ltr">
+          <span data-lexical-text="true">すし</span>
+        </p>
+      `,
+    );
+
+    await assertSelection(page, {
+      anchorOffset: 2,
+      anchorPath: [0, 0, 0],
+      focusOffset: 2,
+      focusPath: [0, 0, 0],
+    });
+
+    await undo(page);
+
+    await assertHTML(
+      page,
+      html`
+        <p class="PlaygroundEditorTheme__paragraph"><br /></p>
+      `,
+    );
+
+    await assertSelection(page, {
+      anchorOffset: 0,
+      anchorPath: [0],
+      focusOffset: 0,
+      focusPath: [0],
+    });
+
+    await redo(page);
+
+    await assertHTML(
+      page,
+      html`
+        <p
+          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+          dir="ltr">
+          <span data-lexical-text="true">すし</span>
+        </p>
+      `,
+    );
+
+    await assertSelection(page, {
+      anchorOffset: 2,
+      anchorPath: [0, 0, 0],
+      focusOffset: 2,
+      focusPath: [0, 0, 0],
+    });
   });
 });

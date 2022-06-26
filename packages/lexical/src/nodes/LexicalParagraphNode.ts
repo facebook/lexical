0beef618d5bdb450b85ff8b09d2a9b1f29e4543e
Dominic Gannaway <dg@domgan.com>
2022-06-26 20:07:27 +0100

    Address feedback

--- packages/lexical/src/nodes/LexicalParagraphNode.ts
+++ packages/lexical/src/nodes/LexicalParagraphNode.ts
@@ -64,10 +64,8 @@ export class ParagraphNode extends ElementNode {
   exportDOM(editor: LexicalEditor): DOMExportOutput {
     const {element} = super.exportDOM(editor);
 
-    if (element) {
-      if (this.isEmpty()) {
-        element.append(document.createElement('br'));
-      }
+    if (element && this.isEmpty()) {
+      element.append(document.createElement('br'));
     }
 
     return {

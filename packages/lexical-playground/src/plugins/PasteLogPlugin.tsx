0eb41eda132bda0b30fa9f9bd1142c2e131a004b
acywatson <acy.watson@gmail.com>
2022-07-07 01:35:54 -0700

    fix

--- packages/lexical-playground/src/plugins/PasteLogPlugin.tsx
+++ packages/lexical-playground/src/plugins/PasteLogPlugin.tsx
@@ -9,7 +9,7 @@
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {COMMAND_PRIORITY_NORMAL, PASTE_COMMAND} from 'lexical';
 import * as React from 'react';
-import {useState} from 'react';
+import {useEffect,useState} from 'react';
 
 export default function PasteLogPlugin(): JSX.Element {
   const [editor] = useLexicalComposerContext();
@@ -17,21 +17,25 @@ export default function PasteLogPlugin(): JSX.Element {
   const [lastClipboardData, setLastClipboardData] = useState<string | null>(
     null,
   );
-  editor.registerCommand(
-    PASTE_COMMAND,
-    (e: ClipboardEvent) => {
-      const {clipboardData} = e;
-      const allData: string[] = [];
-      if (clipboardData) {
-        clipboardData.types.forEach((type) => {
-          allData.push(type.toUpperCase(), clipboardData.getData(type));
-        });
-      }
-      setLastClipboardData(allData.join('\n\n'));
-      return false;
-    },
-    COMMAND_PRIORITY_NORMAL,
-  );
+  useEffect(() => {
+    if (isActive) {
+      return editor.registerCommand(
+        PASTE_COMMAND,
+        (e: ClipboardEvent) => {
+          const {clipboardData} = e;
+          const allData: string[] = [];
+          if (clipboardData && clipboardData.types) {
+            clipboardData.types.forEach((type) => {
+              allData.push(type.toUpperCase(), clipboardData.getData(type));
+            });
+          }
+          setLastClipboardData(allData.join('\n\n'));
+          return false;
+        },
+        COMMAND_PRIORITY_NORMAL,
+      );
+    }
+  }, [editor, isActive]);
   return (
     <>
       <button

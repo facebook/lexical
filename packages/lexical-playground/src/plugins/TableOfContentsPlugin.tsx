7876b2668d3e02e1fb67152d29b14acbc7835fca
Karam Qaoud <kqaoud@fb.com>
2022-07-10 00:15:50 +0100

    Table of contents updates automtaically on scroll without observing all heading nodes

--- packages/lexical-playground/src/plugins/TableOfContentsPlugin.tsx
+++ packages/lexical-playground/src/plugins/TableOfContentsPlugin.tsx
@@ -6,66 +6,42 @@
  *
  */
 import type {HeadingTagType} from '@lexical/rich-text';
-import type {LexicalEditor, NodeKey} from 'lexical';
+import type {NodeKey} from 'lexical';
 
 import '../ui/TableOfContentsStyle.css';
 
 import LexicalTableOfContents__EXPERIMENTAL from '@lexical/react/LexicalTableOfContents__EXPERIMENTAL';
-import {$getNearestNodeFromDOMNode} from 'lexical';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import * as React from 'react';
+
+import {useLexicalComposerContext} from '../../../lexical-react/src/LexicalComposerContext';
 
 function TableOfContentsList({
   tableOfContents,
-  editor,
 }: {
   tableOfContents: Array<[key: NodeKey, text: string, tag: HeadingTagType]>;
-  editor: LexicalEditor;
 }): JSX.Element {
-  const [selectedKey, setSelectedKey] = useState(tableOfContents[0][0]);
-
+  const [selectedKey, setSelectedKey] = useState('');
+  const selectedIndex = useRef(0);
+  const [editor] = useLexicalComposerContext();
+  let timerId: ReturnType<typeof setTimeout>;
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting === true) {
-            editor.update(() => {
-              const node = $getNearestNodeFromDOMNode(entry.target);
-              if (node !== null) {
-                setSelectedKey(node.getKey());
-              }
-            });
-          }
-        });
-      },
-      {rootMargin: '0px -10px 0px 0px'},
-    );
+    if (tableOfContents.length !== 0 && selectedKey === '') {
+      scrollToNode(tableOfContents[0][0], 0);
+    }
+  });
 
-    tableOfContents.map((entry) => {
-      const headingNode = editor.getElementByKey(entry[0]);
-      if (headingNode !== null) {
-        observer.observe(headingNode);
-      }
-    });
-
-    return () => {
-      tableOfContents.map((entry) => {
-        const element = document.getElementById(entry[0]);
-        if (element !== null) {
-          observer.unobserve(element);
-        }
-      });
-    };
-  }, [editor, tableOfContents]);
-  function scrollToNode(key: NodeKey) {
+  function scrollToNode(key: NodeKey, currIndex: number) {
     editor.getEditorState().read(() => {
       const domElement = editor.getElementByKey(key);
       if (domElement !== null) {
         domElement.scrollIntoView();
         setSelectedKey(key);
+        selectedIndex.current = currIndex;
       }
     });
   }
+
   function indent(tagName: HeadingTagType) {
     if (tagName === 'h2') {
       return 'heading2';
@@ -73,13 +49,73 @@ function TableOfContentsList({
       return 'heading3';
     }
   }
+  let lastScrollTop = 0;
+
+  function debounceFunction(func: () => void, delay: number) {
+    clearTimeout(timerId);
+    timerId = setTimeout(func, delay);
+  }
+
+  function isElementOnScreen(element: HTMLElement) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  function scrollCallback() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    //scrolling up
+    if (st > lastScrollTop) {
+      if (selectedIndex.current < tableOfContents.length - 1) {
+        const currHeading = editor.getElementByKey(
+          tableOfContents[selectedIndex.current][0],
+        );
+        if (currHeading !== null) {
+          const elementOnScreen = isElementOnScreen(currHeading);
+          if (elementOnScreen === false) {
+            const nextKey = tableOfContents[selectedIndex.current + 1][0];
+            selectedIndex.current++;
+            setSelectedKey(nextKey);
+          }
+        }
+      }
+    } else {
+      // scrolling down
+      if (
+        selectedIndex.current > 0 &&
+        selectedIndex.current < tableOfContents.length - 1
+      ) {
+        const prevHeading = editor.getElementByKey(
+          tableOfContents[selectedIndex.current - 1][0],
+        );
+        if (prevHeading !== null) {
+          const elementOnScreen = isElementOnScreen(prevHeading);
+          if (elementOnScreen === true) {
+            const prevKey = tableOfContents[selectedIndex.current - 1][0];
+            setSelectedKey(prevKey);
+            selectedIndex.current--;
+          }
+        }
+      }
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  }
+  function onScroll(): void {
+    debounceFunction(scrollCallback, 10);
+  }
+  document.addEventListener('scroll', onScroll);
+
   return (
     <ul className="remove-ul-style">
-      {tableOfContents.map(([key, text, tag]) => (
+      {tableOfContents.map(([key, text, tag], index) => (
         <div
           className={selectedKey === key ? 'selectedHeading' : 'heading'}
           key={key}
-          onClick={() => scrollToNode(key)}
+          onClick={() => scrollToNode(key, index)}
           role="button"
           tabIndex={0}>
           <div className={selectedKey === key ? 'circle' : 'bar'} />
@@ -95,13 +131,8 @@ function TableOfContentsList({
 export default function TableOfContentsPlugin() {
   return (
     <LexicalTableOfContents__EXPERIMENTAL>
-      {(tableOfContents, editor) => {
-        return (
-          <TableOfContentsList
-            tableOfContents={tableOfContents}
-            editor={editor}
-          />
-        );
+      {(tableOfContents) => {
+        return <TableOfContentsList tableOfContents={tableOfContents} />;
       }}
     </LexicalTableOfContents__EXPERIMENTAL>
   );

4982b9dc23ce320102cb918384e59a2afbee398c
Tyler Bainbridge <tbainbridge@fb.com>
2022-07-25 19:20:44 -0400

    Figma config

--- packages/lexical-playground/src/plugins/FigmaPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/FigmaPlugin/index.tsx
@@ -11,7 +11,7 @@ import {$insertBlockNode} from '@lexical/utils';
 import {COMMAND_PRIORITY_EDITOR, createCommand, LexicalCommand} from 'lexical';
 import {useEffect} from 'react';
 
-import {$createFigmaNode, FigmaNode} from '../nodes/FigmaNode';
+import {$createFigmaNode, FigmaNode} from '../../nodes/FigmaNode';
 
 export const INSERT_FIGMA_COMMAND: LexicalCommand<string> = createCommand();
 

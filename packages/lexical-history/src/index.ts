1b561cab0582e7070e3ec236267f439a7db0f012
tylerjbainbridge <tbainbridge@fb.com>
2022-08-03 18:33:50 -0400

    Fix tests

--- packages/lexical-history/src/index.ts
+++ packages/lexical-history/src/index.ts
@@ -333,7 +333,7 @@ function redo(editor: LexicalEditor, historyState: HistoryState): void {
 
     if (historyStateEntry) {
       historyStateEntry.editor.setEditorState(historyStateEntry.editorState, {
-        tag: 'history-redo',
+        tag: 'historic',
       });
     }
   }

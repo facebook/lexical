a17df8f407ccc2c5cbaf56b349084968bdf2a5c7
Acy Watson <acy@fb.com>
2022-08-23 15:07:59 -0600

    back to original solution

--- packages/lexical-history/src/index.ts
+++ packages/lexical-history/src/index.ts
@@ -259,13 +259,10 @@ function createMergeActionGetter(
         currentHistoryEntry === null || currentHistoryEntry.editor === editor;
       const shouldPushHistory = tags.has('history-push');
       const shouldMergeHistory =
-        !shouldPushHistory && tags.has('history-merge');
+        !shouldPushHistory && isSameEditor && tags.has('history-merge');
 
       if (shouldMergeHistory) {
-        if (isSameEditor) {
-          return HISTORY_MERGE;
-        }
-        return DISCARD_HISTORY_CANDIDATE;
+        return HISTORY_MERGE;
       }
 
       if (prevEditorState === null) {

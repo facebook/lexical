ded1206f9ecde589159cb8dfcaa735846d0c7f0e
Acy Watson <acy@fb.com>
2022-09-08 12:57:01 -0600

    reorder keys

--- packages/lexical-markdown/src/MarkdownTransformers.ts
+++ packages/lexical-markdown/src/MarkdownTransformers.ts
@@ -174,18 +174,6 @@ export const HEADING: ElementTransformer = {
 
 export const QUOTE: ElementTransformer = {
   dependencies: [QuoteNode],
-  export: (node, exportChildren) => {
-    if (!$isQuoteNode(node)) {
-      return null;
-    }
-
-    const lines = exportChildren(node).split('\n');
-    const output = [];
-    for (const line of lines) {
-      output.push('> ' + line);
-    }
-    return output.join('\n');
-  },
   regExp: /^>\s/,
   replace: (parentNode, children, _match, isImport) => {
     if (isImport) {
@@ -205,6 +193,19 @@ export const QUOTE: ElementTransformer = {
     node.append(...children);
     parentNode.replace(node);
     node.select(0, 0);
+  },
+  // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+  export: (node, exportChildren) => {
+    if (!$isQuoteNode(node)) {
+      return null;
+    }
+
+    const lines = exportChildren(node).split('\n');
+    const output = [];
+    for (const line of lines) {
+      output.push('> ' + line);
+    }
+    return output.join('\n');
   },
   type: 'element',
 };

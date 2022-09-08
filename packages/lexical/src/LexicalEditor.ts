ebdfe9bee2073a06f9ca751c54d5916a505fc587
Dominic Gannaway <dg@domgan.com>
2022-09-08 15:01:45 +0100

    Revise

--- packages/lexical/src/LexicalEditor.ts
+++ packages/lexical/src/LexicalEditor.ts
@@ -523,7 +523,9 @@ export class LexicalEditor {
 
     this._onError = onError;
     this._htmlConversions = htmlConversions;
-    this._editable = editable;
+    // We don't actually make use of the `editable` argument above.
+    // Doing so, causes e2e tests around the lock to fail.
+    this._editable = true;
     this._headless = false;
     this._window = null;
   }

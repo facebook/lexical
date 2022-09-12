6f736699b0d0890396e6ce5f95fcdb46df6d0107
fantactuka <fantactuka@gmail.com>
2022-09-12 15:06:07 -0400

    Rm unused helpers from older markdown code

--- packages/lexical-text/src/index.ts
+++ packages/lexical-text/src/index.ts
@@ -7,13 +7,7 @@
  *
  */
 
-import type {
-  ElementNode,
-  Klass,
-  LexicalEditor,
-  LexicalNode,
-  RootNode,
-} from 'lexical';
+import type {Klass, LexicalEditor, LexicalNode, RootNode} from 'lexical';
 
 import {
   $createTextNode,
@@ -22,7 +16,6 @@ import {
   $isTextNode,
   TextNode,
 } from 'lexical';
-import invariant from 'shared/invariant';
 
 export type TextNodeWithOffset = {
   node: TextNode;
@@ -69,111 +62,6 @@ export function $findTextIntersectionFromCharacters(
       parent = parent.getParent();
     }
     break;
-  }
-
-  return null;
-}
-
-// Return text content for child text nodes.  Each non-text node is separated by input string.
-// Caution, this function creates a string and should not be used within a tight loop.
-// Use $getNodeWithOffsetsFromJoinedTextNodesFromElementNode below to convert
-// indexes in the return string back into their corresponding node and offsets.
-export function $joinTextNodesInElementNode(
-  elementNode: ElementNode,
-  separator: string,
-  stopAt: TextNodeWithOffset,
-): string {
-  let textContent = '';
-  const children = elementNode.getChildren();
-  const length = children.length;
-
-  for (let i = 0; i < length; ++i) {
-    const child = children[i];
-
-    if ($isTextNode(child)) {
-      const childTextContent = child.getTextContent();
-
-      if (child.is(stopAt.node)) {
-        if (stopAt.offset > childTextContent.length) {
-          invariant(
-            false,
-            'Node %s and selection point do not match.',
-            child.__key,
-          );
-        }
-        textContent += child.getTextContent().substr(0, stopAt.offset);
-        break;
-      } else {
-        textContent += childTextContent;
-      }
-    } else {
-      textContent += separator;
-    }
-  }
-
-  return textContent;
-}
-
-// This function converts the offsetInJoinedText to
-// a node and offset result or null if not found.
-// This function is to be used in conjunction with joinTextNodesInElementNode above.
-// The joinedTextContent should be return value from joinTextNodesInElementNode.
-//
-// The offsetInJoinedText is relative to the entire string which
-// itself is relevant to the parent ElementNode.
-//
-// Example:
-// Given a Paragraph with 2 TextNodes. The first is Hello, the second is World.
-// The joinedTextContent would be "HelloWorld"
-// The offsetInJoinedText might be for the letter "e" = 1 or "r" = 7.
-// The return values would be {TextNode1, 1} or {TextNode2,2}, respectively.
-
-export function $findNodeWithOffsetFromJoinedText(
-  offsetInJoinedText: number,
-  joinedTextLength: number,
-  separatorLength: number,
-  elementNode: ElementNode,
-): TextNodeWithOffset | null {
-  const children = elementNode.getChildren();
-  const childrenLength = children.length;
-  let runningLength = 0;
-  let isPriorNodeTextNode = false;
-
-  for (let i = 0; i < childrenLength; ++i) {
-    // We must examine the offsetInJoinedText that is located
-    // at the length of the string.
-    // For example, given "hello", the length is 5, yet
-    // the caller still wants the node + offset at the
-    // right edge of the "o".
-
-    if (runningLength > joinedTextLength) {
-      break;
-    }
-
-    const child = children[i];
-    const isChildNodeTestNode = $isTextNode(child);
-    const childContentLength = isChildNodeTestNode
-      ? child.getTextContent().length
-      : separatorLength;
-
-    const newRunningLength = runningLength + childContentLength;
-
-    const isJoinedOffsetWithinNode =
-      (isPriorNodeTextNode === false && runningLength === offsetInJoinedText) ||
-      (runningLength === 0 && runningLength === offsetInJoinedText) ||
-      (runningLength < offsetInJoinedText &&
-        offsetInJoinedText <= newRunningLength);
-
-    if (isJoinedOffsetWithinNode && $isTextNode(child)) {
-      // Check isTextNode again for flow.
-
-      return {
-        node: child,
-        offset: offsetInJoinedText - runningLength,
-      };
-    }
-    runningLength = newRunningLength;
-    isPriorNodeTextNode = isChildNodeTestNode;
   }
 
   return null;

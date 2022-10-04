847fcc7ae272901c948afb6c1334d5943f7b05e7
Maksim Horbachevsky <fantactuka@gmail.com>
2022-10-03 22:41:22 -0400

    Fix DOM availability check (#3102)

--- packages/lexical-html/src/index.ts
+++ packages/lexical-html/src/index.ts
@@ -56,7 +56,7 @@ export function $generateHtmlFromNodes(
   editor: LexicalEditor,
   selection?: RangeSelection | NodeSelection | GridSelection | null,
 ): string {
-  if (document == null || window == null) {
+  if (typeof document === 'undefined' || typeof window === 'undefined') {
     throw new Error(
       'To use $generateHtmlFromNodes in headless mode please initialize a headless browser implementation such as JSDom before calling this function.',
     );

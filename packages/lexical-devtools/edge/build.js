4a8dde897c70643285bcccd4af2480f341f11eeb
Adithya Vardhan <imadithyavardhan@gmail.com>
2022-10-10 18:02:03 +0530

    feat: add icons to extension and change build (#3140)
    * feat: add icons to extension and change build
    
    * fix: prettier
    
    * fix: remove clipboard write permissions
    
    * fix: add header
    
    * fix: remove old icons
    
    * fix: script names

---
+++ packages/lexical-devtools/edge/build.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+'use strict';
+
+const chalk = require('chalk');
+const build = require('../build');
+
+const main = async () => {
+  await build('edge');
+
+  // eslint-disable-next-line no-console
+  console.log(chalk.green('\nThe Edge extension has been built!'));
+};
+
+main();

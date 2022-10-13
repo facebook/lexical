7bf34c9d1e73bb9cbabccff07d628fe5919e17af
Gerard Rovira <gerardrovira@meta.com>
2022-10-13 18:30:22 -0400

    Add Error/Suspense boundaries to React DecoratorNodes

--- packages/lexical-react/src/LexicalRichTextPlugin.tsx
+++ packages/lexical-react/src/LexicalRichTextPlugin.tsx
@@ -9,20 +9,29 @@
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import * as React from 'react';
 
+import {ErrorBoundary as ReactErrorBoundary} from './shared/ReactErrorBoundary';
 import {useCanShowPlaceholder} from './shared/useCanShowPlaceholder';
-import {useDecorators} from './shared/useDecorators';
+import {ErrorBoundaryType, useDecorators} from './shared/useDecorators';
 import {useRichTextSetup} from './shared/useRichTextSetup';
 
 export function RichTextPlugin({
   contentEditable,
   placeholder,
+  // TODO 0.6 Make non-optional non-default
+  ErrorBoundary = ({children, onError}) => (
+    <ReactErrorBoundary fallback={null} onError={onError}>
+      {children}
+    </ReactErrorBoundary>
+  ),
 }: Readonly<{
   contentEditable: JSX.Element;
   placeholder: JSX.Element | string;
+  // TODO 0.6 Make non-optional non-default
+  ErrorBoundary?: ErrorBoundaryType;
 }>): JSX.Element {
   const [editor] = useLexicalComposerContext();
   const showPlaceholder = useCanShowPlaceholder(editor);
-  const decorators = useDecorators(editor);
+  const decorators = useDecorators(editor, ErrorBoundary);
   useRichTextSetup(editor);
 
   return (

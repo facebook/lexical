ac41a74ce17d8e421c12c664e18ab91ecf7fd448
Akmarzhan Abylay <akmarzhan@Akmarzhans-MacBook-Pro.local>
2022-10-25 12:46:04 -0700

    remove demos from sidebar

--- packages/lexical-website/docusaurus.config.js
+++ packages/lexical-website/docusaurus.config.js
@@ -181,12 +181,6 @@ const config = {
             sidebarId: 'api',
             type: 'docSidebar',
           },
-          {
-            label: 'Demos',
-            position: 'left',
-            sidebarId: 'demos',
-            type: 'docSidebar',
-          },
           {label: 'Community', position: 'left', to: '/community'},
           {
             href: GITHUB_REPO_URL,

c4a30b9ccbb4e19672c4968c9180597ccb43fb04
Acy Watson <acy@fb.com>
2022-10-26 08:22:21 -0600

    fix nodesOfType return type

--- packages/lexical/src/LexicalUtils.ts
+++ packages/lexical/src/LexicalUtils.ts
@@ -1026,21 +1026,19 @@ export function setMutatedNode(
   }
 }
 
-export function $nodesOfType<T extends LexicalNode>(
-  klass: Klass<T>,
-): Array<LexicalNode> {
+export function $nodesOfType<T extends LexicalNode>(klass: Klass<T>): Array<T> {
   const editorState = getActiveEditorState();
   const readOnly = editorState._readOnly;
   const klassType = klass.getType();
   const nodes = editorState._nodeMap;
-  const nodesOfType = [];
+  const nodesOfType: Array<T> = [];
   for (const [, node] of nodes) {
     if (
       node instanceof klass &&
       node.__type === klassType &&
       (readOnly || node.isAttached())
     ) {
-      nodesOfType.push(node);
+      nodesOfType.push(node as T);
     }
   }
   return nodesOfType;

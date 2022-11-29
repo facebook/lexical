21d697488d83992705d4da488b80c1f50d152f0d
Rajatava Mukherjee <49876019+Rajatava@users.noreply.github.com>
2022-11-29 23:42:12 +0530

    Fix superscript & subscript not being converted from html string (#3453)

--- packages/lexical/src/nodes/LexicalTextNode.ts
+++ packages/lexical/src/nodes/LexicalTextNode.ts
@@ -460,6 +460,14 @@ export class TextNode extends LexicalNode {
         conversion: convertTextFormatElement,
         priority: 0,
       }),
+      sub: (node: Node) => ({
+        conversion: convertTextFormatElement,
+        priority: 0,
+      }),
+      sup: (node: Node) => ({
+        conversion: convertTextFormatElement,
+        priority: 0,
+      }),
       u: (node: Node) => ({
         conversion: convertTextFormatElement,
         priority: 0,
@@ -895,6 +903,8 @@ const nodeNameToTextFormat: Record<string, TextFormatType> = {
   em: 'italic',
   i: 'italic',
   strong: 'bold',
+  sub: 'subscript',
+  sup: 'superscript',
   u: 'underline',
 };
 function convertTextFormatElement(domNode: Node): DOMConversionOutput {

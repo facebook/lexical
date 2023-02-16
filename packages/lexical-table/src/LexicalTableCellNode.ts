f63f012b343d0e5254936230bf65c9babea549ea
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-02-16 11:59:54 +0100

    fix: change tablecell return type for TableCellNode

--- packages/lexical-table/src/LexicalTableCellNode.ts
+++ packages/lexical-table/src/LexicalTableCellNode.ts
@@ -53,7 +53,7 @@ export class TableCellNode extends DEPRECATED_GridCellNode {
   /** @internal */
   __width?: number;
 
-  static getType(): 'tablecell' {
+  static getType(): string {
     return 'tablecell';
   }
 

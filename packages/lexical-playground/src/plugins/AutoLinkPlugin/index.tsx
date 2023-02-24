3aacfc0f7e9595cd0a6199ebc9e350fe6b7ecfc6
Karibash <y.karibayashi@gmail.com>
2023-02-24 19:37:47 +0900

    Make AutoLinkPlugin matchers export

--- packages/lexical-playground/src/plugins/AutoLinkPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/AutoLinkPlugin/index.tsx
@@ -6,41 +6,14 @@
  *
  */
 
-import {AutoLinkPlugin} from '@lexical/react/LexicalAutoLinkPlugin';
+import {
+  AutoLinkPlugin,
+  emailLinkMatcher,
+  urlLinkMatcher,
+} from '@lexical/react/LexicalAutoLinkPlugin';
 import * as React from 'react';
 
-const URL_MATCHER =
-  /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-
-const EMAIL_MATCHER =
-  /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
-
-const MATCHERS = [
-  (text: string) => {
-    const match = URL_MATCHER.exec(text);
-    if (match === null) {
-      return null;
-    }
-    const fullMatch = match[0];
-    return {
-      index: match.index,
-      length: fullMatch.length,
-      text: fullMatch,
-      url: fullMatch.startsWith('http') ? fullMatch : `https://${fullMatch}`,
-    };
-  },
-  (text: string) => {
-    const match = EMAIL_MATCHER.exec(text);
-    return (
-      match && {
-        index: match.index,
-        length: match[0].length,
-        text: match[0],
-        url: `mailto:${match[0]}`,
-      }
-    );
-  },
-];
+const MATCHERS = [urlLinkMatcher, emailLinkMatcher];
 
 export default function LexicalAutoLinkPlugin(): JSX.Element {
   return <AutoLinkPlugin matchers={MATCHERS} />;

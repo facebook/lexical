5c1466c4e38d5339cf08d68b1dfccee6776b65a2
wangqi <donotlb@gmail.com>
2023-03-26 09:40:07 +0800

    minor tweak

--- packages/lexical-react/src/LexicalAutoEmbedPlugin.tsx
+++ packages/lexical-react/src/LexicalAutoEmbedPlugin.tsx
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-// eslint-disable-next-line simple-import-sort/imports
 import type {LexicalNode, MutationListener} from 'lexical';
 
 import {$isLinkNode, AutoLinkNode, LinkNode} from '@lexical/link';

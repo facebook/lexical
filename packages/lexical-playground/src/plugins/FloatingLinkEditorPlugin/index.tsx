13e5f5f9130942ff039145c1c68f0e946b70de32
Alessio Gravili <alessio@gravili.de>
2023-03-27 08:22:02 +0200

    remove useless autolink check

--- packages/lexical-playground/src/plugins/FloatingLinkEditorPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/FloatingLinkEditorPlugin/index.tsx
@@ -7,7 +7,7 @@
  */
 import './index.css';
 
-import {$isAutoLinkNode, $isLinkNode, TOGGLE_LINK_COMMAND} from '@lexical/link';
+import {$isLinkNode, TOGGLE_LINK_COMMAND} from '@lexical/link';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {$findMatchingParent, mergeRegister} from '@lexical/utils';
 import {
@@ -255,9 +255,8 @@ function useFloatingLinkEditorToolbar(
     if ($isRangeSelection(selection)) {
       const node = getSelectedNode(selection);
       const linkParent = $findMatchingParent(node, $isLinkNode);
-      const autoLinkParent = $findMatchingParent(node, $isAutoLinkNode);
 
-      if (linkParent != null || autoLinkParent != null) {
+      if (linkParent != null) {
         setIsLink(true);
       } else {
         setIsLink(false);

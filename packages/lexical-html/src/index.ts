8a295c4d823ca61950d5fbf2e3eeff9be0369b6b
Acy Watson <acy@fb.com>
2023-04-02 22:30:27 -0600

    allow overriding html serialization via editor config

--- packages/lexical-html/src/index.ts
+++ packages/lexical-html/src/index.ts
@@ -92,7 +92,15 @@ function $appendNodesToHTML(
     target = clone;
   }
   const children = $isElementNode(target) ? target.getChildren() : [];
-  const {element, after} = target.exportDOM(editor);
+  const registeredNode = editor._nodes.get(target.getType());
+  let exportOutput;
+  if (registeredNode && registeredNode.exportDOM !== undefined) {
+    exportOutput = registeredNode.exportDOM(editor, target);
+  } else {
+    exportOutput = target.exportDOM(editor);
+  }
+
+  const {element, after} = exportOutput;
 
   if (!element) {
     return false;
--- packages/lexical/src/LexicalEditor.ts
+++ packages/lexical/src/LexicalEditor.ts
@@ -166,7 +166,7 @@ export type CreateEditorArgs = {
   editable?: boolean;
   theme?: EditorThemeClasses;
   html?: {
-    export?: Map<Klass<LexicalNode>, DOMExportOutput>;
+    export?: Map<Klass<LexicalNode>, () => DOMExportOutput>;
     import?: DOMConversionMap;
   };
 };
@@ -178,6 +178,10 @@ export type RegisteredNode = {
   transforms: Set<Transform<LexicalNode>>;
   replace: null | ((node: LexicalNode) => LexicalNode);
   replaceWithKlass: null | Klass<LexicalNode>;
+  exportDOM?: (
+    editor: LexicalEditor,
+    targetNode: LexicalNode,
+  ) => DOMExportOutput;
 };
 
 export type Transform<T extends LexicalNode> = (node: T) => void;

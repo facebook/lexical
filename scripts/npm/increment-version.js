e95a832d6dded61f18ae3fc1137e80d3c7710af2
Acy Watson <acy@fb.com>
2023-04-07 19:54:57 -0600

    try to automate releases!

--- scripts/npm/increment-version.js
+++ scripts/npm/increment-version.js
@@ -11,19 +11,28 @@
 'use strict';
 
 const {exec} = require('child-process-promise');
+const fs = require('fs-extra');
 const argv = require('minimist')(process.argv.slice(2));
 const increment = argv.i;
+const baseVersion = argv.base;
 const validIncrements = new Set(['minor', 'patch', 'prerelease']);
 if (!validIncrements.has(increment)) {
   console.error(`Invalid value for increment: ${increment}`);
   process.exit(1);
 }
 
-async function incrementVersion(increment) {
+async function incrementVersion(increment, base) {
+  if (base !== undefined) {
+    const basePackageJSON = fs.readJsonSync(`./package.json`);
+    basePackageJSON.version = base;
+    fs.writeJsonSync(`./package.json`, basePackageJSON, {
+      spaces: 2,
+    });
+  }
   const preId = increment === 'prerelease' ? '--preid next' : '';
   const workspaces = '';
   const command = `npm --no-git-tag-version version ${increment} --include-workspace-root true ${preId} ${workspaces}`;
   await exec(command);
 }
 
-incrementVersion(increment);
+incrementVersion(increment, baseVersion);

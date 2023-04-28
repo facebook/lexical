23050c5d01b4bd1e464b1ad039e29e9f29bc22e5
zignis <velociraptorm77@gmail.com>
2023-04-28 12:51:54 +0530

    Fix typo

--- packages/lexical-react/src/LexicalClickableLinkPlugin.tsx
+++ packages/lexical-react/src/LexicalClickableLinkPlugin.tsx
@@ -79,7 +79,7 @@ export default function LexicalClickableLinkPlugin({
         return;
       }
 
-      // Allow user to select link text without follwing url
+      // Allow user to select link text without following url
       const selection = editor.getEditorState().read($getSelection);
       if ($isRangeSelection(selection) && !selection.isCollapsed()) {
         event.preventDefault();

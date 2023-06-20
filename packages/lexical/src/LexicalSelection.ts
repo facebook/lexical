c628aa41981e635123633a0cca4361ff21ee441f
John Flockton <johnflockton@fb.com>
2023-06-20 13:52:26 +0100

    Fix small error

--- packages/lexical/src/LexicalSelection.ts
+++ packages/lexical/src/LexicalSelection.ts
@@ -2230,7 +2230,7 @@ function moveNativeSelection(
   direction: 'backward' | 'forward' | 'left' | 'right',
   granularity: 'character' | 'word' | 'lineboundary',
 ): void {
-  // @ts-expect-error Selection.modify() method applies a change to the current selection or cursor position,
+  // Selection.modify() method applies a change to the current selection or cursor position,
   // but is still non-standard in some browsers.
   domSelection.modify(alter, direction, granularity);
 }

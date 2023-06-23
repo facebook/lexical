07e46524f52743ba2cf79d77279ff483b60004ce
Steve Larson <9larsons@gmail.com>
2023-06-23 13:57:10 -0500

    update logic for pasting

--- packages/lexical/src/LexicalSelection.ts
+++ packages/lexical/src/LexicalSelection.ts
@@ -1550,7 +1550,7 @@ export class RangeSelection implements BaseSelection {
       if ($isElementNode(target) && !target.isInline()) {
         lastNode = node;
         if ($isDecoratorNode(node) && !node.isInline()) {
-          if (target.canBeEmpty() && target.isEmpty()) {
+          if (nodes.length === 1 && target.canBeEmpty() && target.isEmpty()) {
             target = target.insertBefore(node, false);
           } else {
             target = target.insertAfter(node, false);

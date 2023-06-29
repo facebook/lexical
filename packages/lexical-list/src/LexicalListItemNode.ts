2540304b13db788e24c271e5b773c9cf89a97c0e
kazukinagata <KNagata8@gmail.com>
2023-06-29 05:49:34 +0000

    Used createListItemNode within importJSON

--- packages/lexical-list/src/LexicalListItemNode.ts
+++ packages/lexical-list/src/LexicalListItemNode.ts
@@ -124,7 +124,9 @@ export class ListItemNode extends ElementNode {
   }
 
   static importJSON(serializedNode: SerializedListItemNode): ListItemNode {
-    const node = new ListItemNode(serializedNode.value, serializedNode.checked);
+    const node = $createListItemNode();
+    node.setChecked(serializedNode.checked);
+    node.setValue(serializedNode.value);
     node.setFormat(serializedNode.format);
     node.setDirection(serializedNode.direction);
     return node;

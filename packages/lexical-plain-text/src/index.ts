13c8617ccc9273143b2c4ae26f1fe3234f279653
ssrivasta196 <ssrivasta196@bloomberg.net>
2023-07-03 09:49:52 +0100

    ci-check error fix

--- packages/lexical-plain-text/src/index.ts
+++ packages/lexical-plain-text/src/index.ts
@@ -52,19 +52,21 @@ function onCopyForPlainText(
   editor: LexicalEditor,
 ): void {
   editor.update(() => {
-    const clipboardData =
-      event instanceof KeyboardEvent ? null : event.clipboardData;
-    const selection = $getSelection();
+    if (event !== null) {
+      const clipboardData =
+        event instanceof KeyboardEvent ? null : event.clipboardData;
+      const selection = $getSelection();
 
-    if (selection !== null && clipboardData != null) {
-      event.preventDefault();
-      const htmlString = $getHtmlContent(editor);
+      if (selection !== null && clipboardData != null) {
+        event.preventDefault();
+        const htmlString = $getHtmlContent(editor);
 
-      if (htmlString !== null) {
-        clipboardData.setData('text/html', htmlString);
+        if (htmlString !== null) {
+          clipboardData.setData('text/html', htmlString);
+        }
+
+        clipboardData.setData('text/plain', selection.getTextContent());
       }
-
-      clipboardData.setData('text/plain', selection.getTextContent());
     }
   });
 }

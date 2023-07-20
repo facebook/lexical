bf0b2eaa9ecaa60fa6f70c8847ad08e355a74b69
Ivan Zvonar <ivanzvonar10@gmail.com>
2023-07-20 04:33:10 +0200

    Fix selection issue in registerLexicalTextEntity

--- packages/lexical-text/src/index.ts
+++ packages/lexical-text/src/index.ts
@@ -251,8 +251,8 @@ export function registerLexicalTextEntity<T extends TextNode>(
           if (diff > 0) {
             const concatText = text.slice(0, diff);
             const newTextContent = previousText + concatText;
-            prevSibling.select();
             prevSibling.setTextContent(newTextContent);
+            prevSibling.select();
 
             if (diff === text.length) {
               node.remove();

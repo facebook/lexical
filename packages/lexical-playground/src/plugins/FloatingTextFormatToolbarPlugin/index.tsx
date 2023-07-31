af9b94c3e51988b2704605737965a84ac6d5a25f
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-07-31 17:38:35 +0200

    fix(text-format-toolbar): don't hide toolbar if current node is a paragraph

--- packages/lexical-playground/src/plugins/FloatingTextFormatToolbarPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/FloatingTextFormatToolbarPlugin/index.tsx
@@ -14,6 +14,7 @@ import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {mergeRegister} from '@lexical/utils';
 import {
   $getSelection,
+  $isParagraphNode,
   $isRangeSelection,
   $isTextNode,
   COMMAND_PRIORITY_LOW,
@@ -322,7 +323,7 @@ function useFloatingTextFormatToolbar(
         !$isCodeHighlightNode(selection.anchor.getNode()) &&
         selection.getTextContent() !== ''
       ) {
-        setIsText($isTextNode(node));
+        setIsText($isTextNode(node) || $isParagraphNode(node));
       } else {
         setIsText(false);
       }

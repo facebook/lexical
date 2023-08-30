5098a49d4b5653378170445fcbffb3c0758e9e1c
zignis <velociraptorm77@gmail.com>
2023-08-30 09:31:55 +0530

    impl forwardRef

--- packages/lexical-react/src/LexicalContentEditable.tsx
+++ packages/lexical-react/src/LexicalContentEditable.tsx
@@ -8,7 +8,7 @@
 
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import * as React from 'react';
-import {useCallback, useState} from 'react';
+import {useCallback, useImperativeHandle, useRef,useState} from 'react';
 import useLayoutEffect from 'shared/useLayoutEffect';
 
 export type Props = {
@@ -26,74 +26,83 @@ export type Props = {
   'data-testid'?: string | null | undefined;
 } & React.AllHTMLAttributes<HTMLDivElement>;
 
-export function ContentEditable({
-  ariaActiveDescendant,
-  ariaAutoComplete,
-  ariaControls,
-  ariaDescribedBy,
-  ariaExpanded,
-  ariaLabel,
-  ariaLabelledBy,
-  ariaMultiline,
-  ariaOwns,
-  ariaRequired,
-  autoCapitalize,
-  className,
-  id,
-  role = 'textbox',
-  spellCheck = true,
-  style,
-  tabIndex,
-  'data-testid': testid,
-  ...rest
-}: Props): JSX.Element {
-  const [editor] = useLexicalComposerContext();
-  const [isEditable, setEditable] = useState(false);
-
-  const ref = useCallback(
-    (rootElement: null | HTMLElement) => {
-      editor.setRootElement(rootElement);
+export const ContentEditable = React.forwardRef<HTMLDivElement, Props>(
+  function ContentEditable(
+    {
+      ariaActiveDescendant,
+      ariaAutoComplete,
+      ariaControls,
+      ariaDescribedBy,
+      ariaExpanded,
+      ariaLabel,
+      ariaLabelledBy,
+      ariaMultiline,
+      ariaOwns,
+      ariaRequired,
+      autoCapitalize,
+      className,
+      id,
+      role = 'textbox',
+      spellCheck = true,
+      style,
+      tabIndex,
+      'data-testid': testid,
+      ...rest
     },
-    [editor],
-  );
+    refProp,
+  ): JSX.Element {
+    const contentEditableRef = useRef<HTMLDivElement | null>(null);
+    const [editor] = useLexicalComposerContext();
+    const [isEditable, setEditable] = useState(false);
 
-  useLayoutEffect(() => {
-    setEditable(editor.isEditable());
-    return editor.registerEditableListener((currentIsEditable) => {
-      setEditable(currentIsEditable);
-    });
-  }, [editor]);
+    const ref = useCallback(
+      (rootElement: null | HTMLDivElement) => {
+        contentEditableRef.current = rootElement;
+        editor.setRootElement(rootElement);
+      },
+      [editor],
+    );
 
-  return (
-    <div
-      {...rest}
-      aria-activedescendant={!isEditable ? undefined : ariaActiveDescendant}
-      aria-autocomplete={!isEditable ? 'none' : ariaAutoComplete}
-      aria-controls={!isEditable ? undefined : ariaControls}
-      aria-describedby={ariaDescribedBy}
-      aria-expanded={
-        !isEditable
-          ? undefined
-          : role === 'combobox'
-          ? !!ariaExpanded
-          : undefined
-      }
-      aria-label={ariaLabel}
-      aria-labelledby={ariaLabelledBy}
-      aria-multiline={ariaMultiline}
-      aria-owns={!isEditable ? undefined : ariaOwns}
-      aria-readonly={!isEditable ? true : undefined}
-      aria-required={ariaRequired}
-      autoCapitalize={autoCapitalize}
-      className={className}
-      contentEditable={isEditable}
-      data-testid={testid}
-      id={id}
-      ref={ref}
-      role={role}
-      spellCheck={spellCheck}
-      style={style}
-      tabIndex={tabIndex}
-    />
-  );
-}
+    useLayoutEffect(() => {
+      setEditable(editor.isEditable());
+      return editor.registerEditableListener((currentIsEditable) => {
+        setEditable(currentIsEditable);
+      });
+    }, [editor]);
+
+    useImperativeHandle(refProp, () => contentEditableRef.current!);
+
+    return (
+      <div
+        {...rest}
+        aria-activedescendant={!isEditable ? undefined : ariaActiveDescendant}
+        aria-autocomplete={!isEditable ? 'none' : ariaAutoComplete}
+        aria-controls={!isEditable ? undefined : ariaControls}
+        aria-describedby={ariaDescribedBy}
+        aria-expanded={
+          !isEditable
+            ? undefined
+            : role === 'combobox'
+            ? !!ariaExpanded
+            : undefined
+        }
+        aria-label={ariaLabel}
+        aria-labelledby={ariaLabelledBy}
+        aria-multiline={ariaMultiline}
+        aria-owns={!isEditable ? undefined : ariaOwns}
+        aria-readonly={!isEditable ? true : undefined}
+        aria-required={ariaRequired}
+        autoCapitalize={autoCapitalize}
+        className={className}
+        contentEditable={isEditable}
+        data-testid={testid}
+        id={id}
+        ref={ref}
+        role={role}
+        spellCheck={spellCheck}
+        style={style}
+        tabIndex={tabIndex}
+      />
+    );
+  },
+);

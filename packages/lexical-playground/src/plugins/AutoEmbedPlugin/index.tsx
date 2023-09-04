d85522653e3b7e17522637f7bc511bada5ce3f85
Ivaylo Pavlov <ivailo90@gmail.com>
2023-09-04 15:37:53 +0100

    Revert "Fix: fixes regex in YouTube AutoEmbedPlugin (#4916)"
    This reverts commit 0f4211532431b17eb157270a36e88fa1ec095896.

--- packages/lexical-playground/src/plugins/AutoEmbedPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/AutoEmbedPlugin/index.tsx
@@ -61,9 +61,7 @@ export const YoutubeEmbedConfig: PlaygroundEmbedConfig = {
   // Determine if a given URL is a match and return url data.
   parseUrl: async (url: string) => {
     const match =
-      / ^.*(?:youtu(?:\.be|be\.com))\/(v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/.exec(
-        url,
-      );
+      /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/.exec(url);
 
     const id = match ? (match?.[2].length === 11 ? match[2] : null) : null;
 

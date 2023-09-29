38e52fcd179d73090456deef0edb293a27954496
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2023-09-29 07:02:21 -0400

    fix prettier

--- packages/lexical-react/src/LexicalTypeaheadMenuPlugin.tsx
+++ packages/lexical-react/src/LexicalTypeaheadMenuPlugin.tsx
@@ -45,7 +45,11 @@ function getTextUpToAnchor(selection: RangeSelection): string | null {
   return anchorNode.getTextContent().slice(0, anchorOffset);
 }
 
-function tryToPositionRange(leadOffset: number, range: Range, editorWindow: Window): boolean {
+function tryToPositionRange(
+  leadOffset: number,
+  range: Range,
+  editorWindow: Window,
+): boolean {
   const domSelection = editorWindow.getSelection();
   if (domSelection === null || !domSelection.isCollapsed) {
     return false;
@@ -258,7 +262,11 @@ export function LexicalTypeaheadMenuPlugin<TOption extends MenuOption>({
           match !== null &&
           !isSelectionOnEntityBoundary(editor, match.leadOffset)
         ) {
-          const isRangePositioned = tryToPositionRange(match.leadOffset, range, editorWindow);
+          const isRangePositioned = tryToPositionRange(
+            match.leadOffset,
+            range,
+            editorWindow,
+          );
           if (isRangePositioned !== null) {
             startTransition(() =>
               openTypeahead({

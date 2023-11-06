d1b204ab5375f9e612aad7e467b0ec3530ca80d6
Tahir Shakir <tahir60652@gmail.com>
2023-11-06 14:25:30 +0500

    Remove comments

--- packages/lexical-rich-text/src/index.ts
+++ packages/lexical-rich-text/src/index.ts
@@ -638,8 +638,6 @@ export function registerRichText(editor: LexicalEditor): () => void {
     editor.registerCommand<ElementFormatType>(
       FORMAT_ELEMENT_COMMAND,
       (format) => {
-        // Register command listener here
-
         const selection = $getSelection();
         if (!$isRangeSelection(selection) && !$isNodeSelection(selection)) {
           return false;

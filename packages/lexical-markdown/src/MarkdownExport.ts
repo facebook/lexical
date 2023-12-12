4890613c7912c93c920483b2c99aa09a5fca7d98
wnhlee <2wheeh@gmail.com>
2023-12-13 01:56:11 +0900

    Fix exportTextFormat to keep dollar sign

--- packages/lexical-markdown/src/MarkdownExport.ts
+++ packages/lexical-markdown/src/MarkdownExport.ts
@@ -165,7 +165,9 @@ function exportTextFormat(
   }
 
   // Replace trimmed version of textContent ensuring surrounding whitespace is not modified
-  return textContent.replace(frozenString, output);
+  return textContent.replace(frozenString, function () {
+    return output;
+  });
 }
 
 // Get next or previous text sibling a text node, including cases

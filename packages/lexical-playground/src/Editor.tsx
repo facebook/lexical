32ec9d30a4e903a8649d907948b5667561f29ef9
wnhlee <2wheeh@gmail.com>
2024-01-07 16:47:54 +0900

    revert floating one

--- packages/lexical-playground/src/Editor.tsx
+++ packages/lexical-playground/src/Editor.tsx
@@ -215,7 +215,6 @@ export default function Editor(): JSX.Element {
                 />
                 <FloatingTextFormatToolbarPlugin
                   anchorElem={floatingAnchorElem}
-                  setIsLinkEditMode={setIsLinkEditMode}
                 />
               </>
             )}

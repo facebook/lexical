5552e0150395e1104614422c675c454acbdbc571
Harry Sanders <harry.sandersiv@yahoo.com>
2024-01-24 00:31:28 -0500

    floating link editor now opens upon completion

--- packages/lexical-playground/src/plugins/FloatingLinkEditorPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/FloatingLinkEditorPlugin/index.tsx
@@ -267,7 +267,7 @@ function useFloatingLinkEditorToolbar(
       const autoLinkParent = $findMatchingParent(node, $isAutoLinkNode);
 
       // We don't want this menu to open for auto links.
-      if (linkParent != null && autoLinkParent == null) {
+      if (linkParent != null || autoLinkParent != null) {
         setIsLink(true);
       } else {
         setIsLink(false);

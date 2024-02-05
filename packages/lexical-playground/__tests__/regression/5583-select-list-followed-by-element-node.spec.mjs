03669f119bc906ff92563fd54036d018c7f74304
Ivaylo Pavlov <ivailo90@gmail.com>
2024-02-05 22:22:10 +0000

    Tests

--- packages/lexical-playground/__tests__/regression/5583-select-list-followed-by-element-node.spec.mjs
+++ packages/lexical-playground/__tests__/regression/5583-select-list-followed-by-element-node.spec.mjs
@@ -9,6 +9,8 @@
 import {
   click,
   dragMouse,
+  focusEditor,
+  initialize,
   mouseMoveToSelector,
   selectFromInsertDropdown,
   selectorBoundingBox,
@@ -20,40 +22,43 @@ async function toggleBulletList(page) {
   await click(page, '.block-controls');
   await click(page, '.dropdown .icon.bullet-list');
 }
-
-test(`Element node in the middle of a bullet list and selecting doesn't crash`, async ({
-  page,
-  isPlainText,
-}) => {
-  test.skip(isPlainText);
-
-  page.on('console', (msg) => {
-    if (msg.type() === 'error') {
-      if (
-        msg.text().includes('error #68') ||
-        msg.text().includes('getNodesBetween: ancestor is null')
-      ) {
-        test.fail();
-      }
-    }
-  });
-
-  await toggleBulletList(page);
-  await page.keyboard.type('one');
-  await page.keyboard.press('Enter');
-  await page.keyboard.type('two');
-  await page.keyboard.press('Enter');
-  await selectFromInsertDropdown(page, '.horizontal-rule');
-  await waitForSelector(page, 'hr');
-  await page.keyboard.type('three');
-  await page.keyboard.press('Enter');
-  await mouseMoveToSelector(page, 'li:has-text("one")');
-  await page.mouse.down();
-  await dragMouse(
+test.describe('Regression test #5251', () => {
+  test.beforeEach(({isCollab, page}) => initialize({isCollab, page}));
+  test(`Element node in the middle of a bullet list and selecting doesn't crash`, async ({
     page,
-    await selectorBoundingBox(page, 'li:has-text("one")'),
-    await selectorBoundingBox(page, 'li:has-text("three")'),
-    'middle',
-    'end',
-  );
+    isPlainText,
+  }) => {
+    test.skip(isPlainText);
+    await focusEditor(page);
+
+    page.on('console', (msg) => {
+      if (msg.type() === 'error') {
+        if (
+          msg.text().includes('error #68') ||
+          msg.text().includes('getNodesBetween: ancestor is null')
+        ) {
+          test.fail();
+        }
+      }
+    });
+
+    await toggleBulletList(page);
+    await page.keyboard.type('one');
+    await page.keyboard.press('Enter');
+    await page.keyboard.type('two');
+    await page.keyboard.press('Enter');
+    await selectFromInsertDropdown(page, '.horizontal-rule');
+    await waitForSelector(page, 'hr');
+    await page.keyboard.type('three');
+    await page.keyboard.press('Enter');
+    await mouseMoveToSelector(page, 'li:has-text("one")');
+    await page.mouse.down();
+    await dragMouse(
+      page,
+      await selectorBoundingBox(page, 'li:has-text("one")'),
+      await selectorBoundingBox(page, 'li:has-text("three")'),
+      'middle',
+      'end',
+    );
+  });
 });

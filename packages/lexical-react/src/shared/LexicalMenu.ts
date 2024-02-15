9f08979844808ac34477e5a3cb3108a9f69db837
Gerard Rovira <gerardrovira@meta.com>
2024-02-15 09:08:08 -0500

    Revert "Fixed position for lexical menu (#5602)"
    This reverts commit 0ebb41307624af16cb2cdd48e9804ccb96edd32c.

--- packages/lexical-react/src/shared/LexicalMenu.ts
+++ packages/lexical-react/src/shared/LexicalMenu.ts
@@ -530,7 +530,7 @@ export function useMenuAnchorRef(
         containerDiv.setAttribute('id', 'typeahead-menu');
         containerDiv.setAttribute('role', 'listbox');
         containerDiv.style.display = 'block';
-        containerDiv.style.position = 'fixed';
+        containerDiv.style.position = 'absolute';
         parent.append(containerDiv);
       }
       anchorElementRef.current = containerDiv;

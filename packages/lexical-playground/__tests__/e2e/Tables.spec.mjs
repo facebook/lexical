392878bcdd90671dfb2d5268b9eb3ed4224a4624
Serey Roth <88986106+serey-roth@users.noreply.github.com>
2024-03-04 12:28:52 -0800

    Add support for grid selection in text alignment. (#5652)

--- packages/lexical-playground/__tests__/e2e/Tables.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/Tables.spec.mjs
@@ -36,6 +36,7 @@ import {
   SAMPLE_IMAGE_URL,
   selectCellsFromTableCords,
   selectFromAdditionalStylesDropdown,
+  selectFromAlignDropdown,
   setBackgroundColor,
   test,
   toggleColumnHeader,
@@ -2061,6 +2062,110 @@ test.describe('Tables', () => {
         </table>
         <p class="PlaygroundEditorTheme__paragraph"><br /></p>
       `,
+    );
+  });
+
+  test('Can align text using Table selection', async ({
+    page,
+    isPlainText,
+    isCollab,
+  }) => {
+    await initialize({isCollab, page});
+    test.skip(isPlainText);
+
+    await focusEditor(page);
+    await insertTable(page, 2, 3);
+
+    await fillTablePartiallyWithText(page);
+    await selectCellsFromTableCords(
+      page,
+      {x: 0, y: 0},
+      {x: 1, y: 1},
+      true,
+      false,
+    );
+
+    await selectFromAlignDropdown(page, '.center-align');
+
+    await assertHTML(
+      page,
+      html`
+        <p><br /></p>
+        <table>
+          <tr>
+            <th
+              style="background-color: rgb(172, 206, 247); caret-color: transparent; text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">a</span>
+              </p>
+            </th>
+            <th
+              style="background-color: rgb(172, 206, 247); caret-color: transparent; text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">bb</span>
+              </p>
+            </th>
+            <th>
+              <p dir="ltr"><span data-lexical-text="true">cc</span></p>
+            </th>
+          </tr>
+          <tr>
+            <th
+              style="background-color: rgb(172, 206, 247); caret-color: transparent; text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">d</span>
+              </p>
+            </th>
+            <td
+              style="background-color: rgb(172, 206, 247); caret-color: transparent; text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">e</span>
+              </p>
+            </td>
+            <td>
+              <p dir="ltr"><span data-lexical-text="true">f</span></p>
+            </td>
+          </tr>
+        </table>
+        <p><br /></p>
+      `,
+      html`
+        <p><br /></p>
+        <table>
+          <tr>
+            <th style="text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">a</span>
+              </p>
+            </th>
+            <th style="text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">bb</span>
+              </p>
+            </th>
+            <th>
+              <p dir="ltr"><span data-lexical-text="true">cc</span></p>
+            </th>
+          </tr>
+          <tr>
+            <th style="text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">d</span>
+              </p>
+            </th>
+            <td style="text-align: center">
+              <p dir="ltr" style="text-align: center">
+                <span data-lexical-text="true">e</span>
+              </p>
+            </td>
+            <td>
+              <p dir="ltr"><span data-lexical-text="true">f</span></p>
+            </td>
+          </tr>
+        </table>
+        <p><br /></p>
+      `,
+      {ignoreClasses: true},
     );
   });
 });

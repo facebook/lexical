5127acedd721941111985e7e6b92f5a61ee31d3a
wnhlee <2wheeh@gmail.com>
2024-03-16 00:09:37 +0900

    convert moduleResolution to bundler

--- packages/lexical/src/nodes/LexicalElementNode.ts
+++ packages/lexical/src/nodes/LexicalElementNode.ts
@@ -16,7 +16,7 @@ import type {KlassConstructor, Spread} from 'lexical';
 
 import invariant from 'shared/invariant';
 
-import {$isTextNode, TextNode} from '../';
+import {$isTextNode, TextNode} from '../index';
 import {
   DOUBLE_LINE_BREAK,
   ELEMENT_FORMAT_TO_TYPE,

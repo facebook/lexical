22199def06702e1ea2c74e5b60a6a376113184d9
Vladlen Fedosov <vladlenfedosov@meta.com>
2024-03-20 14:09:05 -0400

    chore(@lexical/playground): Moved Vite config to TS and removed code duplication

---
+++ packages/lexical-playground/vite.config.ts
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import babel from '@rollup/plugin-babel';
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
+import {replaceCodePlugin} from 'vite-plugin-replace';
+
+import moduleResolution from './viteModuleResolution';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: new URL('./index.html', import.meta.url).pathname,
+        split: new URL('./split/index.html', import.meta.url).pathname,
+      },
+    },
+  },
+  define: {
+    'process.env.IS_PREACT': process.env.IS_PREACT,
+  },
+  plugins: [
+    replaceCodePlugin({
+      replacements: [
+        {
+          from: /__DEV__/g,
+          to: 'true',
+        },
+      ],
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      babelrc: false,
+      configFile: false,
+      exclude: '/**/node_modules/**',
+      extensions: ['jsx', 'js', 'ts', 'tsx', 'mjs'],
+      plugins: [
+        '@babel/plugin-transform-flow-strip-types',
+        [
+          require('../../scripts/error-codes/transform-error-messages'),
+          {
+            noMinify: true,
+          },
+        ],
+      ],
+      presets: ['@babel/preset-react'],
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: moduleResolution,
+  },
+});

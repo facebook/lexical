2624d5ee878b0b50e8965113a972eafd2c23b468
Ivaylo Pavlov <ivailo90@gmail.com>
2024-03-26 22:21:21 +0000

    Test build

--- packages/lexical-playground/vite.prod.config.js
+++ packages/lexical-playground/vite.prod.config.js
@@ -204,6 +204,26 @@ export default defineConfig({
         })()),
       ],
     }),
+    commonjs({
+      include: 'node_modules/**',
+      // left-hand side can be an absolute path, a path
+      // relative to the current directory, or the name
+      // of a module in node_modules
+      namedExports: {
+        'node_modules/react/index.js': [
+          'cloneElement',
+          'createContext',
+          'Component',
+          'createElement'
+        ],
+        'node_modules/react-dom/index.js': ['render', 'hydrate'],
+        'node_modules/react-is/index.js': [
+          'isElement',
+          'isValidElementType',
+          'ForwardRef'
+        ]
+      }
+    })
   ],
   resolve: {
     alias: moduleResolution,

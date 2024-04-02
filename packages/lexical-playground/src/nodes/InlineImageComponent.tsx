f2ac5310b55f8a476466a963ec7dd5c47206958c
Ivaylo Pavlov <ivailo90@gmail.com>
2024-04-02 09:19:04 +0100

    Editable immediately when adding link from Floating toolbar (#5801)

--- packages/lexical-playground/src/nodes/InlineImageComponent.tsx
+++ packages/lexical-playground/src/nodes/InlineImageComponent.tsx
@@ -35,7 +35,6 @@ import * as React from 'react';
 import {Suspense, useCallback, useEffect, useRef, useState} from 'react';
 
 import useModal from '../hooks/useModal';
-import FloatingTextFormatToolbarPlugin from '../plugins/FloatingTextFormatToolbarPlugin/index';
 import LinkPlugin from '../plugins/LinkPlugin';
 import Button from '../ui/Button';
 import ContentEditable from '../ui/ContentEditable';
@@ -387,7 +386,6 @@ export default function InlineImageComponent({
             <LexicalNestedComposer initialEditor={caption}>
               <AutoFocusPlugin />
               <LinkPlugin />
-              <FloatingTextFormatToolbarPlugin />
               <RichTextPlugin
                 contentEditable={
                   <ContentEditable className="InlineImageNode__contentEditable" />

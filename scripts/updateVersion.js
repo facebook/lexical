55afe579ad76a5d41df28606838e475fcb24396d
Bob Ippolito <bob@redivi.com>
2024-04-10 14:45:25 -0700

    Add comments and refactor per PR review

--- scripts/updateVersion.js
+++ scripts/updateVersion.js
@@ -39,6 +39,18 @@ const packages = {
   shared: 'shared',
 };
 
+/**
+ * Update every package.json in the packages/ directory from the packages
+ * list above.
+ *
+ * - Set the version to the monorepo ./package.json version
+ * - Update the dependencies and peerDependencies
+ * - Update the exports map and set other required default fields
+ *
+ * Also scans the examples/ directory for package.json files and updates
+ * their dependencies as well, so the examples are kept up to date with
+ * the most recently published version of lexical.
+ */
 function updateVersion() {
   // get version from monorepo package.json version
   const basePackageJSON = fs.readJsonSync(`./package.json`);
@@ -64,25 +76,66 @@ function updateVersion() {
   });
 }
 
-function withExtension(fileName, ext) {
+/**
+ * Replace the extension in a .js or .mjs filename with another extension.
+ * Used to convert between the two or to add a prefix before the extension.
+ *
+ * `ext` may contain $1 to use the original extension in the
+ * replacement, e.g. replaceExtension('foo.js', '.bar$1') -> 'foo.bar.js'
+ *
+ * @param {string} fileName
+ * @param {string} ext
+ * @returns {string} fileName with ext as the new extension
+ */
+function replaceExtension(fileName, ext) {
   return fileName.replace(/(\.m?js)$/, ext);
 }
 
+/**
+ * Replace a .js filename with .mjs, this extension is required for maximal
+ * ESM compatibility because bundlers and node have hard-coded behavior for
+ * files of this extension.
+ *
+ * @param {string} fileName
+ * @returns {string} fileName with .mjs instead of .js
+ */
 function withEsmExtension(fileName) {
-  return withExtension(fileName, '.mjs');
+  return replaceExtension(fileName, '.mjs');
 }
 
+/**
+ * Used for naming node condition ESM fork modules, which can use top-level
+ * await.
+ *
+ * @param {string} fileName
+ * @returns {string} fileName with .node.mjs extension
+ */
 function withNodeEsmExtension(fileName) {
-  return withExtension(fileName, '.node.mjs');
+  return replaceExtension(fileName, '.node.mjs');
 }
 
+/**
+ * webpack can use these conditions to choose a dev or prod
+ * build without a fork module, which is especially helpful
+ * in the ESM build.
+ *
+ * @param {string} fileName may have .js or .mjs extension
+ * @returns {Record<'development'|'production', string>}
+ */
 function withEnvironments(fileName) {
   return {
-    development: withExtension(fileName, '.dev$1'),
-    production: withExtension(fileName, '.prod$1'),
+    development: replaceExtension(fileName, '.dev$1'),
+    production: replaceExtension(fileName, '.prod$1'),
   };
 }
 
+/**
+ * Build an export map for a particular entry point in the package.json
+ *
+ * @param {string} file the path to the cjs build product for an entry point (e.g. 'index.js')
+ * @param {string} types the path to the TypeScript .d.ts file
+ * @returns {Record<'import'|'require', Record<string,string>>} The export map for this file
+ */
 function exportEntry(file, types) {
   // Bundlers such as webpack require 'types' to be first and 'default' to be
   // last per #5731. Keys are in descending priority order.
@@ -103,6 +156,13 @@ function exportEntry(file, types) {
   };
 }
 
+/**
+ * Update the parsed packageJSON in-place to add default configurations
+ * for `sideEffects` and `module` as well as to maintain the `exports` map.
+ *
+ * @param {Record<string, any>} packageJSON
+ * @param {string} pkg
+ */
 function updateModule(packageJSON, pkg) {
   if (packageJSON.sideEffects === undefined) {
     packageJSON.sideEffects = false;
@@ -128,6 +188,14 @@ function updateModule(packageJSON, pkg) {
   }
 }
 
+/**
+ * Replace the dependency map at packageJSON[key] in-place with
+ * deps sorted lexically by key. If deps was empty, it will be removed.
+ *
+ * @param {Record<string, any>} packageJSON
+ * @param {'dependencies'|'peerDependencies'} key
+ * @param {Record<string, string>} deps
+ */
 function sortDependencies(packageJSON, key, deps) {
   const entries = Object.entries(deps);
   if (entries.length === 0) {
@@ -139,6 +207,14 @@ function sortDependencies(packageJSON, key, deps) {
   }
 }
 
+/**
+ * Update depdenencies and peerDependencies in packageJSON in-place.
+ * All entries for monorepo packages will be updated to version.
+ * All peerDependencies for monorepo packages will be moved to dependencies.
+ *
+ * @param {Record<string, any>} packageJSON
+ * @param {string} version
+ */
 function updateDependencies(packageJSON, version) {
   const {dependencies = {}, peerDependencies = {}} = packageJSON;
   Object.keys(dependencies).forEach((dep) => {

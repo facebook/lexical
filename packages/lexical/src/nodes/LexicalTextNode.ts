70c785e7e5d7827ce3fe495ef2e6169d0a1b5483
Gerard Rovira <zurfyx@users.noreply.github.com>
2024-04-11 01:22:48 +0100

    TextNode support font-weight bold (#5852)

--- packages/lexical/src/nodes/LexicalTextNode.ts
+++ packages/lexical/src/nodes/LexicalTextNode.ts
@@ -1090,17 +1090,18 @@ export class TextNode extends LexicalNode {
 function convertSpanElement(domNode: Node): DOMConversionOutput {
   // domNode is a <span> since we matched it by nodeName
   const span = domNode as HTMLSpanElement;
+  const style = span.style;
+  const fontWeight = style.fontWeight;
   // Google Docs uses span tags + font-weight for bold text
-  const hasBoldFontWeight = span.style.fontWeight === '700';
+  const hasBoldFontWeight = fontWeight === '700' || fontWeight === 'bold';
   // Google Docs uses span tags + text-decoration: line-through for strikethrough text
-  const hasLinethroughTextDecoration =
-    span.style.textDecoration === 'line-through';
+  const hasLinethroughTextDecoration = style.textDecoration === 'line-through';
   // Google Docs uses span tags + font-style for italic text
-  const hasItalicFontStyle = span.style.fontStyle === 'italic';
+  const hasItalicFontStyle = style.fontStyle === 'italic';
   // Google Docs uses span tags + text-decoration: underline for underline text
-  const hasUnderlineTextDecoration = span.style.textDecoration === 'underline';
+  const hasUnderlineTextDecoration = style.textDecoration === 'underline';
   // Google Docs uses span tags + vertical-align to specify subscript and superscript
-  const verticalAlign = span.style.verticalAlign;
+  const verticalAlign = style.verticalAlign;
 
   return {
     forChild: (lexicalNode) => {

9c7b4553de1626ff0d2adc03407802edfbba994c
Hansel <hansel@killswitchlabs.com>
2024-04-17 09:19:00 -0400

    add support for powershell

--- packages/lexical-code/src/CodeHighlighterPrism.ts
+++ packages/lexical-code/src/CodeHighlighterPrism.ts
@@ -17,6 +17,7 @@ import 'prismjs/components/prism-c';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-objectivec';
 import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-powershell';
 import 'prismjs/components/prism-python';
 import 'prismjs/components/prism-rust';
 import 'prismjs/components/prism-swift';

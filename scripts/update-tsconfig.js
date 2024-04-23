dba9d169fc4e4d86c819d576a2e3c51bc95b0d58
Bob Ippolito <bob@redivi.com>
2024-04-23 12:19:06 -0700

    use monorepo prettier config in update-tsconfig

--- scripts/update-tsconfig.js
+++ scripts/update-tsconfig.js
@@ -16,17 +16,24 @@ const {packagesManager} = require('./shared/packagesManager');
 
 /**
  * @typedef {Object} UpdateTsconfigOptions
- * @property {string} jsonFileName path to the tsconfig.json
- * @property {boolean} test true to include the test paths (default: false)
  * @property {Array<[string, Array<string>]>} extraPaths additional paths to add
+ * @property {string} jsonFileName path to the tsconfig.json
+ * @property {import('prettier').Options} prettierConfig the monorepo prettier config
+ * @property {boolean} test true to include the test paths (default: false)
  */
 
 /**
  * @param {opts} UpdateTsconfigOptions
+ * @returns {Promise<void>}
  */
-function updateTsconfig({jsonFileName, test = false, extraPaths = []}) {
-  const tsconfig = fs.readJsonSync(jsonFileName);
-  const prev = JSON.stringify(tsconfig.compilerOptions.paths);
+async function updateTsconfig({
+  extraPaths,
+  jsonFileName,
+  prettierConfig,
+  test,
+}) {
+  const prevTsconfigContents = fs.readFileSync(jsonFileName, 'utf8');
+  const tsconfig = JSON.parse(prevTsconfigContents);
   const publicPaths = [];
   const privatePaths = [];
   const testPaths = [];
@@ -67,26 +74,37 @@ function updateTsconfig({jsonFileName, test = false, extraPaths = []}) {
     ...privatePaths,
     ...testPaths,
   ]);
-  if (JSON.stringify(paths) !== prev) {
-    tsconfig.compilerOptions.paths = paths;
-    fs.writeFileSync(
-      jsonFileName,
-      prettier.format(JSON.stringify(tsconfig), {filepath: jsonFileName}),
-    );
+  tsconfig.compilerOptions.paths = paths;
+  // This is async in future versions of prettier
+  const nextTsconfigContents = await prettier.format(JSON.stringify(tsconfig), {
+    ...prettierConfig,
+    filepath: jsonFileName,
+  });
+  if (prevTsconfigContents !== nextTsconfigContents) {
+    fs.writeFileSync(jsonFileName, nextTsconfigContents);
   }
 }
-updateTsconfig({
-  extraPaths: [],
-  jsonFileName: './tsconfig.json',
-  test: true,
-});
-updateTsconfig({
-  extraPaths: [],
-  jsonFileName: './tsconfig.build.json',
-  test: false,
-});
-updateTsconfig({
-  extraPaths: [['lexicalOriginal', ['../lexical/src/']]],
-  jsonFileName: './packages/lexical-devtools/tsconfig.json',
-  test: false,
-});
+
+async function updateAllTsconfig() {
+  const prettierConfig = (await prettier.resolveConfig('./')) || {};
+  await updateTsconfig({
+    extraPaths: [],
+    jsonFileName: './tsconfig.json',
+    prettierConfig,
+    test: true,
+  });
+  await updateTsconfig({
+    extraPaths: [],
+    jsonFileName: './tsconfig.build.json',
+    prettierConfig,
+    test: false,
+  });
+  await updateTsconfig({
+    extraPaths: [['lexicalOriginal', ['../lexical/src/']]],
+    jsonFileName: './packages/lexical-devtools/tsconfig.json',
+    prettierConfig,
+    test: false,
+  });
+}
+
+updateAllTsconfig();

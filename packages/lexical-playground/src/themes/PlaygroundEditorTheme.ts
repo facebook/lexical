b380127dee36600dd542df65386504725727f67a
Ajay Prakash PP <ajayprakash0320@gmail.com>
2024-04-24 16:36:04 +0530

    Autocomplete: Move styling to theme (#5944)

--- packages/lexical-playground/src/themes/PlaygroundEditorTheme.ts
+++ packages/lexical-playground/src/themes/PlaygroundEditorTheme.ts
@@ -11,6 +11,7 @@ import type {EditorThemeClasses} from 'lexical';
 import './PlaygroundEditorTheme.css';
 
 const theme: EditorThemeClasses = {
+  autocomplete: 'PlaygroundEditorTheme__autocomplete',
   blockCursor: 'PlaygroundEditorTheme__blockCursor',
   characterLimit: 'PlaygroundEditorTheme__characterLimit',
   code: 'PlaygroundEditorTheme__code',

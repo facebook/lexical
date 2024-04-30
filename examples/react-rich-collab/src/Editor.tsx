77ab80a8492904cee8cf024b9e101d7e96506a25
Vladlen Fedosov <vladlen_fedosov@epam.com>
2024-04-29 21:08:10 -0400

    feat(examples/react-rich-collab): Added collaboration focused example

---
+++ examples/react-rich-collab/src/Editor.tsx
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+
+import ToolbarPlugin from './plugins/ToolbarPlugin';
+import TreeViewPlugin from './plugins/TreeViewPlugin';
+
+function Placeholder() {
+  return <div className="editor-placeholder">Enter some rich text...</div>;
+}
+
+export default function Editor() {
+  return (
+    <div className="editor-container">
+      <ToolbarPlugin />
+      <div className="editor-inner">
+        <RichTextPlugin
+          contentEditable={<ContentEditable className="editor-input" />}
+          placeholder={<Placeholder />}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <AutoFocusPlugin />
+        <TreeViewPlugin />
+      </div>
+    </div>
+  );
+}

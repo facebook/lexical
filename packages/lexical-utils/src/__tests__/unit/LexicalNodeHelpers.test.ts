6086471a71683b75549ad59cf7388e7e08654361
Bob Ippolito <bob@redivi.com>
2024-05-02 15:51:10 -0700

    fix type issue from #5977

--- packages/lexical-utils/src/__tests__/unit/LexicalNodeHelpers.test.ts
+++ packages/lexical-utils/src/__tests__/unit/LexicalNodeHelpers.test.ts
@@ -207,7 +207,7 @@ describe('LexicalNodeHelpers tests', () => {
     test('DFS of empty ParagraphNode returns only itself', async () => {
       const editor: LexicalEditor = testEnv.editor;
 
-      let paragraphKey;
+      let paragraphKey: string;
 
       await editor.update(() => {
         const root = $getRoot();
@@ -222,7 +222,7 @@ describe('LexicalNodeHelpers tests', () => {
         root.append(paragraph, paragraph2);
       });
       await editor.update(() => {
-        const paragraph = $getNodeByKey(paragraphKey);
+        const paragraph = $getNodeByKey(paragraphKey)!;
 
         expect($dfs(paragraph)).toEqual([
           {

f591d141c98491fc867f19fabc0282f3678e9a59
Bob Ippolito <bob@redivi.com>
2024-05-03 12:30:02 -0700

    Use build artifacts not typescript sources for playground build

--- packages/lexical-playground/vite.prod.config.ts
+++ packages/lexical-playground/vite.prod.config.ts
@@ -9,11 +9,48 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import react from '@vitejs/plugin-react';
+import * as fs from 'node:fs';
+import {createRequire} from 'node:module';
+import * as path from 'node:path';
 import {defineConfig} from 'vite';
 import {replaceCodePlugin} from 'vite-plugin-replace';
 
-import moduleResolution from '../shared/viteModuleResolution';
 import viteCopyEsm from './viteCopyEsm';
+
+const require = createRequire(import.meta.url);
+const {packagesManager} =
+  require('../../scripts/shared/packagesManager') as typeof import('../../scripts/shared/packagesManager');
+
+const moduleResolution = [
+  ...packagesManager.getPublicPackages().flatMap((pkg) =>
+    pkg.getNormalizedNpmModuleExportEntries().map(([find, exports]) => {
+      // Prefer the development esm version because we want nice errors and
+      // introspection on the playground!
+      const replacements = (['development', 'default'] as const).map((k) =>
+        pkg.resolve('dist', exports.import[k]),
+      );
+      const replacement = replacements.find((fn) => fs.existsSync(fn));
+      if (!replacement) {
+        throw new Error(
+          `ERROR: Missing ./${path.relative(
+            '../..',
+            replacements[1],
+          )}. Did you run \`npm run build\` in the monorepo first?`,
+        );
+      }
+      return {
+        find,
+        replacement,
+      };
+    }),
+  ),
+  ...[packagesManager.getPackageByDirectoryName('shared')].flatMap((pkg) =>
+    pkg.getPrivateModuleEntries().map(({name, sourceFileName}) => ({
+      find: name,
+      replacement: pkg.resolve('src', sourceFileName),
+    })),
+  ),
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -40,6 +77,7 @@ export default defineConfig({
       compress: {
         toplevel: true,
       },
+      keep_classnames: true,
     },
   },
   define: {

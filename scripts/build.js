facebook/lexical

commit 956ae8b55b8474a345eb9706daa2253fc42a11c9

[lexical-react] Breaking change: Deprecate public default exports (#6088)

scripts/build.js
@@ -191,7 +191,7 @@ async function build(name, inputFile, outputPath, outputFile, isProd, format) {
               tsconfig: path.resolve('./tsconfig.build.json'),
             },
           ],
-          '@babel/preset-react',
+          ['@babel/preset-react', {runtime: 'automatic'}],
         ],
       }),
       {
@@ -238,7 +238,7 @@ async function build(name, inputFile, outputPath, outputFile, isProd, format) {
   };
   const outputOptions = {
     esModule: false,
-    exports: 'auto',
+    exports: 'named',
     externalLiveBindings: false,
     file: outputFile,
     format, // change between es and cjs modules

.eslintrc.js
@@ -111,6 +111,28 @@ module.exports = {
         'lexical/no-optional-chaining': OFF,
       },
     },
+    {
+      files: [
+        'packages/*/src/index.ts',
+        'packages/*/src/index.tsx',
+        'packages/lexical-react/src/*.ts',
+        'packages/lexical-react/src/*.tsx',
+      ],
+      rules: {
+        'no-restricted-exports': [
+          'error',
+          {
+            restrictDefaultExports: {
+              defaultFrom: true,
+              direct: true,
+              named: true,
+              namedFrom: true,
+              namespaceFrom: true,
+            },
+          },
+        ],
+      },
+    },
   ],
 
   parser: '@babel/eslint-parser',

.flowconfig
@@ -69,6 +69,7 @@ module.name_mapper='^@lexical/react/LexicalPlainTextPlugin$' -> '<PROJECT_ROOT>/
 module.name_mapper='^@lexical/react/LexicalRichTextPlugin$' -> '<PROJECT_ROOT>/packages/lexical-react/flow/LexicalRichTextPlugin.js.flow'
 module.name_mapper='^@lexical/react/LexicalTabIndentationPlugin$' -> '<PROJECT_ROOT>/packages/lexical-react/flow/LexicalTabIndentationPlugin.js.flow'
 module.name_mapper='^@lexical/react/LexicalTableOfContents$' -> '<PROJECT_ROOT>/packages/lexical-react/flow/LexicalTableOfContents.js.flow'
+module.name_mapper='^@lexical/react/LexicalTableOfContentsPlugin$' -> '<PROJECT_ROOT>/packages/lexical-react/flow/LexicalTableOfContentsPlugin.js.flow'
 module.name_mapper='^@lexical/react/LexicalTablePlugin$' -> '<PROJECT_ROOT>/packages/lexical-react/flow/LexicalTablePlugin.js.flow'
 module.name_mapper='^@lexical/react/LexicalTreeView$' -> '<PROJECT_ROOT>/packages/lexical-react/flow/LexicalTreeView.js.flow'
 module.name_mapper='^@lexical/react/LexicalTypeaheadMenuPlugin$' -> '<PROJECT_ROOT>/packages/lexical-react/flow/LexicalTypeaheadMenuPlugin.js.flow'

babel.config.js
@@ -24,7 +24,7 @@ module.exports = {
         },
       },
     ],
-    '@babel/preset-react',
+    ['@babel/preset-react', {runtime: 'automatic'}],
     '@babel/preset-flow',
   ],
 };

examples/react-rich-collab/src/Editor.tsx
@@ -7,7 +7,7 @@
  */
 import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
 import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 
 import ToolbarPlugin from './plugins/ToolbarPlugin';

examples/react-rich/src/App.tsx
@@ -8,7 +8,7 @@
 import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
 import {LexicalComposer} from '@lexical/react/LexicalComposer';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 

packages/lexical-devtools/wxt.config.ts
@@ -112,7 +112,7 @@ export default defineConfig({
             },
           ],
         ],
-        presets: ['@babel/preset-react'],
+        presets: [['@babel/preset-react', {runtime: 'automatic'}]],
       }),
       react(),
     ],

packages/lexical-eslint-plugin/src/index.ts
@@ -14,4 +14,5 @@
 import * as plugin from './LexicalEslintPlugin.js';
 
 export type {RulesOfLexicalOptions} from './rules/rules-of-lexical.js';
+// eslint-disable-next-line no-restricted-exports
 export default plugin;

commit 780a87479c2b91a45243f1e7113fa697176b2754

fix(lexical): #6132 react build size regression from #6088
Changes are based on the https://github.com/facebook/lexical/pull/6048/commits/492816e0e9512f59976965be93190bc61bb28d08 by @etrepum

scripts/build.js
@@ -94,13 +94,11 @@ function resolveExternalEsm(id) {
  * party dependencies or peerDependencies that we do not want to include
  * in the bundles.
  */
-const externals = [
-  ...Object.entries(wwwMappings).flat(),
-  'react-dom',
-  'react',
-  'yjs',
-  'y-websocket',
-].sort();
+const monorepoExternalsSet = new Set(Object.entries(wwwMappings).flat());
+const thirdPartyExternals = ['react', 'react-dom', 'yjs', 'y-websocket'];
+const thirdPartyExternalsRegExp = new RegExp(
+  `^(${thirdPartyExternals.join('|')})(\\/|$)`,
+);
 
 const strictWWWMappings = {};
 
@@ -131,7 +129,10 @@ async function build(name, inputFile, outputPath, outputFile, isProd, format) {
   const extensions = ['.js', '.jsx', '.ts', '.tsx'];
   const inputOptions = {
     external(modulePath, src) {
-      return externals.includes(modulePath);
+      return (
+        monorepoExternalsSet.has(modulePath) ||
+        thirdPartyExternalsRegExp.test(modulePath)
+      );
     },
     input: inputFile,
     onwarn(warning) {
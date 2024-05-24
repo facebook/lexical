3df01baf8161aaa883c3bcca34382f5166e838ee
Maksym Plavinskyi <Maksym_Plavinskyi@epam.com>
2024-05-24 15:42:43 -0500

    [docs] Update examples in monorepo

--- examples/react-plain-text/src/App.tsx
+++ examples/react-plain-text/src/App.tsx
@@ -10,14 +10,13 @@ import {LexicalComposer} from '@lexical/react/LexicalComposer';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
-import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
 
 import ExampleTheme from './ExampleTheme';
-import ToolbarPlugin from './plugins/ToolbarPlugin';
 import TreeViewPlugin from './plugins/TreeViewPlugin';
 
 function Placeholder() {
-  return <div className="editor-placeholder">Enter some rich text...</div>;
+  return <div className="editor-placeholder">Enter some plain text...</div>;
 }
 
 const editorConfig = {
@@ -35,9 +34,8 @@ export default function App() {
   return (
     <LexicalComposer initialConfig={editorConfig}>
       <div className="editor-container">
-        <ToolbarPlugin />
         <div className="editor-inner">
-          <RichTextPlugin
+          <PlainTextPlugin
             contentEditable={<ContentEditable className="editor-input" />}
             placeholder={<Placeholder />}
             ErrorBoundary={LexicalErrorBoundary}
--- examples/react-rich/README.md
+++ examples/react-rich/README.md
@@ -1,7 +1,7 @@
-# Basic Vanilla JS example
+# React Rich Text example
 
 Here we have simplest Lexical setup in rich text configuration (`@lexical/rich-text`) with history (`@lexical/history`) and accessibility (`@lexical/dragon`) features enabled.
 
 **Run it locally:** `npm i && npm run dev`
 
-[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/facebook/lexical/tree/main/examples/react-rich?file=src/main.ts)
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/facebook/lexical/tree/main/examples/react-rich?file=src/main.tsx)

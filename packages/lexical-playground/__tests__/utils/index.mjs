db1eedd1bc70715f9611634b1c032c981399ccc3
Bob Ippolito <bob@redivi.com>
2024-05-31 11:00:43 -0700

    Normalize windows path for findAsset

--- packages/lexical-playground/__tests__/utils/index.mjs
+++ packages/lexical-playground/__tests__/utils/index.mjs
@@ -18,7 +18,7 @@ function findAsset(pattern) {
   const prefix = 'packages/lexical-playground/build';
   const resolvedPattern = `${prefix}/assets/${pattern}`;
   for (const fn of glob.sync(resolvedPattern, {windowsPathsNoEscape: true})) {
-    return fn.slice(prefix.length);
+    return fn.replace('\\', '/').slice(prefix.length);
   }
   throw new Error(`Missing asset at ${resolvedPattern}`);
 }

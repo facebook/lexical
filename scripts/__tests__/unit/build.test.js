facebook/lexical

commit 835e89bec424fc28069c86fce55357192180a606

[lexical-website] Documentation Update: Validate stackblitz URLs in documentation (#6215)

packages/lexical-playground/__tests__/utils/index.mjs
@@ -7,7 +7,7 @@
  */
 
 import {expect, test as base} from '@playwright/test';
-import glob from 'glob';
+import * as glob from 'glob';
 import {randomUUID} from 'node:crypto';
 import prettier from 'prettier';
 import {URLSearchParams} from 'url';
@@ -15,7 +15,7 @@ import {URLSearchParams} from 'url';
 import {selectAll} from '../keyboardShortcuts/index.mjs';
 
 function findAsset(pattern) {
-  const prefix = './packages/lexical-playground/build';
+  const prefix = 'packages/lexical-playground/build';
   const resolvedPattern = `${prefix}/assets/${pattern}`;
   for (const fn of glob.sync(resolvedPattern)) {
     return fn.slice(prefix.length);

packages/lexical-website/src/components/HomepageExamples/index.js
@@ -7,6 +7,7 @@
  */
 
 import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import * as Tabs from '@radix-ui/react-tabs';
 import {useState} from 'react';
 
@@ -23,7 +24,7 @@ const EXAMPLES = [
     ),
     id: 'example-feature-1',
     label: 'Simple Setup',
-    src: 'https://stackblitz.com/github/facebook/lexical/tree/main/examples/vanilla-js?embed=1&file=src%2Fmain.ts&terminalHeight=0&ctl=1',
+    src: 'vanilla-js?embed=1&file=src%2Fmain.ts&terminalHeight=0&ctl=1',
   },
   {
     content: (
@@ -38,7 +39,7 @@ const EXAMPLES = [
     ),
     id: 'example-feature-2',
     label: 'Powerful Features',
-    src: 'https://stackblitz.com/github/facebook/lexical/tree/main/examples/react-rich?embed=1&file=src%2FApp.tsx&terminalHeight=0&ctl=1',
+    src: 'react-rich?embed=1&file=src%2FApp.tsx&terminalHeight=0&ctl=1',
   },
   {
     content: (
@@ -50,12 +51,15 @@ const EXAMPLES = [
     ),
     id: 'example-feature-3',
     label: 'Built to Extend',
-    src: 'https://stackblitz.com/github/facebook/lexical/tree/main/examples/vanilla-js-plugin?embed=1&file=src%2Femoji-plugin%2FEmojiPlugin.ts&terminalHeight=0&ctl=1',
+    src: 'vanilla-js-plugin?embed=1&file=src%2Femoji-plugin%2FEmojiPlugin.ts&terminalHeight=0&ctl=1',
   },
 ];
 
 export default function HomepageExamples() {
   const [activeItemID, setActiveItemID] = useState(EXAMPLES[0].id);
+  const {
+    siteConfig: {customFields},
+  } = useDocusaurusContext();
 
   return (
     <Tabs.Root
@@ -97,7 +101,7 @@ export default function HomepageExamples() {
             <div>
               <iframe
                 className="h-[500px] w-full overflow-hidden"
-                src={src}
+                src={`${customFields.STACKBLITZ_PREFIX}examples/${src}`}
                 title={label}
                 sandbox="allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts"
               />

packages/lexical-website/docusaurus.config.js
@@ -193,13 +193,39 @@ const docusaurusPluginTypedocConfig = {
   watch: process.env.TYPEDOC_WATCH === 'true',
 };
 
+const GIT_COMMIT_SHA = process.env.VERCEL_GIT_COMMIT_SHA || 'main';
+const GIT_COMMIT_REF = process.env.VERCEL_GIT_COMMIT_REF || 'main';
+const GIT_REPO_OWNER = process.env.VERCEL_GIT_REPO_OWNER || 'facebook';
+const GIT_REPO_SLUG = process.env.VERCEL_GIT_REPO_SLUG || 'lexical';
+const STACKBLITZ_PREFIX = `https://stackblitz.com/github/${GIT_REPO_OWNER}/${GIT_REPO_SLUG}/tree/${
+  // Vercel does not set owner and slug correctly for fork PRs so we can't trust the ref by default
+  (GIT_COMMIT_REF === 'main' && !process.env.VERCEL_GIT_PULL_REQUEST_ID) ||
+  GIT_COMMIT_REF.endsWith('__release')
+    ? GIT_COMMIT_REF
+    : GIT_COMMIT_SHA
+}/`;
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   baseUrl: '/',
 
+  customFields: {
+    GIT_COMMIT_REF,
+    GIT_REPO_OWNER,
+    GIT_REPO_SLUG,
+    STACKBLITZ_PREFIX,
+  },
+
   favicon: 'img/favicon.ico',
 
-  markdown: {format: 'md'},
+  markdown: {
+    format: 'md',
+    preprocessor: ({fileContent}) =>
+      fileContent.replaceAll(
+        'https://stackblitz.com/github/facebook/lexical/tree/main/',
+        STACKBLITZ_PREFIX,
+      ),
+  },
 
   onBrokenAnchors: 'throw',
   // These are false positives when linking from API docs
@@ -235,6 +261,7 @@ const config = {
       };
     },
   ],
+
   presets: [
     [
       'classic',
@@ -387,7 +414,6 @@ const config = {
     }),
 
   title: TITLE,
-
   url: 'https://lexical.dev',
 };
 

scripts/__tests__/unit/stackblitz.test.js
@@ -1,0 +1,33 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+// @ts-check
+'use strict';
+
+const fs = require('fs-extra');
+const glob = require('glob');
+
+const STACKBLITZ_RE = /:\/\/stackblitz.com\/github\/([^?"']+)/g;
+
+describe('stackblitz doc url audits', () => {
+  glob
+    .sync(['packages/*/README.md', 'packages/lexical-website/docs/**/*.md'], {
+      ignore: 'packages/lexical-website/docs/api/**',
+    })
+    .forEach((fn) =>
+      describe(fn, () =>
+        it('Does not have incorrect stackblitz URLs', () => {
+          const contents = fs.readFileSync(fn, 'utf8');
+          for (const [, githubPath] of contents.matchAll(STACKBLITZ_RE)) {
+            expect(githubPath).toMatch(
+              /^facebook\/lexical\/tree\/main\/examples\//,
+            );
+          }
+        }),
+      ),
+    );
+});

commit 7289332f7e2edba743b4e33b0a52480520d71428

Fix: Resolve Windows regression caused by upgrading glob in #6215

.prettierignore
@@ -3,6 +3,7 @@
 **/dist/**
 **/build/**
 **/npm/**
+!scripts/npm/**
 **/.output/**
 **/.browser-profiles/**
 **/__tests__/integration/fixtures/**

packages/lexical-website/plugins/package-docs/index.js
@@ -36,9 +36,13 @@ module.exports = async function (context, options) {
     loadContent: () => {
       fs.mkdirSync(options.targetDir, {recursive: true});
       const oldTargets = new Set(
-        glob.sync(path.resolve(options.targetDir, '*.md')),
+        glob.sync(path.resolve(options.targetDir, '*.md'), {
+          windowsPathsNoEscape: true,
+        }),
       );
-      for (const srcPath of glob.sync(`${options.baseDir}/*/README.md`)) {
+      for (const srcPath of glob.sync(`${options.baseDir}/*/README.md`, {
+        windowsPathsNoEscape: true,
+      })) {
         const jsonPath = path.resolve(path.dirname(srcPath), 'package.json');
         if (!fs.existsSync(jsonPath)) {
           continue;

scripts/npm/prepare-release.js
@@ -31,7 +31,9 @@ function preparePackage(pkg) {
   fs.removeSync(pkg.resolve('npm'));
   fs.ensureDirSync(pkg.resolve('npm'));
   fs.copySync(pkg.resolve('dist'), pkg.resolve('npm'));
-  const flowSources = glob.sync(pkg.resolve('flow', '*.flow'));
+  const flowSources = glob.sync(pkg.resolve('flow', '*.flow'), {
+    windowsPathsNoEscape: true,
+  });
   if (flowSources.length === 0) {
     console.error(
       `Missing Flow type definitions for package ${pkg.getDirectoryName()}`,

scripts/override-react.js
@@ -19,7 +19,10 @@ async function main() {
     throw new Error('USAGE: node ./scripts/override-react --version=beta');
   }
   const packages = ['react', 'react-dom'];
-  ['package.json', ...glob.sync('./packages/*/package.json')].forEach((fn) => {
+  [
+    'package.json',
+    ...glob.sync('./packages/*/package.json', {windowsPathsNoEscape: true}),
+  ].forEach((fn) => {
     const json = fs.readJsonSync(fn);
     const isRoot = fn === 'package.json';
     let didUpdate = isRoot;

scripts/shared/packagesManager.js
@@ -129,5 +129,6 @@ exports.packagesManager = new PackagesManager(
       path.dirname(path.dirname(__dirname)),
       'packages/*/package.json',
     ),
+    {windowsPathsNoEscape: true},
   ),
 );

scripts/update-tsconfig.js
@@ -60,6 +60,7 @@ async function updateTsconfig({
       testPaths.push([`${pkg.getNpmName()}/src`, [resolveRelative('src')]]);
       for (const fn of glob.sync(
         pkg.resolve('src', '__tests__', 'utils', '*.{ts,tsx,mjs,jsx}'),
+        {windowsPathsNoEscape: true},
       )) {
         testPaths.push([
           `${pkg.getNpmName()}/src/__tests__/utils`,

scripts/validate-tsc-types.js
@@ -29,7 +29,7 @@ const diagnosticsHost = {
  */
 function validateTscTypes() {
   const dtsFilesPattern = './.ts-temp/packages/{lexical,lexical-*}/**/*.d.ts';
-  const dtsFiles = glob.sync(dtsFilesPattern);
+  const dtsFiles = glob.sync(dtsFilesPattern, {windowsPathsNoEscape: true});
   if (dtsFiles.length === 0) {
     console.error(
       `Missing ${dtsFilesPattern}, \`npm run build-prod\` or \`npm run build-release\` first`,

scripts/www/rewriteImports.js
@@ -22,7 +22,9 @@ const transformFlowFileContents = require('./transformFlowFileContents');
 // for each package so they can easily be copied to www.
 async function rewriteImports() {
   for (const pkg of packagesManager.getPackages()) {
-    for (const flowFile of glob.sync(pkg.resolve('flow', '*.flow'))) {
+    for (const flowFile of glob.sync(pkg.resolve('flow', '*.flow'), {
+      windowsPathsNoEscape: true,
+    })) {
       const data = fs.readFileSync(flowFile, 'utf8');
       const result = await transformFlowFileContents(data);
       fs.writeFileSync(

packages/lexical-playground/__tests__/utils/index.mjs
@@ -17,7 +17,7 @@ import {selectAll} from '../keyboardShortcuts/index.mjs';
 function findAsset(pattern) {
   const prefix = 'packages/lexical-playground/build';
   const resolvedPattern = `${prefix}/assets/${pattern}`;
-  for (const fn of glob.sync(resolvedPattern)) {
+  for (const fn of glob.sync(resolvedPattern, {windowsPathsNoEscape: true})) {
     return fn.slice(prefix.length);
   }
   throw new Error(`Missing asset at ${resolvedPattern}`);

scripts/__tests__/integration/prepare-release.test.js
@@ -27,5 +27,7 @@ describe('prepare-release tests', () => {
   }
 });
 ['examples', 'scripts/__tests__/integration/fixtures']
-  .flatMap((packagesDir) => glob.sync(`${packagesDir}/*/package.json`))
+  .flatMap((packagesDir) =>
+    glob.sync(`${packagesDir}/*/package.json`, {windowsPathsNoEscape: true}),
+  )
   .forEach((exampleJsonPath) => describeExample(exampleJsonPath));

scripts/__tests__/unit/build.test.js
@@ -38,7 +38,11 @@ describe('public package.json audits (`npm run update-packages` to fix most issu
         });
       }
       it('has *.flow types', () => {
-        expect(glob.sync(pkg.resolve('flow', '*.flow'))).not.toEqual([]);
+        expect(
+          glob.sync(pkg.resolve('flow', '*.flow'), {
+            windowsPathsNoEscape: true,
+          }),
+        ).not.toEqual([]);
       });
       it('uses the expected directory/npm naming convention', () => {
         expect(npmName.replace(/^@/, '').replace('/', '-')).toBe(
@@ -118,7 +122,11 @@ describe('www public package audits (`npm run update-packages` to fix most issue
     const wwwEntrypoint = `${npmToWwwName(npmName)}.js`;
     describe(npmName, () => {
       it('has *.flow types', () => {
-        expect(glob.sync(pkg.resolve('flow', '*.flow'))).not.toEqual([]);
+        expect(
+          glob.sync(pkg.resolve('flow', '*.flow'), {
+            windowsPathsNoEscape: true,
+          }),
+        ).not.toEqual([]);
       });
       // Only worry about the entrypoint stub if it has a single module export
       if (pkg.getExportedNpmModuleNames().every((name) => name === npmName)) {
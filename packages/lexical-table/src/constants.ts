573105cbab1d48df6a80cf8f8e3deab38b948171
Alex Reznik <alexander.reznik.01@gmail.com>
2024-06-03 21:50:43 +0100

    [lexical-table] Bug Fix: Change exported table column width (#6243)
    Co-authored-by: areznik10 <areznik10@bloomberg.net>

--- packages/lexical-table/src/constants.ts
+++ packages/lexical-table/src/constants.ts
@@ -7,3 +7,7 @@
  */
 
 export const PIXEL_VALUE_REG_EXP = /^(\d+(?:\.\d+)?)px$/;
+
+// .PlaygroundEditorTheme__tableCell width value from
+// packages/lexical-playground/src/themes/PlaygroundEditorTheme.css
+export const COLUMN_WIDTH = 75;

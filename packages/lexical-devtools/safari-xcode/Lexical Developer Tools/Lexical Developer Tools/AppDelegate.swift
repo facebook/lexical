8f169ba7e7b3a7bd9a59a2318943ca21b8c97ef5
Sahejkm <163521239+Sahejkm@users.noreply.github.com>
2024-06-06 20:00:35 +0800

    [Lexical] Fix missing meta copyright headers in files  (#6258)

--- packages/lexical-devtools/safari-xcode/Lexical Developer Tools/Lexical Developer Tools/AppDelegate.swift
+++ packages/lexical-devtools/safari-xcode/Lexical Developer Tools/Lexical Developer Tools/AppDelegate.swift
@@ -1,9 +1,10 @@
-//
-//  AppDelegate.swift
-//  Lexical Developer Tools
-//
-//  Created by Vladlen Fedosov on 5/14/24.
-//
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
 
 import Cocoa
 
--- scripts/__tests__/integration/fixtures/lexical-esm-sveltekit-vanilla-js/tests/test.ts
+++ scripts/__tests__/integration/fixtures/lexical-esm-sveltekit-vanilla-js/tests/test.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { expect, test } from '@playwright/test';
 
 test('index page has expected h1 and lexical state', async ({ page }) => {

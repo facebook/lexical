0e37c2eaadb42915e9ad28bceceb59a9fc74a864
sahejkm <sahejk@meta.com>
2024-07-04 19:37:39 +0800

    add option to render preview at run time if img uri not available

--- packages/lexical-website/src/components/Gallery/pluginList.js
+++ packages/lexical-website/src/components/Gallery/pluginList.js
@@ -8,7 +8,13 @@
 
 export const plugins = (customFields) => [
   {
+    description: 'Learn how to create an editor with Emojis',
     title: 'EmojiPlugin',
     uri: `${customFields.STACKBLITZ_PREFIX}examples/vanilla-js-plugin?embed=1&file=src%2Femoji-plugin%2FEmojiPlugin.ts&terminalHeight=0&ctl=0`,
+  },
+  {
+    description: 'Learn how to create an editor with RichText',
+    title: 'Collab RichText',
+    uri: 'https://stackblitz.com/github/facebook/lexical/tree/fix/collab_example/examples/react-rich-collab?ctl=0&file=src%2Fmain.tsx&terminalHeight=0&embed=1',
   },
 ];

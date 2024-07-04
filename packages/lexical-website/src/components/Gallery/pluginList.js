485d90bb38e1c24f513fc8e462824aa6424eb08c
sahejkm <sahejk@meta.com>
2024-07-04 19:41:20 +0800

    update desc text

--- packages/lexical-website/src/components/Gallery/pluginList.js
+++ packages/lexical-website/src/components/Gallery/pluginList.js
@@ -13,7 +13,7 @@ export const plugins = (customFields) => [
     uri: `${customFields.STACKBLITZ_PREFIX}examples/vanilla-js-plugin?embed=1&file=src%2Femoji-plugin%2FEmojiPlugin.ts&terminalHeight=0&ctl=0`,
   },
   {
-    description: 'Learn how to create an editor with RichText',
+    description: 'Learn how to create an editor with Real Time Collaboration',
     title: 'Collab RichText',
     uri: 'https://stackblitz.com/github/facebook/lexical/tree/fix/collab_example/examples/react-rich-collab?ctl=0&file=src%2Fmain.tsx&terminalHeight=0&embed=1',
   },

e58edc6e7ef7173d6635a1f28d8c98ddb50890d4
Gerard Rovira <gerardrovira@meta.com>
2024-07-08 16:56:20 -0400

    more code

--- packages/lexical-react/src/LexicalContentEditable.tsx
+++ packages/lexical-react/src/LexicalContentEditable.tsx
@@ -10,43 +10,52 @@ import type {Props as ElementProps} from './shared/LexicalContentEditableElement
 
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {useLexicalEditable} from '@lexical/react/useLexicalEditable';
+import {forwardRef, Ref} from 'react';
 
 import {ContentEditableElement} from './shared/LexicalContentEditableElement';
 import {useCanShowPlaceholder} from './shared/useCanShowPlaceholder';
 
 /* eslint-disable @typescript-eslint/ban-types */
-export type Props = (
-  | {}
-  | {
-      placeholder: null;
-    }
-  | {
-      'aria-placeholder': string;
-      placeholder: ((isEditable: boolean) => null | JSX.Element) | JSX.Element;
-    }
-) &
-  ElementProps;
+export type Props = ElementProps &
+  (
+    | {
+        'aria-placeholder'?: void;
+        placeholder?: null;
+      }
+    | {
+        'aria-placeholder': string;
+        placeholder:
+          | ((isEditable: boolean) => null | JSX.Element)
+          | JSX.Element;
+      }
+  );
+
 /* eslint-enable @typescript-eslint/ban-types */
 
-export function ContentEditable(props: Props): JSX.Element {
+function ContentEditableImpl(
+  props: Props,
+  ref: Ref<HTMLDivElement>,
+): JSX.Element {
   let placeholder = null;
-  let rest = props;
+  let rest: Omit<Props, 'placeholder'> = props;
   if ('placeholder' in props) {
     ({placeholder, ...rest} = props);
   }
 
   return (
     <>
-      <ContentEditableElement {...rest} />
-      <Placeholder content={placeholder} />
+      <ContentEditableElement {...rest} ref={ref} />
+      {placeholder != null && <Placeholder content={placeholder} />}
     </>
   );
 }
 
+export const ContentEditable = forwardRef(ContentEditableImpl);
+
 function Placeholder({
   content,
 }: {
-  content: ((isEditable: boolean) => null | JSX.Element) | null | JSX.Element;
+  content: ((isEditable: boolean) => null | JSX.Element) | JSX.Element;
 }): null | JSX.Element {
   const [editor] = useLexicalComposerContext();
   const showPlaceholder = useCanShowPlaceholder(editor);

36b8eab0b5ed8acd2be816dad85fe92cc908e592
Gerard Rovira <zurfyx@users.noreply.github.com>
2024-08-26 19:28:34 +0100

    Revert "[lexical-playground] Fix: in playground show component-menu w… (#6558)

--- packages/lexical-react/src/shared/LexicalMenu.ts
+++ packages/lexical-react/src/shared/LexicalMenu.ts
@@ -494,7 +494,9 @@ export function useMenuAnchorRef(
     if (rootElement !== null && resolution !== null) {
       const {left, top, width, height} = resolution.getRect();
       const anchorHeight = anchorElementRef.current.offsetHeight; // use to position under anchor
-      containerDiv.style.top = `${top + anchorHeight + 3}px`;
+      containerDiv.style.top = `${
+        top + window.pageYOffset + anchorHeight + 3
+      }px`;
       containerDiv.style.left = `${left + window.pageXOffset}px`;
       containerDiv.style.height = `${height}px`;
       containerDiv.style.width = `${width}px`;
@@ -516,7 +518,9 @@ export function useMenuAnchorRef(
             top + menuHeight > rootElementRect.bottom) &&
           top - rootElementRect.top > menuHeight + height
         ) {
-          containerDiv.style.top = `${top - menuHeight - height}px`;
+          containerDiv.style.top = `${
+            top - menuHeight + window.pageYOffset - height
+          }px`;
         }
       }
 

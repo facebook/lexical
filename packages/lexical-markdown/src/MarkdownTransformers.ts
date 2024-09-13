fc9babd883744d05532d0dd6b7b4ecb5ee8813e9
Bob Ippolito <bob@redivi.com>
2024-09-12 19:47:48 -0700

    prettier

--- packages/lexical-markdown/src/MarkdownTransformers.ts
+++ packages/lexical-markdown/src/MarkdownTransformers.ts
@@ -160,7 +160,8 @@ const HEADING_REGEX = /^(#{1,6})\s/;
 const QUOTE_REGEX = /^>\s/;
 const CODE_START_REGEX = /^[ \t]*```(\w+)?/;
 const CODE_END_REGEX = /[ \t]*```$/;
-const CODE_SINGLE_LINE_REGEX = /^[ \t]*```[^`]+(?:(?:`{1,2}|`{4,})[^`]+)*```(?:[^`]|$)/;
+const CODE_SINGLE_LINE_REGEX =
+  /^[ \t]*```[^`]+(?:(?:`{1,2}|`{4,})[^`]+)*```(?:[^`]|$)/;
 const TABLE_ROW_REG_EXP = /^(?:\|)(.+)(?:\|)\s?$/;
 const TABLE_ROW_DIVIDER_REG_EXP = /^(\| ?:?-*:? ?)+\|\s?$/;
 

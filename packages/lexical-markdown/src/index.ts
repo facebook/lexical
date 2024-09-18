3511943115c7b15c59eb21044ad8ef97ab1bebe1
Sherry Wong <potatowagon@fb.com>
2024-09-18 18:08:26 +0800

    resolve JSC_DEFAULT_PARAM_MUST_BE_MARKED_OPTIONAL

--- packages/lexical-markdown/src/index.ts
+++ packages/lexical-markdown/src/index.ts
@@ -76,16 +76,15 @@ const TRANSFORMERS: Array<Transformer> = [
 
 /**
  * Renders markdown from a string. The selection is moved to the start after the operation.
+ *
+ *  @param {boolean} [shouldPreserveNewLines] By setting this to true, new lines will be preserved between conversions
+ *  @param {boolean} [shouldNormalizeMarkdown] By setting this to true, adjacent non empty lines will be merged according to commonmark spec: https://spec.commonmark.org/0.24/#example-177
  */
 function $convertFromMarkdownString(
   markdown: string,
   transformers: Array<Transformer> = TRANSFORMERS,
   node?: ElementNode,
   shouldPreserveNewLines = false,
-  /**
-   * By setting this to true, adjacent non empty lines will be merged
-   * according to commonmark spec: https://spec.commonmark.org/0.24/#example-177
-   */
   shouldNormalizeMarkdown = true,
 ): void {
   const sanitizedMarkdown =

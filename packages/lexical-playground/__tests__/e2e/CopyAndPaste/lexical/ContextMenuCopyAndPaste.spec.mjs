ad4f0db47e260ce99f3458a965451bb3389bda3a
C. <cairohyfr@gmail.com>
2024-10-30 14:13:51 -0500

    Add test case for context menu ability to paste text with custom style

--- packages/lexical-playground/__tests__/e2e/CopyAndPaste/lexical/ContextMenuCopyAndPaste.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/CopyAndPaste/lexical/ContextMenuCopyAndPaste.spec.mjs
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-
+import {selectAll} from '../../../keyboardShortcuts/index.mjs';
 import {
   assertHTML,
   click,
@@ -48,6 +48,46 @@ test.describe('ContextMenuCopyAndPaste', () => {
           class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
           dir="ltr">
           <span data-lexical-text="true">hellohello</span>
+        </p>
+      `,
+    );
+  });
+  test('Rich text Copy and Paste', async ({page, isRichText}) => {
+    test.skip(isRichText);
+    await focusEditor(page);
+    await page.keyboard.type('MLH Fellowship');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Enter');
+    await page.keyboard.type('Fall 2024');
+
+    await selectAll();
+    await click(page, '.font-increment');
+    await click(page, '.lock');
+
+    await page.pause();
+    await doubleClick(page, 'div[contenteditable="false"] span');
+    await page.pause();
+    await click(page, 'div[contenteditable="false"] span', {button: 'right'});
+    await click(page, '#typeahead-menu [role="option"] :text("Copy")');
+
+    await click(page, '.unlock');
+    await focusEditor(page);
+
+    await pasteFromClipboard(page);
+
+    await assertHTML(
+      page,
+      html`
+        <p class="PlaygroundEditorTheme__paragraph" dir="ltr">
+          <span style="font-size: 17px; white-space: pre-wrap;">
+            MLH Fellowship
+          </span>
+        </p>
+        <p class="PlaygroundEditorTheme__paragraph">
+          <br />
+        </p>
+        <p class="PlaygroundEditorTheme__paragraph" dir="ltr">
+          <span style="font-size: 17px; white-space: pre-wrap;">Fall 2024</span>
         </p>
       `,
     );

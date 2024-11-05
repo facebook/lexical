205db9232cca82f102b52da2e84ba5085ea84499
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-11-05 01:37:14 -0600

    chore: add the import property configuration

--- examples/react-rich/src/App.tsx
+++ examples/react-rich/src/App.tsx
@@ -13,6 +13,7 @@ import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {
+  $isTextNode,
   DOMConversionMap,
   DOMExportOutput,
   Klass,
@@ -25,6 +26,7 @@ import {
 import ExampleTheme from './ExampleTheme';
 import ToolbarPlugin from './plugins/ToolbarPlugin';
 import TreeViewPlugin from './plugins/TreeViewPlugin';
+import {parseAllowedColor, parseAllowedFontSize} from './styleConfig';
 
 const placeholder = 'Enter some rich text...';
 
@@ -49,34 +51,76 @@ const exportMap = new Map<
   [TextNode, removeStylesExportDOM],
 ]);
 
-const importMap: DOMConversionMap<HTMLElement> = {
-  '*': (element) => ({
-    // conversions for other elements
-    conversion: () => ({
-      node: new TextNode(element.textContent || ''),
-      priority: 0,
-    }),
-  }),
-  p: (element) => ({
-    conversion: () => ({
-      node: new ParagraphNode(element.textContent || ''),
-      priority: 0,
-    }),
-  }),
-  span: (element) => ({
-    conversion: () => ({
-      node: new TextNode(element.textContent || ''),
-      priority: 0,
-    }),
-  }),
+const getExtraStyles = (element: HTMLElement): string => {
+  // Parse styles from pasted input, but only if they match exactly the
+  // sort of styles that would be produced by exportDOM
+  let extraStyles = '';
+  const fontSize = parseAllowedFontSize(element.style.fontSize);
+  const backgroundColor = parseAllowedColor(element.style.backgroundColor);
+  const color = parseAllowedColor(element.style.color);
+  if (fontSize !== '' && fontSize !== '15px') {
+    extraStyles += `font-size: ${fontSize};`;
+  }
+  if (backgroundColor !== '' && backgroundColor !== 'rgb(255, 255, 255)') {
+    extraStyles += `background-color: ${backgroundColor};`;
+  }
+  if (color !== '' && color !== 'rgb(0, 0, 0)') {
+    extraStyles += `color: ${color};`;
+  }
+  return extraStyles;
+};
+
+const constructImportMap = (): DOMConversionMap => {
+  const importMap: DOMConversionMap = {};
+
+  // Wrap all TextNode importers with a function that also imports
+  // the custom styles implemented by the playground
+  for (const [tag, fn] of Object.entries(TextNode.importDOM() || {})) {
+    importMap[tag] = (importNode) => {
+      const importer = fn(importNode);
+      if (!importer) {
+        return null;
+      }
+      return {
+        ...importer,
+        conversion: (element) => {
+          const output = importer.conversion(element);
+          if (
+            output === null ||
+            output.forChild === undefined ||
+            output.after !== undefined ||
+            output.node !== null
+          ) {
+            return output;
+          }
+          const extraStyles = getExtraStyles(element);
+          if (extraStyles) {
+            const {forChild} = output;
+            return {
+              ...output,
+              forChild: (child, parent) => {
+                const textNode = forChild(child, parent);
+                if ($isTextNode(textNode)) {
+                  textNode.setStyle(textNode.getStyle() + extraStyles);
+                }
+                return textNode;
+              },
+            };
+          }
+          return output;
+        },
+      };
+    };
+  }
+
+  return importMap;
 };
 
 const editorConfig = {
   html: {
     export: exportMap,
-    import: importMap,
+    import: constructImportMap(),
   },
-
   namespace: 'React.js Demo',
   nodes: [ParagraphNode, TextNode],
   onError(error: Error) {

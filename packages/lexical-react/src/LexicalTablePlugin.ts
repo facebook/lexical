50dadbcbb87ab8aca20b206c9b1e8aef80c8eadc
Bob Ippolito <bob@redivi.com>
2024-11-11 08:13:47 -0800

    Update @lexical/react/TablePlugin documentation

--- packages/lexical-react/src/LexicalTablePlugin.ts
+++ packages/lexical-react/src/LexicalTablePlugin.ts
@@ -44,17 +44,38 @@ import {
 import {useEffect} from 'react';
 import invariant from 'shared/invariant';
 
+export interface TablePluginProps {
+  /**
+   * When `false` (default `true`), merged cell support (colspan and rowspan) will be disabled and all
+   * tables will be forced into a regular grid with 1x1 table cells.
+   */
+  hasCellMerge?: boolean;
+  /**
+   * When `false` (default `true`), the background color of TableCellNode will always be removed.
+   */
+  hasCellBackgroundColor?: boolean;
+  /**
+   * When `true` (default `true`), the tab key can be used to navigate table cells.
+   */
+  hasTabHandler?: boolean;
+  /**
+   * When `true` (default `false`), tables will be wrapped in a `<div>` to enable horizontal scrolling
+   */
+  hasHorizontalScroll?: boolean;
+}
+
+/**
+ * A plugin to enable all of the features of Lexical's TableNode.
+ *
+ * @param props - See type for documentation
+ * @returns An element to render in your LexicalComposer
+ */
 export function TablePlugin({
   hasCellMerge = true,
   hasCellBackgroundColor = true,
   hasTabHandler = true,
   hasHorizontalScroll = false,
-}: {
-  hasCellMerge?: boolean;
-  hasCellBackgroundColor?: boolean;
-  hasTabHandler?: boolean;
-  hasHorizontalScroll?: boolean;
-}): JSX.Element | null {
+}: TablePluginProps): JSX.Element | null {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {

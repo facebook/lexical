ea515e9dfa6228280909951897541967cd076ed5
bedre7 <bedru2019@gmail.com>
2024-11-13 15:53:39 +0300

    fix failing test and add new test to autocompletion with different text formats

--- packages/lexical-playground/__tests__/e2e/Autocomplete.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/Autocomplete.spec.mjs
@@ -7,6 +7,13 @@
  */
 
 import {
+  increaseFontSize,
+  toggleBold,
+  toggleItalic,
+  toggleStrikethrough,
+  toggleUnderline,
+} from '../keyboardShortcuts/index.mjs';
+import {
   assertHTML,
   focusEditor,
   html,
@@ -30,14 +37,11 @@ test.describe('Autocomplete', () => {
           class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
           dir="ltr">
           <span data-lexical-text="true">Sort by alpha</span>
-          <span contenteditable="false" data-lexical-decorator="true">
-            <span
-              class="PlaygroundEditorTheme__autocomplete"
-              spellcheck="false">
-              betical (TAB)
-            </span>
+          <span
+            class="PlaygroundEditorTheme__autocomplete"
+            data-lexical-text="true">
+            betical (TAB)
           </span>
-          <br />
         </p>
       `,
       html`
@@ -59,6 +63,80 @@ test.describe('Autocomplete', () => {
           class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
           dir="ltr">
           <span data-lexical-text="true">Sort by alphabetical order:</span>
+        </p>
+      `,
+    );
+  });
+
+  test('Can autocomplete in the same format as the original text', async ({
+    page,
+    isPlainText,
+  }) => {
+    await focusEditor(page);
+    await toggleBold(page);
+    await toggleItalic(page);
+    await toggleUnderline(page);
+    await toggleStrikethrough(page);
+    await increaseFontSize(page);
+
+    await page.keyboard.type('MLH Fell');
+    await sleep(500);
+
+    await assertHTML(
+      page,
+      html`
+        <p
+          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+          dir="ltr">
+          <strong
+            class="PlaygroundEditorTheme__textUnderlineStrikethrough PlaygroundEditorTheme__textBold PlaygroundEditorTheme__textItalic"
+            style="font-size: 17px;"
+            data-lexical-text="true">
+            MLH Fell
+          </strong>
+          <strong
+            class="PlaygroundEditorTheme__textUnderlineStrikethrough PlaygroundEditorTheme__textBold PlaygroundEditorTheme__textItalic PlaygroundEditorTheme__autocomplete"
+            style="font-size: 17px;"
+            data-lexical-text="true">
+            owship (TAB)
+          </strong>
+        </p>
+      `,
+      html`
+        <p
+          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+          dir="ltr">
+          <strong
+            class="PlaygroundEditorTheme__textUnderlineStrikethrough PlaygroundEditorTheme__textBold PlaygroundEditorTheme__textItalic"
+            style="font-size: 17px;"
+            data-lexical-text="true">
+            MLH Fell
+          </strong>
+        </p>
+      `,
+    );
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.type(' is awesome!');
+
+    await assertHTML(
+      page,
+      html`
+        <p
+          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+          dir="ltr">
+          <strong
+            class="PlaygroundEditorTheme__textUnderlineStrikethrough PlaygroundEditorTheme__textBold PlaygroundEditorTheme__textItalic"
+            style="font-size: 17px;"
+            data-lexical-text="true">
+            MLH Fell
+          </strong>
+          <strong
+            class="PlaygroundEditorTheme__textUnderlineStrikethrough PlaygroundEditorTheme__textBold PlaygroundEditorTheme__textItalic"
+            style="font-size: 17px;"
+            data-lexical-text="true">
+            owship is awesome!
+          </strong>
         </p>
       `,
     );

8f98a2c09d78e8bbf979ecf3c2f311044cfffd97
Bob Ippolito <bob@redivi.com>
2024-11-25 22:41:38 -0800

    Restore selection explicitly

--- packages/lexical-playground/src/plugins/FloatingLinkEditorPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/FloatingLinkEditorPlugin/index.tsx
@@ -19,6 +19,7 @@ import {
   $getSelection,
   $isLineBreakNode,
   $isRangeSelection,
+  $setSelection,
   BaseSelection,
   CLICK_COMMAND,
   COMMAND_PRIORITY_CRITICAL,
@@ -205,6 +206,7 @@ function FloatingLinkEditor({
     if (lastSelection !== null) {
       if (linkUrl !== '') {
         editor.update(() => {
+          $setSelection(lastSelection.clone());
           editor.dispatchCommand(
             TOGGLE_LINK_COMMAND,
             sanitizeUrl(editedLinkUrl),

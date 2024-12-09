6c43d9b9f7573b9a9ca29bc618f3b5f7c5f5cd6a
Aleksandr Lapuskin <aleksandr.lapuskin@epilot.cloud>
2024-12-09 10:40:23 +0200

    Add e2e test

--- packages/lexical-playground/__tests__/e2e/List.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/List.spec.mjs
@@ -17,6 +17,7 @@ import {
   redo,
   selectAll,
   selectCharacters,
+  toggleBold,
   undo,
 } from '../keyboardShortcuts/index.mjs';
 import {
@@ -32,6 +33,7 @@ import {
   pasteFromClipboard,
   repeat,
   selectFromAlignDropdown,
+  selectFromColorPicker,
   selectFromFormatDropdown,
   test,
   waitForSelector,
@@ -155,6 +157,25 @@ test.describe.parallel('Nested List', () => {
     await assertHTML(
       page,
       '<ul class="PlaygroundEditorTheme__ul"><li value="1" class="PlaygroundEditorTheme__listItem PlaygroundEditorTheme__ltr" dir="ltr"><span data-lexical-text="true">Hello</span></li><li value="2" class="PlaygroundEditorTheme__listItem PlaygroundEditorTheme__nestedListItem"><ul class="PlaygroundEditorTheme__ul"><li value="1" class="PlaygroundEditorTheme__listItem"><br></li></ul></li></ul>',
+    );
+  });
+
+  test('Should retain selection style when exiting list', async ({page}) => {
+    await focusEditor(page);
+    await toggleBulletList(page);
+
+    await selectFromColorPicker(page);
+    await await focusEditor(page);
+    await toggleBold(page);
+    await page.keyboard.type('Hello');
+    //Double-enter to exit list
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Enter');
+    await page.keyboard.type('World');
+
+    await assertHTML(
+      page,
+      '<ul class="PlaygroundEditorTheme__ul"><li value="1" class="PlaygroundEditorTheme__listItem PlaygroundEditorTheme__ltr" dir="ltr"><strong class="PlaygroundEditorTheme__textBold" style="color: rgb(208, 2, 27)" data-lexical-text="true">Hello</strong></li></ul><p class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr" dir="ltr"><strong class="PlaygroundEditorTheme__textBold" style="color: rgb(208, 2, 27)" data-lexical-text="true">World</strong></p>',
     );
   });
 

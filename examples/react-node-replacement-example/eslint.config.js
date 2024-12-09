e1b862277249965f062b8057c2561d7f9fed0be0
Fadekemi Adebayo <fadekemi.adebayo@pau.edu.ng>
2024-12-09 18:45:15 +0000

    node replacement example

---
+++ examples/react-node-replacement-example/eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: '6',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+        "allowImportExportEverywhere": true
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]
---
+++ examples/react-node-replacement-example/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import Editor from "./Editor";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Node Replacement Example</h1>
+      <p>Note: this is an experimental build of Lexical</p>
+      <Editor />
+    </div>
+  );
+}

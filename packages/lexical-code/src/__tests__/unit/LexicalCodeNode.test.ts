facebook/lexical

commit 66f805c41a9c72a4b92f8a5056938335c1b3d7b1

[Breaking Change][lexical] Feature: New update tag: skip-dom-selection, $onUpdate now always called (#6894)

packages/lexical/src/LexicalUtils.ts
@@ -72,7 +72,6 @@ import {
   internalGetActiveEditorState,
   isCurrentlyReadOnlyMode,
   triggerCommandListeners,
-  updateEditor,
 } from './LexicalUpdates';
 
 export const emptyFunction = () => {
@@ -498,22 +497,31 @@ export function getEditorStateTextContent(editorState: EditorState): string {
   return editorState.read(() => $getRoot().getTextContent());
 }
 
-export function markAllNodesAsDirty(editor: LexicalEditor, type: string): void {
-  // Mark all existing text nodes as dirty
-  updateEditor(
-    editor,
+export function markNodesWithTypesAsDirty(
+  editor: LexicalEditor,
+  types: string[],
+): void {
+  // We only need to mark nodes dirty if they were in the previous state.
+  // If they aren't, then they are by definition dirty already.
+  const cachedMap = getCachedTypeToNodeMap(editor.getEditorState());
+  const dirtyNodeMaps: NodeMap[] = [];
+  for (const type of types) {
+    const nodeMap = cachedMap.get(type);
+    if (nodeMap) {
+      // By construction these are non-empty
+      dirtyNodeMaps.push(nodeMap);
+    }
+  }
+  // Nothing to mark dirty, no update necessary
+  if (dirtyNodeMaps.length === 0) {
+    return;
+  }
+  editor.update(
     () => {
-      const editorState = getActiveEditorState();
-      if (editorState.isEmpty()) {
-        return;
-      }
-      if (type === 'root') {
-        $getRoot().markDirty();
-        return;
-      }
-      const nodeMap = editorState._nodeMap;
-      for (const [, node] of nodeMap) {
-        node.markDirty();
+      for (const nodeMap of dirtyNodeMaps) {
+        for (const node of nodeMap.values()) {
+          node.markDirty();
+        }
       }
     },
     editor._pendingEditorState === null
@@ -1825,17 +1833,26 @@ export function getCachedTypeToNodeMap(
   );
   let typeToNodeMap = cachedNodeMaps.get(editorState);
   if (!typeToNodeMap) {
-    typeToNodeMap = new Map();
+    typeToNodeMap = computeTypeToNodeMap(editorState);
     cachedNodeMaps.set(editorState, typeToNodeMap);
-    for (const [nodeKey, node] of editorState._nodeMap) {
-      const nodeType = node.__type;
-      let nodeMap = typeToNodeMap.get(nodeType);
-      if (!nodeMap) {
-        nodeMap = new Map();
-        typeToNodeMap.set(nodeType, nodeMap);
-      }
-      nodeMap.set(nodeKey, node);
+  }
+  return typeToNodeMap;
+}
+
+/**
+ * @internal
+ * Compute a Map of node type to nodes for an EditorState
+ */
+function computeTypeToNodeMap(editorState: EditorState): TypeToNodeMap {
+  const typeToNodeMap = new Map();
+  for (const [nodeKey, node] of editorState._nodeMap) {
+    const nodeType = node.__type;
+    let nodeMap = typeToNodeMap.get(nodeType);
+    if (!nodeMap) {
+      nodeMap = new Map();
+      typeToNodeMap.set(nodeType, nodeMap);
     }
+    nodeMap.set(nodeKey, node);
   }
   return typeToNodeMap;
 }

packages/lexical-code/flow/LexicalCode.js.flow
@@ -77,12 +77,6 @@ declare export class CodeHighlightNode extends TextNode {
   // $FlowFixMe
   static clone(node: CodeHighlightNode): CodeHighlightNode;
   createDOM(config: EditorConfig): HTMLElement;
-  updateDOM(
-    // $FlowFixMe
-    prevNode: CodeHighlightNode,
-    dom: HTMLElement,
-    config: EditorConfig,
-  ): boolean;
   setFormat(format: number): this;
 }
 
@@ -125,7 +119,6 @@ declare export class CodeNode extends ElementNode {
   static clone(node: CodeNode): CodeNode;
   constructor(language: ?string, key?: NodeKey): void;
   createDOM(config: EditorConfig): HTMLElement;
-  updateDOM(prevNode: CodeNode, dom: HTMLElement): boolean;
   insertNewAfter(
     selection: RangeSelection,
     restoreSelection?: boolean,

packages/lexical-code/src/CodeHighlightNode.ts
@@ -136,11 +136,7 @@ export class CodeHighlightNode extends TextNode {
     return element;
   }
 
-  updateDOM(
-    prevNode: CodeHighlightNode,
-    dom: HTMLElement,
-    config: EditorConfig,
-  ): boolean {
+  updateDOM(prevNode: this, dom: HTMLElement, config: EditorConfig): boolean {
     const update = super.updateDOM(prevNode, dom, config);
     const prevClassName = getHighlightThemeClass(
       config.theme,

packages/lexical-code/src/CodeNode.ts
@@ -107,11 +107,7 @@ export class CodeNode extends ElementNode {
     }
     return element;
   }
-  updateDOM(
-    prevNode: CodeNode,
-    dom: HTMLElement,
-    config: EditorConfig,
-  ): boolean {
+  updateDOM(prevNode: this, dom: HTMLElement, config: EditorConfig): boolean {
     const language = this.__language;
     const prevLanguage = prevNode.__language;
 

packages/lexical-link/flow/LexicalLink.js.flow
@@ -39,11 +39,6 @@ declare export class LinkNode extends ElementNode {
   static clone(node: LinkNode): LinkNode;
   constructor(url: string, attributes?: LinkAttributes, key?: NodeKey): void;
   createDOM(config: EditorConfig): HTMLElement;
-  updateDOM(
-    prevNode: LinkNode,
-    dom: HTMLElement,
-    config: EditorConfig,
-  ): boolean;
   static importDOM(): DOMConversionMap | null;
   exportJSON(): SerializedLinkNode;
   getURL(): string;

packages/lexical-link/src/index.ts
@@ -113,7 +113,7 @@ export class LinkNode extends ElementNode {
   }
 
   updateDOM(
-    prevNode: LinkNode,
+    prevNode: this,
     anchor: LinkHTMLElementType,
     config: EditorConfig,
   ): boolean {
@@ -393,7 +393,7 @@ export class AutoLinkNode extends LinkNode {
   }
 
   updateDOM(
-    prevNode: AutoLinkNode,
+    prevNode: this,
     anchor: LinkHTMLElementType,
     config: EditorConfig,
   ): boolean {

packages/lexical-list/src/LexicalListNode.ts
@@ -111,11 +111,7 @@ export class ListNode extends ElementNode {
     return dom;
   }
 
-  updateDOM(
-    prevNode: ListNode,
-    dom: HTMLElement,
-    config: EditorConfig,
-  ): boolean {
+  updateDOM(prevNode: this, dom: HTMLElement, config: EditorConfig): boolean {
     if (prevNode.__tag !== this.__tag) {
       return true;
     }

packages/lexical-mark/src/MarkNode.ts
@@ -78,7 +78,7 @@ export class MarkNode extends ElementNode {
   }
 
   updateDOM(
-    prevNode: MarkNode,
+    prevNode: this,
     element: HTMLElement,
     config: EditorConfig,
   ): boolean {

commit 1aa2cb4a032061643e237933b0c908a9d2dc2b84

Fix registerNodeTransform regression introduced in #6894

packages/lexical/src/LexicalUtils.ts
@@ -543,8 +543,13 @@ export function markNodesWithTypesAsDirty(
   editor.update(
     () => {
       for (const nodeMap of dirtyNodeMaps) {
-        for (const node of nodeMap.values()) {
-          node.markDirty();
+        for (const nodeKey of nodeMap.keys()) {
+          // We are only concerned with nodes that are still in the latest NodeMap,
+          // if they no longer exist then markDirty would raise an exception
+          const latest = $getNodeByKey(nodeKey);
+          if (latest) {
+            latest.markDirty();
+          }
         }
       }
     },

packages/lexical-code/src/__tests__/unit/LexicalCodeNode.test.ts
@@ -7,6 +7,7 @@
  */
 
 import {
+  $createCodeHighlightNode,
   $createCodeNode,
   $isCodeHighlightNode,
   registerCodeHighlighting,
@@ -855,6 +856,34 @@ describe('LexicalCodeNode tests', () => {
           );
         }
       }
+    });
+    describe('initial editor state before transforms', () => {
+      test('can be registered after initial editor state (regression #7014)', async () => {
+        const {editor} = testEnv;
+        await editor.update(
+          () => {
+            const root = $getRoot();
+            const codeBlock = $createCodeNode('javascript');
+            codeBlock.append(
+              $createCodeHighlightNode('const lexical = "awesome"'),
+            );
+            root.append(codeBlock);
+          },
+          {tag: 'history-merge'},
+        );
+        // before transform
+        expect(testEnv.innerHTML).toBe(
+          '<code spellcheck="false" data-language="javascript" data-highlight-language="javascript" dir="ltr"><span data-lexical-text="true">const lexical = "awesome"</span></code>',
+        );
+        registerRichText(editor);
+        registerTabIndentation(editor);
+        registerCodeHighlighting(editor);
+        await Promise.resolve(undefined);
+        // after transforms
+        expect(testEnv.innerHTML).toBe(
+          '<code spellcheck="false" data-language="javascript" data-highlight-language="javascript" dir="ltr" data-gutter="1"><span data-lexical-text="true">const</span><span data-lexical-text="true"> lexical </span><span data-lexical-text="true">=</span><span data-lexical-text="true"> </span><span data-lexical-text="true">"awesome"</span></code>',
+        );
+      });
     });
   });
 });
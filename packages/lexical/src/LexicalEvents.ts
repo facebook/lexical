46c9c2f9d738f974cc9b7cacf7088c168edba78c
CityHunter <62472294+EruditionTu@users.noreply.github.com>
2025-01-18 13:00:38 +0800

    [lexical] Bug Fix: In the Safari browser, during the compositing event process, the delete key exhibits unexpected behavior. (#7061)
    Co-authored-by: 涂博闻 <tubowen@moonshot.cn>

--- packages/lexical/src/LexicalEvents.ts
+++ packages/lexical/src/LexicalEvents.ts
@@ -176,6 +176,8 @@ let isSelectionChangeFromDOMUpdate = false;
 let isSelectionChangeFromMouseDown = false;
 let isInsertLineBreak = false;
 let isFirefoxEndingComposition = false;
+let isSafariEndComposition = false;
+let safariEndCompositionEventData = '';
 let collapsedSelectionFormat: [number, string, number, NodeKey, number] = [
   0,
   '',
@@ -1006,6 +1008,9 @@ function onCompositionEnd(
   // the logic in onInput.
   if (IS_FIREFOX) {
     isFirefoxEndingComposition = true;
+  } else if (!IS_IOS && (IS_SAFARI || IS_APPLE_WEBKIT)) {
+    isSafariEndComposition = true;
+    safariEndCompositionEventData = event.data;
   } else {
     updateEditorSync(editor, () => {
       $onCompositionEndImpl(editor, event.data);
@@ -1027,6 +1032,14 @@ function onKeyDown(event: KeyboardEvent, editor: LexicalEditor): void {
   }
 
   if (key == null) {
+    return;
+  }
+  if (isSafariEndComposition && isBackspace(lastKeyCode)) {
+    updateEditorSync(editor, () => {
+      $onCompositionEndImpl(editor, safariEndCompositionEventData);
+    });
+    isSafariEndComposition = false;
+    safariEndCompositionEventData = '';
     return;
   }
 

2a78ee87501766222ce6f0a66edcf6f2bc89259e
Sherry Wong <potatowagon@fb.com>
2025-02-04 22:13:45 +0800

    install babel plugin-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,9 @@ module.exports = {
       require('./scripts/error-codes/transform-error-messages'),
       {noMinify: true},
     ],
+    // When this plugin is enabled, the useBuiltIns option in @babel/preset-env must not be set. Otherwise, this plugin may not able to completely sandbox the environment.
+    // https://babeljs.io/docs/babel-plugin-transform-runtime
+    ['@babel/plugin-transform-runtime'],
   ],
   presets: [
     [

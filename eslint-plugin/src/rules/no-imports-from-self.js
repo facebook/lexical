3af216e4afdb15cceebcbffe5f5b5dd1aabc184a
Alessio Gravili <alessio@gravili.de>
2025-03-02 00:42:37 -0700

    update license

--- eslint-plugin/src/rules/no-imports-from-self.js
+++ eslint-plugin/src/rules/no-imports-from-self.js
@@ -1,29 +1,12 @@
 /**
- * From: https://github.com/payloadcms/payload/blob/main/packages/eslint-plugin/customRules/no-imports-from-self.js
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Portions Copyright (c) Payload CMS, Inc. <info@payloadcms.com>
  *
- * MIT License
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  *
- * Copyright (c) 2018-2025 Payload CMS, Inc. <info@payloadcms.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * 'Software'), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+// eslint-disable-next-line header/header
 'use strict';
 
 const fs = require('fs');

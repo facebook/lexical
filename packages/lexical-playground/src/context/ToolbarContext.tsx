3f38d1dbccd8ca70f3b3d2153c3f41de24cb3b8b
kirandash <kirandash07@gmail.com>
2025-04-14 21:55:49 +0800

    refactor: remove 'Fit' option from zoom level types
    - Update zoomLevel type in ToolbarContext to only use number

--- packages/lexical-playground/src/context/ToolbarContext.tsx
+++ packages/lexical-playground/src/context/ToolbarContext.tsx
@@ -73,8 +73,8 @@ const INITIAL_TOOLBAR_STATE = {
   isUppercase: false,
   isCapitalize: false,
   rootType: 'root' as keyof typeof rootTypeToRootName,
-  // Zoom level in percentage or 'Fit'
-  zoomLevel: 100 as number | 'Fit',
+  // Zoom level in percentage
+  zoomLevel: 100 as number,
 };
 
 type ToolbarState = typeof INITIAL_TOOLBAR_STATE;

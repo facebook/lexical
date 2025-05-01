ce56349d6bacb187914f033649a2807831b953ce
Bob Ippolito <bob@redivi.com>
2025-04-30 17:31:59 -0700

    remove tsconfig and vite.config changes for styled-system

--- examples/node-state-style/vite.config.ts
+++ examples/node-state-style/vite.config.ts
@@ -6,19 +6,9 @@
  *
  */
 import react from '@vitejs/plugin-react';
-import * as path from 'node:path';
 import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      {find: '~', replacement: path.resolve(__dirname, 'src')},
-      {
-        find: 'styled-system',
-        replacement: path.resolve(__dirname, 'styled-system'),
-      },
-    ],
-  },
 });

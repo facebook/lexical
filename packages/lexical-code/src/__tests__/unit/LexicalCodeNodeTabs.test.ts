84a6822f06e5008f27526b01d0eb30bf24c1d05c
jeromew <jerome.wagner@m4x.org>
2025-05-27 08:12:05 +0000

    [lexical-code] Feature: Improve Tab/Indent/Outdent to match VSCode

---
+++ packages/lexical-code/src/__tests__/unit/LexicalCodeNodeTabs.test.ts
@@ -0,0 +1,216 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import type {LexicalCommand} from 'lexical';
+
+import {
+  $createCodeNode,
+  $isCodeNode,
+  registerCodeHighlighting,
+} from '@lexical/code';
+import {registerTabIndentation} from '@lexical/react/LexicalTabIndentationPlugin';
+import {registerRichText} from '@lexical/rich-text';
+import {
+  $createRangeSelection,
+  $getRoot,
+  $getSelection,
+  $isLineBreakNode,
+  $setSelection,
+  INDENT_CONTENT_COMMAND,
+  KEY_TAB_COMMAND,
+  OUTDENT_CONTENT_COMMAND,
+} from 'lexical';
+import {
+  initializeUnitTest,
+  tabKeyboardEvent,
+} from 'lexical/src/__tests__/utils';
+
+describe('LexicalCodeNode tests', () => {
+  initializeUnitTest((testEnv) => {
+    describe('Tabs', () => {
+      // Tests are described using the following convention
+      // > is the beginning of a line
+      // - is a Tab
+      // | is the selection anchor and focus (they may be collapsed)
+      // in the format [before, after, operation]
+      const suite = [
+        ['>|', '>-|', 'tab'],
+        ['>|', '>-|', 'indent'],
+        ['>|', '>|', 'outdent'],
+        ['>|line1', '>-|line1', 'tab'],
+        ['>|line1', '>-|line1', 'indent'],
+        ['>|line1', '>|line1', 'outdent'],
+        ['>-li|ne1', '>-li-|ne1', 'tab'],
+        ['>-li|ne1', '>--li|ne1', 'indent'],
+        ['>-li|ne1', '>li|ne1', 'outdent'],
+        ['>|-line1|', '>|--line1|', 'tab'],
+        ['>|-line1|', '>|--line1|', 'indent'],
+        ['>|-line1|', '>|line1|', 'outdent'],
+        ['>-li|ne1|', '>-li-|', 'tab'],
+        ['>-li|ne1|', '>--li|ne1|', 'indent'],
+        ['>-li|ne1|', '>li|ne1|', 'outdent'],
+        ['>|word1| word2', '>-| word2', 'tab'],
+        ['>|line1>|', '>|-line1>|', 'tab'],
+        ['>|line1>|', '>|-line1>|', 'indent'],
+        ['>|line1>|line2', '>|-line1>|line2', 'tab'],
+        ['>|line1>|line2', '>|-line1>|line2', 'indent'],
+        ['>line1|>line2|', '>-line1|>-line2|', 'tab'],
+        ['>line1|>line2|', '>-line1|>-line2|', 'indent'],
+        ['>|>li|ne1', '>|>-li|ne1', 'tab'],
+        ['>|>li|ne1', '>|>-li|ne1', 'indent'],
+        ['>|>|line1', '>|->|line1', 'tab'],
+        ['>|>|line1', '>|->|line1', 'indent'],
+        ['>|>>|line1', '>|>>|line1', 'tab'],
+        ['>|>>|line1', '>|>>|line1', 'indent'],
+        ['>|line1>li|ne2', '>|-line1>-li|ne2', 'tab'],
+        ['>|line1>li|ne2', '>|-line1>-li|ne2', 'indent'],
+        ['>|-line1>li|ne2', '>|line1>li|ne2', 'outdent'],
+        ['>li|ne1>li|ne2', '>-li|ne1>-li|ne2', 'tab'],
+        ['>li|ne1>li|ne2', '>-li|ne1>-li|ne2', 'indent'],
+        ['>-li|ne1>>-li|ne2', '>--li|ne1>>--li|ne2', 'tab'],
+        ['>-li|ne1>>-li|ne2', '>--li|ne1>>--li|ne2', 'indent'],
+        ['>-li|ne1>>-li|ne2', '>li|ne1>>li|ne2', 'outdent'],
+        ['>|-line1>->-line2|', '>|--line1>-->--line2|', 'tab'],
+      ];
+      suite.forEach((scenario) => {
+        test(`testing ${scenario[2]}: ${scenario[0]} => ${scenario[1]} `, async () => {
+          const {editor} = testEnv;
+
+          const getDispatchArgs = (type) => {
+            switch (type) {
+              case 'indent':
+                return [INDENT_CONTENT_COMMAND, undefined]; // : [LexicalCommand<void>, void];
+              case 'outdent':
+                return [OUTDENT_CONTENT_COMMAND, undefined]; // : [LexicalCommand<void>, void];
+              case 'tabs':
+              default:
+                return [KEY_TAB_COMMAND, tabKeyboardEvent()]; // : [LexicalCommand<KeyboardEvent>, KeyboardEvent];
+            }
+          };
+
+          const getRawTextWithSelection = (input) => {
+            return input
+              .replace(/^>/, '')
+              .replaceAll('>', '\n')
+              .replaceAll('-', '\t');
+          };
+          const getRawText = (input) => {
+            return getRawTextWithSelection(input).replaceAll('|', '');
+          };
+
+          registerRichText(editor);
+          registerTabIndentation(editor);
+          registerCodeHighlighting(editor);
+
+          // fill editor with scenario input text
+          let codeNode;
+          await editor.update(() => {
+            const root = $getRoot();
+            codeNode = $createCodeNode();
+            root.append(codeNode);
+            codeNode.selectStart();
+            $getSelection()!.insertRawText(getRawText(scenario[0]));
+          });
+
+          // Create a RangeSelection that matches the selection
+          // described in the scenario input via the | convention
+          await editor.update(() => {
+            const selection = $createRangeSelection();
+            if ($isCodeNode(codeNode) && codeNode.isEmpty()) {
+              selection.anchor.set(codeNode.getKey(), 0, 'element');
+              selection.focus.set(codeNode.getKey(), 0, 'element');
+            } else {
+              const rawText = getRawTextWithSelection(scenario[0]);
+              const selFirst = rawText.indexOf('|');
+              let selLast = rawText.lastIndexOf('|');
+
+              // if selection is not collapsed, we need to adjust
+              // the last index to take account of the first |
+              if (selLast !== selFirst) {
+                selLast -= 1;
+              }
+
+              let matching = $isCodeNode(codeNode) && codeNode.getFirstChild();
+              let parentIndex = 0;
+              let offset = 0;
+              while (
+                matching !== null &&
+                offset + matching.getTextContentSize() < selFirst
+              ) {
+                offset += matching.getTextContentSize();
+                matching = matching.getNextSibling();
+                parentIndex++;
+              }
+              if (!$isLineBreakNode(matching)) {
+                selection.anchor.set(
+                  matching.getKey(),
+                  selFirst - offset,
+                  'text',
+                );
+              } else {
+                selection.anchor.set(codeNode.getKey(), parentIndex, 'element');
+              }
+              while (
+                matching !== null &&
+                offset + matching.getTextContentSize() < selLast
+              ) {
+                offset += matching.getTextContentSize();
+                matching = matching.getNextSibling();
+                parentIndex++;
+              }
+              if (!$isLineBreakNode(matching)) {
+                selection.focus.set(
+                  matching.getKey(),
+                  selLast - offset,
+                  'text',
+                );
+              } else {
+                selection.focus.set(
+                  codeNode.getKey(),
+                  parentIndex + 1,
+                  'element',
+                );
+              }
+            }
+            $setSelection(selection);
+
+            // dispatch command from the current context to avoid
+            // selectionTarget.getBoundingClientRect is not a function Error
+            editor.dispatchCommand(
+              ...(getDispatchArgs(scenario[2]) as [
+                LexicalCommand<unknown>,
+                unknown,
+              ]),
+            );
+          });
+
+          editor.read(() => {
+            const output = scenario[1].replaceAll('|', '');
+            const selectedOutput = scenario[1]
+              .replace(/^[^|]*/, '')
+              .replace(/[^|]*$/, '')
+              .replaceAll('|', '');
+            expect(
+              '>' +
+                codeNode
+                  .getTextContent()
+                  .replaceAll('\t', '-')
+                  .replaceAll('\n', '>'),
+            ).toBe(output);
+            expect(
+              $getSelection()
+                .getTextContent()
+                .replaceAll('\t', '-')
+                .replaceAll('\n', '>'),
+            ).toBe(selectedOutput);
+          });
+        });
+      });
+    });
+  });
+});

d42b94e48c14d569755150dad2caacf56cf6b615
Yuncheng Lu <yunclu@meta.com>
2025-05-28 13:45:29 +0800

    update docusaurus version to 3.8

--- packages/lexical-website/docusaurus.config.js
+++ packages/lexical-website/docusaurus.config.js
@@ -228,9 +228,7 @@ const config = {
   future: {
     // See https://docusaurus.io/blog/releases/3.6
     experimental_faster: true,
-    v4: {
-      removeLegacyPostBuildHeadAttribute: true,
-    },
+    v4: true,
   },
 
   markdown: {

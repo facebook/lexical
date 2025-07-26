55fc17d80df26db9ebca880285ae4fa7afac50c1
Salihu <salihudickson@gmail.com>
2025-07-26 21:16:01 +0100

    fix regresstion tests

--- packages/lexical-playground/__tests__/regression/2648-block-formatting.spec.mjs
+++ packages/lexical-playground/__tests__/regression/2648-block-formatting.spec.mjs
@@ -8,23 +8,20 @@
 
 import {
   assertHTML,
+  click,
   focusEditor,
   html,
   initialize,
   test,
 } from '../utils/index.mjs';
 
-test.describe('Regression test #2648', () => {
-  test.beforeEach(({isCollab, page, isPlainText}) => {
-    test.skip(isPlainText);
-
-    initialize({isCollab, page});
-  });
+test.describe.parallel('Regression test #2648', () => {
+  test.beforeEach(({isCollab, page}) => initialize({isCollab, page}));
 
   // some distinct formats
   const blockFormats = [
     {
-      dropDown: 'style',
+      dropDownSelector: '.block-controls',
       expected: html`
         <h1
           class="PlaygroundEditorTheme__h1 PlaygroundEditorTheme__ltr"
@@ -32,10 +29,11 @@ test.describe('Regression test #2648', () => {
           <span data-lexical-text="true">line of text</span>
         </h1>
       `,
+      formatSelector: '.dropdown .icon.h1',
       name: 'Heading 1',
     },
     {
-      dropDown: 'style',
+      dropDownSelector: '.block-controls',
       expected: html`
         <blockquote
           class="PlaygroundEditorTheme__quote PlaygroundEditorTheme__ltr"
@@ -43,23 +41,33 @@ test.describe('Regression test #2648', () => {
           <span data-lexical-text="true">line of text</span>
         </blockquote>
       `,
+      formatSelector: '.dropdown .icon.quote',
       name: 'Quote',
     },
     {
-      dropDown: 'style',
+      dropDownSelector: '.block-controls',
       expected: html`
-        <p
-          class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
-          dir="ltr">
-          <code spellcheck="false" data-lexical-text="true">
-            <span class="PlaygroundEditorTheme__textCode">line of text</span>
-          </code>
-        </p>
+        <code
+          class="PlaygroundEditorTheme__code PlaygroundEditorTheme__ltr"
+          dir="ltr"
+          spellcheck="false"
+          data-gutter="1"
+          data-highlight-language="javascript"
+          data-language="javascript">
+          <span data-lexical-text="true">line</span>
+          <span
+            class="PlaygroundEditorTheme__tokenAttr"
+            data-lexical-text="true">
+            of
+          </span>
+          <span data-lexical-text="true">text</span>
+        </code>
       `,
+      formatSelector: '.dropdown .icon.code',
       name: 'Code Block',
     },
     {
-      dropDown: 'alignment',
+      dropDownSelector: '.alignment',
       expected: html`
         <p
           class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
@@ -68,10 +76,11 @@ test.describe('Regression test #2648', () => {
           <span data-lexical-text="true">line of text</span>
         </p>
       `,
-      name: 'Right Align',
+      formatSelector: '.dropdown .icon.right-align',
+      name: 'Text Align',
     },
     {
-      dropDown: 'alignment',
+      dropDownSelector: '.alignment',
       expected: html`
         <p
           class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr PlaygroundEditorTheme__indent"
@@ -80,60 +89,64 @@ test.describe('Regression test #2648', () => {
           <span data-lexical-text="true">line of text</span>
         </p>
       `,
+      formatSelector: '.dropdown .icon.indent',
       name: 'Indent',
     },
   ];
 
   test.describe
     .parallel('Block formatting after selecting the entire line', () => {
-    blockFormats.forEach(({name, dropDown, expected}) => {
-      test(`when ${name} format is applied`, async ({page}) => {
-        await focusEditor(page);
-        await page.keyboard.type('line of text');
-        await page.keyboard.press('Enter');
-        await page.keyboard.type('line of text');
-        await page.keyboard.press('Enter');
-        await page.keyboard.type('line of text');
-
-        const firstLine = await page.getByText('line of text').nth(0);
-        const secondLine = await page.getByText('line of text').nth(1);
-        const dropDownButton = await page
-          .getByRole('button', {
-            name: 'Formatting options for text ' + dropDown,
-          })
-          .first();
-        const unFormattedLine = html`
-          <p
-            class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
-            dir="ltr">
-            <span data-lexical-text="true">line of text</span>
-          </p>
-        `;
-
-        //format and check the first line
-        await firstLine.click({clickCount: 4}); // Note: the issue sometimes shows up on  triple click but the 4th click seems to be a more reliable way to replicate it
-        await dropDownButton.click();
-        await page.getByRole('button', {name}).first().click();
-        await assertHTML(
+    blockFormats.forEach(
+      ({name, dropDownSelector, formatSelector, expected}) => {
+        test(`when ${name} format is applied`, async ({
           page,
-          html`
-            ${expected} ${unFormattedLine} ${unFormattedLine}
-          `,
-        );
+          isPlainText,
+          isCollab,
+        }) => {
+          test.skip(isPlainText);
 
-        await page.keyboard.press('Escape');
+          await focusEditor(page);
+          await page.keyboard.type('line of text');
+          await page.keyboard.press('Enter');
+          await page.keyboard.type('line of text');
+          await page.keyboard.press('Enter');
+          await page.keyboard.type('line of text');
 
-        // format and test the middle line
-        await secondLine.click({clickCount: 4});
-        await dropDownButton.click();
-        await page.getByRole('button', {name}).first().click();
-        await assertHTML(
-          page,
-          html`
-            ${expected} ${expected} ${unFormattedLine}
-          `,
-        );
-      });
-    });
+          const firstLine = '.PlaygroundEditorTheme__paragraph:nth-child(1)';
+          const secondLine = '.PlaygroundEditorTheme__paragraph:nth-child(2)';
+          const unFormattedLine = html`
+            <p
+              class="PlaygroundEditorTheme__paragraph PlaygroundEditorTheme__ltr"
+              dir="ltr">
+              <span data-lexical-text="true">line of text</span>
+            </p>
+          `;
+
+          //format and check the first line
+          await click(page, firstLine, {clickCount: 4}); // Note: the issue sometimes shows up on  triple click but the 4th click seems to be a more reliable way to replicate it
+          await click(page, dropDownSelector);
+          await click(page, formatSelector);
+          await assertHTML(
+            page,
+            html`
+              ${expected} ${unFormattedLine} ${unFormattedLine}
+            `,
+          );
+
+          await page.keyboard.press('Escape');
+
+          // format and test the middle line
+          await click(page, secondLine, {clickCount: 4});
+          await click(page, dropDownSelector);
+          await click(page, formatSelector);
+          await assertHTML(
+            page,
+            html`
+              ${expected} ${expected} ${unFormattedLine}
+            `,
+          );
+        });
+      },
+    );
   });
 });

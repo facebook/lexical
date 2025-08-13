e0849ec4172fab24e653df11fae5a93c03078114
Zsolt Felszeghy <zfelszeghy@microsoft.com>
2025-08-13 09:49:10 +0200

    Add NodeMapBuilder Add initial set of tests for createSelectionFromOffsets

---
+++ packages/lexical-offset/src/__tests__/unit/OffsetView.test.ts
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import {$createOffsetView, type OffsetView} from '@lexical/offset';
+import {
+  $getNodeByKey,
+  type EditorState,
+  type LexicalEditor,
+  type LexicalNode,
+} from 'lexical';
+
+import {NodeMapBuilder} from './helpers/NodeMapBuilder';
+
+describe('OffsetView', () => {
+  describe('createSelectionFromOffsets', () => {
+    it('should return null when end offset is over text length', () => {
+      const nodeMapBuilder = new NodeMapBuilder();
+      const nodeMap = nodeMapBuilder
+        .addRootNode()
+        .addParagraphNode()
+        .addTextNode('text')
+        .build();
+      const offsetView: OffsetView = $arrangeOffsetView(nodeMap);
+
+      const selection = offsetView.createSelectionFromOffsets(0, 6);
+
+      expect(selection).toBeNull();
+    });
+
+    it('should return null when start offset is over text length', () => {
+      const nodeMapBuilder = new NodeMapBuilder();
+      const nodeMap = nodeMapBuilder
+        .addRootNode()
+        .addParagraphNode()
+        .addTextNode('text')
+        .build();
+      const offsetView: OffsetView = $arrangeOffsetView(nodeMap);
+
+      const selection = offsetView.createSelectionFromOffsets(6, 0);
+
+      expect(selection).toBeNull();
+    });
+
+    it('should return selection with anchor being same as focus when start offset is same as end offset', () => {
+      const nodeMapBuilder = new NodeMapBuilder();
+      const nodeMap = nodeMapBuilder
+        .addRootNode()
+        .addParagraphNode()
+        .addTextNode('text', 'targetNode')
+        .build();
+      const offsetView: OffsetView = $arrangeOffsetView(nodeMap);
+
+      const selection = offsetView.createSelectionFromOffsets(0, 0);
+
+      expect(selection).toBeTruthy();
+
+      if (!selection) {
+        throw new Error('Selection is null');
+      }
+
+      expect(selection.anchor.key).toBe('targetNode');
+      expect(selection.anchor.key).toEqual(selection.focus.key);
+      expect(selection.anchor.offset).toBe(0);
+      expect(selection.anchor.offset).toEqual(selection.focus.offset);
+      expect(selection.anchor.type).toBe('text');
+      expect(selection.anchor.type).toEqual(selection.focus.type);
+    });
+  });
+});
+
+function $arrangeOffsetView(
+  nodeMap: Map<string, LexicalNode>,
+  doArrangeGetNodeByKey: boolean = true,
+): OffsetView {
+  if (doArrangeGetNodeByKey) {
+    ($getNodeByKey as jest.Mock).mockImplementation((key) => {
+      return nodeMap.get(key);
+    });
+  }
+  const editorState = {
+    _nodeMap: nodeMap,
+  } as EditorState;
+  const editor = {} as LexicalEditor;
+  const offsetView: OffsetView = $createOffsetView(editor, 1, editorState);
+  return offsetView;
+}
+
+jest.mock('lexical', () => {
+  const actual = jest.requireActual('lexical');
+  return {
+    ...actual,
+    $createRangeSelection: jest.fn(() => {
+      const createPointStub = () => {
+        const pointStub = {
+          key: '',
+          offset: -1,
+          set: (key: string, offset: number, type: string) => {},
+          type: 'element',
+        };
+        pointStub.set = (key: string, offset: number, type: string) => {
+          pointStub.key = key;
+          pointStub.offset = offset;
+          pointStub.type = type;
+        };
+        return pointStub;
+      };
+
+      return {
+        anchor: createPointStub(),
+        focus: createPointStub(),
+      };
+    }),
+    $getNodeByKey: jest.fn(),
+    $isElementNode: jest.fn((node) => {
+      return node.__type === 'element' || node.__type === 'paragraph';
+    }),
+    $isTextNode: jest.fn((node) => {
+      return node.__type === 'text';
+    }),
+  };
+});
---
+++ packages/lexical-offset/src/__tests__/unit/helpers/NodeMapBuilder.ts
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import {
+  type ElementNode,
+  type LexicalNode,
+  type LineBreakNode,
+  type ParagraphNode,
+  type RootNode,
+  type TextNode,
+} from 'lexical';
+
+export class NodeMapBuilder {
+  private nodeMap: Map<string, LexicalNode> = new Map();
+  private rootNode: RootNode | null = null;
+  private paragraphKeyToChildren: Map<string, LexicalNode[]> = new Map();
+  private currentParagraph: ParagraphNode | null = null;
+
+  addRootNode(): NodeMapBuilder {
+    if (this.rootNode) {
+      throw new Error('Root node already exists');
+    }
+    const key = 'root';
+    const rootNode = {
+      ...this.createBaseStubNode('root', key),
+      __first: null,
+    } as RootNode;
+    this.nodeMap.set(key, rootNode);
+    this.rootNode = rootNode;
+    return this;
+  }
+
+  addParagraphNode(key?: string): NodeMapBuilder {
+    if (!this.rootNode) {
+      throw new Error('Root node does not exist');
+    }
+
+    const paragraphNode = {
+      ...this.createBaseStubNode('paragraph', key),
+      __first: null,
+    } as ParagraphNode;
+    this.nodeMap.set(paragraphNode.__key, paragraphNode);
+    if (this.currentParagraph === null) {
+      this.rootNode.__first = paragraphNode.__key;
+    } else {
+      this.currentParagraph.__next = paragraphNode.__key;
+    }
+
+    this.currentParagraph = paragraphNode;
+    this.paragraphKeyToChildren.set(paragraphNode.__key, []);
+
+    return this;
+  }
+
+  addTextNode(text: string, key?: string): NodeMapBuilder {
+    if (this.currentParagraph === null) {
+      throw new Error('No paragraph to add text node to');
+    }
+
+    const textNode = {
+      ...this.createBaseStubNode('text', key),
+      __text: text,
+      getTextContentSize: () => text.length,
+    } as TextNode;
+    this.nodeMap.set(textNode.__key, textNode);
+    this.linkNode(textNode);
+
+    return this;
+  }
+
+  addLineBreakNode(key?: string): NodeMapBuilder {
+    if (this.currentParagraph === null) {
+      throw new Error('No paragraph to add line break node to');
+    }
+
+    const lineBreakNode = {
+      ...this.createBaseStubNode('lineBreak', key),
+    } as LineBreakNode;
+    this.nodeMap.set(lineBreakNode.__key, lineBreakNode);
+    this.linkNode(lineBreakNode);
+
+    return this;
+  }
+
+  build(): Map<string, LexicalNode> {
+    return this.nodeMap;
+  }
+
+  private linkNode(newNode: LexicalNode) {
+    if (this.currentParagraph === null) {
+      throw new Error('No paragraph to link node to');
+    }
+
+    if (this.currentParagraph.__first === null) {
+      this.currentParagraph.__first = newNode.__key;
+    }
+
+    const childrenOfContainingParagraph = this.paragraphKeyToChildren.get(
+      this.currentParagraph.__key,
+    );
+    if (childrenOfContainingParagraph) {
+      const linkTo = childrenOfContainingParagraph.at(-1);
+      if (linkTo) {
+        linkTo.__next = newNode.__key;
+      }
+
+      newNode.getIndexWithinParent = () => childrenOfContainingParagraph.length;
+      newNode.getParentOrThrow = <T extends ElementNode>(): T =>
+        this.currentParagraph as unknown as T;
+
+      childrenOfContainingParagraph.push(newNode);
+    }
+  }
+
+  private createBaseStubNode(type: string, key?: string): LexicalNode {
+    key = key ?? `autoKey-${this.nodeMap.size + 1}-${type}`;
+    const node = {
+      __key: key,
+      __next: null,
+      __type: type,
+      getKey: () => key,
+    } as LexicalNode;
+
+    node.getNextSibling = <T extends LexicalNode>(): T | null =>
+      node.__next ? (this.nodeMap.get(node.__next) as T) : null;
+
+    return node;
+  }
+}

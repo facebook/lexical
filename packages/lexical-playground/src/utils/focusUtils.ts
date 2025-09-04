ce5c99bf92a8196e18804be477bfa8db85fea185
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-09-04 12:26:28 -0700

    Add focus utility functions

---
+++ packages/lexical-playground/src/utils/focusUtils.ts
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+const isFocusable = (el: HTMLElement): boolean => {
+  const interactiveTags = [
+    'A',
+    'BUTTON',
+    'INPUT',
+    'SELECT',
+    'TEXTAREA',
+    'DETAILS',
+    'SUMMARY',
+  ];
+  if (
+    interactiveTags.includes(el.tagName) ||
+    el.hasAttribute('tabindex') ||
+    el.hasAttribute('contenteditable')
+  ) {
+    return true;
+  }
+  return false;
+};
+
+export const findFirstFocusableDescendant = (
+  startElement: HTMLElement,
+): HTMLElement | null => {
+  for (const childNode of startElement.children) {
+    if (!(childNode instanceof HTMLElement)) {
+      continue;
+    }
+
+    if (isFocusable(childNode as HTMLElement)) {
+      return childNode as HTMLElement;
+    }
+
+    const focusableChild = findFirstFocusableDescendant(
+      childNode as HTMLElement,
+    );
+
+    if (focusableChild) {
+      return focusableChild;
+    }
+  }
+
+  return null;
+};
+
+export const focusNearestDescendant = (
+  startElement: HTMLElement,
+): HTMLElement | null => {
+  const el = findFirstFocusableDescendant(startElement);
+  el?.focus();
+  return el;
+};

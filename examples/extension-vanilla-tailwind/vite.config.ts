d6a02134481ef28ab536df9da4c65d3e876c291a
Bob Ippolito <bob@redivi.com>
2025-09-07 19:26:03 -0700

    stackblitz-workaround again

--- examples/extension-vanilla-tailwind/vite.config.ts
+++ examples/extension-vanilla-tailwind/vite.config.ts
@@ -6,9 +6,23 @@
  *
  */
 import tailwindcss from '@tailwindcss/vite';
+import copy from 'rollup-plugin-copy';
 import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    // This is a workaround for https://github.com/tailwindlabs/tailwindcss/issues/18418
+    copy({
+      hook: 'writeBundle',
+      targets: [
+        {
+          dest: './src/stackblitz-workaround/',
+          src: '../../packages/lexical-tailwind/src/*.ts',
+        },
+      ],
+      verbose: true,
+    }),
+    tailwindcss(),
+  ],
 });

2846008c2e687fb2e63756677293e2f3f5fda73a
Bob Ippolito <bob@redivi.com>
2025-09-12 12:55:49 -0700

    maybe fix integration test by upgrading astro and using additional options

--- scripts/__tests__/integration/utils.js
+++ scripts/__tests__/integration/utils.js
@@ -92,7 +92,7 @@ async function buildExample({packageJson, exampleDir}) {
   );
   await withCwd(exampleDir, async () => {
     await exec(
-      `npm install --no-save ${installDeps.map((fn) => `'${fn}'`).join(' ')}`,
+      `npm install --prefix=./ --no-save ${installDeps.map((fn) => `'${fn}'`).join(' ')}`,
     );
     await exec('npm run build');
     if (hasPlaywright) {

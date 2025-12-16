eabdd08205a610c402d6f037b9cf67c8ece33c54
Ajinkya Nikam <ajinkyanikam464@gmail.com>
2025-12-16 22:49:40 +0530

    Refactor text node style handling logic
    Removed unnecessary checks and code related to text node styles.

--- packages/lexical-html/src/index.ts
+++ packages/lexical-html/src/index.ts
@@ -14,7 +14,6 @@ import type {
   ElementFormatType,
   LexicalEditor,
   LexicalNode,
-  TextNode,
 } from 'lexical';
 
 import {$sliceSelectedTextNodeContent} from '@lexical/selection';
@@ -222,25 +221,6 @@ function $createNodesFromDOM(
     ? transformFunction(node as HTMLElement)
     : null;
   let postTransform = null;
-
-  if (
-    node.nodeName === '#text' &&
-    (node.nodeValue !== '' || node.nodeValue !== null) &&
-    ((node as HTMLElement).parentNode as HTMLElement).getAttribute('style') !==
-      null
-  ) {
-    if (transformOutput) {
-      if (transformOutput.node) {
-        const firstTextNode = (transformOutput.node as TextNode[])[0];
-        if (firstTextNode) {
-          firstTextNode.__style =
-            ((node as HTMLElement).parentNode as HTMLElement).getAttribute(
-              'style',
-            ) || '';
-        }
-      }
-    }
-  }
 
   if (transformOutput !== null) {
     postTransform = transformOutput.after;

54940d840764f0e425cad8e5f41b19875f008049
Gerard Rovira <gerard.rovira.sanchez@gmail.com>
2025-12-24 18:14:52 -0500

    .

--- scripts/npm/postversion.js
+++ scripts/npm/postversion.js
@@ -43,6 +43,8 @@ async function main() {
     ['git', 'checkout', '-B', `${CHANNEL}__release`],
     // Update all package.json versions in the monorepo
     `pnpm run update-version`,
+    // Update pnpm-lock.yaml (offline mode prevents fetching from npm)
+    `pnpm install --offline --no-frozen-lockfile`,
     // Fix up all package.json files
     `pnpm run update-packages`,
     // Extract error codes and update changelog, but only in production

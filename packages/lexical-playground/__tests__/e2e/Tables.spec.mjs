3874bdcf723c9447390176857902c36f32ddec24
Ivaylo Pavlov <ivailo90@gmail.com>
2026-01-15 21:51:37 +0000

    fix test expectations 2

--- packages/lexical-playground/__tests__/e2e/Tables.spec.mjs
+++ packages/lexical-playground/__tests__/e2e/Tables.spec.mjs
@@ -903,17 +903,13 @@ test.describe.parallel('Tables', () => {
             <col style="width: 92px" />
           </colgroup>
           <tr>
-            <th
-              class="PlaygroundEditorTheme__tableCellSelected"
-              data-drop-target-for-element="true">
+            <th class="PlaygroundEditorTheme__tableCellSelected">
               <p><span data-lexical-text="true">a</span></p>
             </th>
-            <th
-              class="PlaygroundEditorTheme__tableCellSelected"
-              data-drop-target-for-element="true">
+            <th class="PlaygroundEditorTheme__tableCellSelected">
               <p><span data-lexical-text="true">bb</span></p>
             </th>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><span data-lexical-text="true">cc</span></p>
             </th>
           </tr>
@@ -1048,13 +1044,13 @@ test.describe.parallel('Tables', () => {
             <col style="width: 92px" />
           </colgroup>
           <tr>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><strong data-lexical-text="true">a</strong></p>
             </th>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><strong data-lexical-text="true">bb</strong></p>
             </th>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><strong data-lexical-text="true">cc</strong></p>
             </th>
           </tr>
@@ -1120,13 +1116,13 @@ test.describe.parallel('Tables', () => {
             <col style="width: 92px" />
           </colgroup>
           <tr>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><span data-lexical-text="true">aa</span></p>
             </th>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><span data-lexical-text="true">bb</span></p>
             </th>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><span data-lexical-text="true">cc</span></p>
             </th>
           </tr>
@@ -1600,15 +1596,13 @@ test.describe.parallel('Tables', () => {
           </colgroup>
           <tr>
             <th
-              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader"
-              data-drop-target-for-element="true">
+              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader">
               <p class="PlaygroundEditorTheme__paragraph">
                 <span data-lexical-text="true">cell one</span>
               </p>
             </th>
             <th
-              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader"
-              data-drop-target-for-element="true">
+              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader">
               <p class="PlaygroundEditorTheme__paragraph">
                 <span data-lexical-text="true">first line</span>
               </p>
@@ -1634,15 +1628,13 @@ test.describe.parallel('Tables', () => {
           </colgroup>
           <tr>
             <th
-              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader"
-              data-drop-target-for-element="true">
+              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader">
               <p class="PlaygroundEditorTheme__paragraph">
                 <span data-lexical-text="true">cell one</span>
               </p>
             </th>
             <th
-              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader"
-              data-drop-target-for-element="true">
+              class="PlaygroundEditorTheme__tableCell PlaygroundEditorTheme__tableCellHeader">
               <p class="PlaygroundEditorTheme__paragraph"><br /></p>
             </th>
           </tr>
@@ -5933,10 +5925,10 @@ test.describe.parallel('Tables', () => {
             <col style="width: 92px" />
           </colgroup>
           <tr>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><br /></p>
             </th>
-            <th data-drop-target-for-element="true">
+            <th>
               <p><br /></p>
             </th>
           </tr>

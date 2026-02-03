8dc6e17deeef1fe41201f8ae866a3e30b8652e4e
Randal <126554055+randal-atticus@users.noreply.github.com>
2026-02-04 05:47:21 +1100

    [lexical-table][lexical-playground] Feature: Implement "fit nested tables" for nested table pasting (#8097)

--- packages/lexical-playground/__tests__/utils/index.mjs
+++ packages/lexical-playground/__tests__/utils/index.mjs
@@ -98,6 +98,7 @@ export async function initialize({
   hasNestedTables,
   hasFitNestedTables,
   shouldDisableFocusOnClickChecklist,
+  hasFitNestedTables,
   showNestedEditorTreeView,
   tableCellMerge,
   tableCellBackgroundColor,

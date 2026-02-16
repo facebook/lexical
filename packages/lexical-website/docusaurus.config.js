9c930e23cb5c4e87b0c34ad546e04bf8e4a84ec7
Bagoyy <rzkyannda1207@gmail.com>
2026-02-16 13:40:06 +0700

    fix(website): resolve sidebar generator crash and formatting

--- packages/lexical-website/docusaurus.config.js
+++ packages/lexical-website/docusaurus.config.js
@@ -6,27 +6,27 @@
  *
  */
 
-"use strict";
+'use strict';
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const { github: lightCodeTheme, dracula: darkCodeTheme } =
-  require("prism-react-renderer").themes;
-const slugifyPlugin = require("./src/plugins/lexical-remark-slugify-anchors");
-const { packagesManager } = process.env.FB_INTERNAL
+const {github: lightCodeTheme, dracula: darkCodeTheme} =
+  require('prism-react-renderer').themes;
+const slugifyPlugin = require('./src/plugins/lexical-remark-slugify-anchors');
+const {packagesManager} = process.env.FB_INTERNAL
   ? {}
-  : require("../../scripts/shared/packagesManager");
-const path = require("node:path");
+  : require('../../scripts/shared/packagesManager');
+const path = require('node:path');
 
-const TITLE = "Lexical";
-const GITHUB_REPO_URL = "https://github.com/facebook/lexical"; // TODO: Update when repo name updated
-const IOS_GITHUB_REPO_URL = "https://github.com/facebook/lexical-ios";
+const TITLE = 'Lexical';
+const GITHUB_REPO_URL = 'https://github.com/facebook/lexical'; // TODO: Update when repo name updated
+const IOS_GITHUB_REPO_URL = 'https://github.com/facebook/lexical-ios';
 
 function sourceLinkOptions() {
   const sourceLinkTemplate = `${GITHUB_REPO_URL}/tree/{gitRevision}/{path}#L{line}`;
   return {
     disableGit: true,
-    gitRevision: "main",
+    gitRevision: 'main',
     sourceLinkTemplate,
   };
 }
@@ -37,18 +37,18 @@ function sourceLinkOptions() {
  */
 /** @type Record<string, string | undefined> */
 const docLabels = {
-  "api/index": "Readme",
-  "api/modules": "Table of Contents",
+  'api/index': 'Readme',
+  'api/modules': 'Table of Contents',
 };
 
 /** @param {string} lowercaseLabel */
 function categoryOrder(lowercaseLabel) {
   switch (lowercaseLabel) {
-    case "Modules":
+    case 'Modules':
       return 0;
-    case "Classes":
+    case 'Classes':
       return 1;
-    case "Interfaces":
+    case 'Interfaces':
       return 2;
     default:
       return Infinity;
@@ -70,11 +70,11 @@ function capitalizeLabel(label) {
 function sidebarSort(a, b) {
   // Categories always come last and have their own defined sort order
   // Otherwise leave the sort as-is
-  if (a.type === "category" && b.type === "category") {
+  if (a.type === 'category' && b.type === 'category') {
     return categoryOrder(a.label) - categoryOrder(b.label);
-  } else if (a.type === "category") {
+  } else if (a.type === 'category') {
     return 1;
-  } else if (b.type === "category") {
+  } else if (b.type === 'category') {
     return -1;
   } else {
     return 0;
@@ -89,10 +89,10 @@ function sidebarSort(a, b) {
  */
 function idToModuleName(id) {
   return id
-    .replace(/^api\/modules\//i, "")
-    .replace(/^lexical_react_/, "@lexical/react/")
-    .replace(/^lexical_/, "@lexical/")
-    .replace(/_/g, "-");
+    .replace(/^api\/modules\//i, '')
+    .replace(/^lexical_react_/, '@lexical/react/')
+    .replace(/^lexical_/, '@lexical/')
+    .replace(/_/g, '-');
 }
 
 /**
@@ -102,7 +102,7 @@ function idToModuleName(id) {
  * @param {string} id
  */
 function classOrInterfaceIdToLabel(id) {
-  return id.replace(/^[^.]+./, "");
+  return id.replace(/^[^.]+./, '');
 }
 
 /**
@@ -113,37 +113,37 @@ const sidebarItemsGenerator = async ({
   ...args
 }) => {
   const items = await defaultSidebarItemsGenerator(args);
-  if (args.item.dirName === "api") {
+  if (args.item.dirName === 'api') {
     return items
       .map((sidebarItem) => {
-        if (sidebarItem.type === "doc" && sidebarItem.id in docLabels) {
-          return { ...sidebarItem, label: docLabels[sidebarItem.id] };
-        } else if (sidebarItem.type !== "category") {
+        if (sidebarItem.type === 'doc' && sidebarItem.id in docLabels) {
+          return {...sidebarItem, label: docLabels[sidebarItem.id]};
+        } else if (sidebarItem.type !== 'category') {
           return sidebarItem;
         }
         /** @type {NormalizedSidebarItem[]} */
         const groupedItems = [];
         for (const item of sidebarItem.items) {
-          if (item.type === "doc" && item.id.match(/^api\/modules\//i)) {
+          if (item.type === 'doc' && item.id.match(/^api\/modules\//i)) {
             const label = idToModuleName(item.id);
             const lastItem = groupedItems.at(-1);
             if (
               lastItem &&
-              lastItem.type === "category" &&
+              lastItem.type === 'category' &&
               lastItem.label === label
             ) {
               lastItem.link = {
                 id: item.id,
-                type: "doc",
+                type: 'doc',
               };
               continue;
             }
             const m = /^(@lexical\/[^/]+)\/(.*)$/.exec(label);
             if (m) {
-              const groupedItem = { ...item, label: m[2] };
+              const groupedItem = {...item, label: m[2]};
               if (
                 lastItem &&
-                lastItem.type === "category" &&
+                lastItem.type === 'category' &&
                 lastItem.label === m[1]
               ) {
                 lastItem.items.push(groupedItem);
@@ -151,18 +151,18 @@ const sidebarItemsGenerator = async ({
                 groupedItems.push({
                   items: [groupedItem],
                   label: m[1],
-                  type: "category",
+                  type: 'category',
                 });
               }
               continue;
             }
-            groupedItems.push({ ...item, label });
-          } else if (item.type === "doc") {
+            groupedItems.push({...item, label});
+          } else if (item.type === 'doc') {
             groupedItems.push({
               ...item,
               label: classOrInterfaceIdToLabel(item.id),
             });
-          } else if (item.type === "category") {
+          } else if (item.type === 'category') {
             groupedItems.push({
               ...item,
               label: idToModuleName(item.label),
@@ -185,18 +185,18 @@ const sidebarItemsGenerator = async ({
 /** @type {import('@docusaurus/types').ParseFrontMatter} */
 const parseFrontMatter = async (params) => {
   const result = await params.defaultParseFrontMatter(params);
-  if (params.filePath.endsWith("/docs/api/modules.md")) {
+  if (params.filePath.endsWith('/docs/api/modules.md')) {
     Object.assign(result.frontMatter, {
       custom_edit_url: null,
       hide_table_of_contents: true,
-      id: "modules",
-      title: "@lexical/monorepo",
+      id: 'modules',
+      title: '@lexical/monorepo',
     });
-  } else if (params.filePath.endsWith("/docs/api/index.md")) {
+  } else if (params.filePath.endsWith('/docs/api/index.md')) {
     Object.assign(result.frontMatter, {
       custom_edit_url: null,
-      id: "index",
-      title: "@lexical/monorepo",
+      id: 'index',
+      title: '@lexical/monorepo',
     });
   }
   return result;
@@ -205,49 +205,44 @@ const parseFrontMatter = async (params) => {
 /** @type {Partial<import('docusaurus-plugin-typedoc/dist/types').PluginOptions>} */
 const docusaurusPluginTypedocConfig = {
   ...sourceLinkOptions(),
-  customAnchorsFormat: "curlyBrace",
+  customAnchorsFormat: 'curlyBrace',
   entryPoints: process.env.FB_INTERNAL
     ? []
-    : packagesManager
-        .getPublicPackages()
-        .flatMap((pkg) =>
-          pkg
-            .getExportedNpmModuleEntries()
-            .map((entry) =>
-              path.relative(
-                __dirname,
-                pkg.resolve("src", entry.sourceFileName),
-              ),
-            ),
-        ),
+    : packagesManager.getPublicPackages().flatMap((pkg) =>
+        pkg
+          .getExportedNpmModuleEntries()
+          .map((entry) =>
+            path.relative(__dirname, pkg.resolve('src', entry.sourceFileName)),
+          ),
+      ),
   excludeInternal: true,
   plugin: [
-    "typedoc-plugin-no-inherit",
-    require.resolve("./src/plugins/lexical-typedoc-plugin-module-name"),
-    require.resolve("./src/plugins/lexical-typedoc-plugin-legacy-router"),
-    "typedoc-plugin-rename-defaults",
+    'typedoc-plugin-no-inherit',
+    require.resolve('./src/plugins/lexical-typedoc-plugin-module-name'),
+    require.resolve('./src/plugins/lexical-typedoc-plugin-legacy-router'),
+    'typedoc-plugin-rename-defaults',
   ],
-  router: "legacy",
-  sidebar: { pretty: true },
-  tsconfig: "../../tsconfig.build.json",
+  router: 'legacy',
+  sidebar: {pretty: true},
+  tsconfig: '../../tsconfig.build.json',
   useCustomAnchors: true,
-  watch: process.env.TYPEDOC_WATCH === "true",
+  watch: process.env.TYPEDOC_WATCH === 'true',
 };
 
-const GIT_COMMIT_SHA = process.env.VERCEL_GIT_COMMIT_SHA || "main";
-const GIT_COMMIT_REF = process.env.VERCEL_GIT_COMMIT_REF || "main";
-const GIT_REPO_OWNER = process.env.VERCEL_GIT_REPO_OWNER || "facebook";
-const GIT_REPO_SLUG = process.env.VERCEL_GIT_REPO_SLUG || "lexical";
+const GIT_COMMIT_SHA = process.env.VERCEL_GIT_COMMIT_SHA || 'main';
+const GIT_COMMIT_REF = process.env.VERCEL_GIT_COMMIT_REF || 'main';
+const GIT_REPO_OWNER = process.env.VERCEL_GIT_REPO_OWNER || 'facebook';
+const GIT_REPO_SLUG = process.env.VERCEL_GIT_REPO_SLUG || 'lexical';
 const STACKBLITZ_PREFIX = `https://stackblitz.com/github/${GIT_REPO_OWNER}/${GIT_REPO_SLUG}/tree/${
-  (GIT_COMMIT_REF === "main" && !process.env.VERCEL_GIT_PULL_REQUEST_ID) ||
-  GIT_COMMIT_REF.endsWith("__release")
+  (GIT_COMMIT_REF === 'main' && !process.env.VERCEL_GIT_PULL_REQUEST_ID) ||
+  GIT_COMMIT_REF.endsWith('__release')
     ? GIT_COMMIT_REF
     : GIT_COMMIT_SHA
 }/`;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  baseUrl: "/",
+  baseUrl: '/',
 
   customFields: {
     GIT_COMMIT_REF,
@@ -256,7 +251,7 @@ const config = {
     STACKBLITZ_PREFIX,
   },
 
-  favicon: "img/favicon.ico",
+  favicon: 'img/favicon.ico',
 
   future: {
     experimental_faster: true,
@@ -267,57 +262,57 @@ const config = {
 
   markdown: {
     hooks: {
-      onBrokenMarkdownLinks: "throw",
+      onBrokenMarkdownLinks: 'throw',
     },
     mermaid: true,
     parseFrontMatter,
-    preprocessor: ({ fileContent }) =>
+    preprocessor: ({fileContent}) =>
       fileContent.replaceAll(
-        "https://stackblitz.com/github/facebook/lexical/tree/main/",
+        'https://stackblitz.com/github/facebook/lexical/tree/main/',
         STACKBLITZ_PREFIX,
       ),
   },
 
-  onBrokenAnchors: "throw",
-  onBrokenLinks: "warn",
-  organizationName: "facebook",
+  onBrokenAnchors: 'throw',
+  onBrokenLinks: 'warn',
+  organizationName: 'facebook',
   plugins: [
     process.env.FB_INTERNAL
       ? null
       : [
-          "./plugins/package-docs",
+          './plugins/package-docs',
           /** @type {import('./plugins/package-docs').PackageDocsPluginOptions} */
           {
-            baseDir: path.resolve(__dirname, ".."),
+            baseDir: path.resolve(__dirname, '..'),
             editUrl: `${GITHUB_REPO_URL}/tree/main/packages/`,
             packageFrontMatter: {
               lexical: [
-                "sidebar_position: 1",
-                "sidebar_label: lexical (core)",
-              ].join("\n"),
+                'sidebar_position: 1',
+                'sidebar_label: lexical (core)',
+              ].join('\n'),
             },
-            targetDir: path.resolve(__dirname, "docs/packages"),
+            targetDir: path.resolve(__dirname, 'docs/packages'),
           },
         ],
-    "./plugins/webpack-buffer",
-    ["docusaurus-plugin-typedoc", docusaurusPluginTypedocConfig],
+    './plugins/webpack-buffer',
+    ['docusaurus-plugin-typedoc', docusaurusPluginTypedocConfig],
     async function tailwindcss() {
       return {
         configurePostCss(postcssOptions) {
-          postcssOptions.plugins.push(require("tailwindcss"));
-          postcssOptions.plugins.push(require("autoprefixer"));
+          postcssOptions.plugins.push(require('tailwindcss'));
+          postcssOptions.plugins.push(require('autoprefixer'));
           return postcssOptions;
         },
-        name: "docusaurus-tailwindcss",
+        name: 'docusaurus-tailwindcss',
       };
     },
     [
-      "@docusaurus/plugin-client-redirects",
+      '@docusaurus/plugin-client-redirects',
       {
         redirects: [
           {
-            from: "/2024-recap",
-            to: "https://github.com/facebook/lexical/discussions/7220",
+            from: '/2024-recap',
+            to: 'https://github.com/facebook/lexical/discussions/7220',
           },
         ],
       },
@@ -326,34 +321,33 @@ const config = {
 
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         blog: {
-          editUrl:
-            GITHUB_REPO_URL + "/tree/main/packages/lexical-website/blog/",
+          editUrl: GITHUB_REPO_URL + '/tree/main/packages/lexical-website/blog/',
           showReadingTime: true,
         },
         docs: {
           beforeDefaultRemarkPlugins: [slugifyPlugin],
-          editUrl: GITHUB_REPO_URL + "/tree/main/packages/lexical-website/",
-          path: "docs",
+          editUrl: GITHUB_REPO_URL + '/tree/main/packages/lexical-website/',
+          path: 'docs',
           sidebarItemsGenerator,
-          sidebarPath: "./sidebars.js",
+          sidebarPath: './sidebars.js',
         },
         gtag: {
-          trackingID: "G-7C6YYBYBBT",
+          trackingID: 'G-7C6YYBYBBT',
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       }),
     ],
   ],
 
-  projectName: "lexical",
+  projectName: 'lexical',
 
-  tagline: "An extensible text editor framework that does things differently",
+  tagline: 'An extensible text editor framework that does things differently',
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -370,109 +364,109 @@ const config = {
           {
             items: [
               {
-                label: "Introduction",
-                to: "/docs/intro",
+                label: 'Introduction',
+                to: '/docs/intro',
               },
             ],
-            title: "Docs",
+            title: 'Docs',
           },
           {
             items: [
               {
-                href: "https://discord.gg/KmG4wQnnD9",
-                label: "Discord",
+                href: 'https://discord.gg/KmG4wQnnD9',
+                label: 'Discord',
               },
               {
-                href: "https://stackoverflow.com/questions/tagged/lexicaljs",
-                label: "Stack Overflow",
+                href: 'https://stackoverflow.com/questions/tagged/lexicaljs',
+                label: 'Stack Overflow',
               },
               {
-                href: "https://twitter.com/lexicaljs",
-                label: "Twitter",
+                href: 'https://twitter.com/lexicaljs',
+                label: 'Twitter',
               },
             ],
-            title: "Community",
+            title: 'Community',
           },
           {
             items: [
               {
-                href: "https://github.com/facebook/lexical",
-                label: "GitHub",
+                href: 'https://github.com/facebook/lexical',
+                label: 'GitHub',
               },
             ],
-            title: "More",
+            title: 'More',
           },
           {
             items: [
               {
-                href: "https://opensource.facebook.com/legal/privacy/",
-                label: "Privacy",
-                rel: "noreferrer noopener",
-                target: "_blank",
+                href: 'https://opensource.facebook.com/legal/privacy/',
+                label: 'Privacy',
+                rel: 'noreferrer noopener',
+                target: '_blank',
               },
               {
-                href: "https://opensource.facebook.com/legal/terms/",
-                label: "Terms",
-                rel: "noreferrer noopener",
-                target: "_blank",
+                href: 'https://opensource.facebook.com/legal/terms/',
+                label: 'Terms',
+                rel: 'noreferrer noopener',
+                target: '_blank',
               },
             ],
-            title: "Legal",
+            title: 'Legal',
           },
         ],
-        style: "dark",
+        style: 'dark',
       },
       navbar: {
         items: [
           {
-            label: "Playground",
-            position: "left",
-            to: "https://playground.lexical.dev/",
+            label: 'Playground',
+            position: 'left',
+            to: 'https://playground.lexical.dev/',
           },
           {
-            label: "Docs",
-            position: "left",
-            sidebarId: "docs",
-            type: "docSidebar",
+            label: 'Docs',
+            position: 'left',
+            sidebarId: 'docs',
+            type: 'docSidebar',
           },
           process.env.FB_INTERNAL
             ? {
-                href: "https://lexical.dev/docs/api/",
-                label: "API",
-                position: "left",
+                href: 'https://lexical.dev/docs/api/',
+                label: 'API',
+                position: 'left',
               }
             : {
-                label: "API",
-                position: "left",
-                sidebarId: "api",
-                type: "docSidebar",
+                label: 'API',
+                position: 'left',
+                sidebarId: 'api',
+                type: 'docSidebar',
               },
-          { label: "Community", position: "left", to: "/community" },
+          {label: 'Community', position: 'left', to: '/community'},
           {
-            href: "https://facebook.github.io/lexical-ios/",
-            label: "iOS",
-            position: "left",
+            href: 'https://facebook.github.io/lexical-ios/',
+            label: 'iOS',
+            position: 'left',
           },
           {
-            label: "Gallery",
-            position: "left",
-            to: "/gallery",
+            label: 'Gallery',
+            position: 'left',
+            to: '/gallery',
           },
           {
             href: GITHUB_REPO_URL,
-            label: "GitHub",
-            position: "right",
+            label: 'GitHub',
+            position: 'right',
           },
           {
             href: IOS_GITHUB_REPO_URL,
-            label: "iOS GitHub",
-            position: "right",
+            label: 'iOS GitHub',
+            position: 'right',
           },
         ].filter((item) => item != null),
         logo: {
-          alt: "Lexical",
-          src: "img/logo.svg",
-          srcDark: "img/logo-dark.svg",
+          alt: 'Lexical',
+          src: 'img/logo.svg',
+          srcDark: 'img/logo-dark.svg',
         },
       },
       prism: {
@@ -482,20 +476,20 @@ const config = {
     }),
 
   themes: [
-    "@docusaurus/theme-mermaid",
+    '@docusaurus/theme-mermaid',
     [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
+      require.resolve('@easyops-cn/docusaurus-search-local'),
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
         hashed: true,
         indexBlog: false,
-        language: ["en"],
+        language: ['en'],
       }),
     ],
   ],
 
   title: TITLE,
-  url: "https://lexical.dev",
+  url: 'https://lexical.dev',
 };
 
 module.exports = config;

fcba0d84333d77c00c9168634c3f8180ecc2821d
Randal Grant <randal@atticus.tech>
2026-03-10 23:25:40 +1100

    fix bizarre firefox-only bug in playground (selecting single-cell merged selection showing wrong menu)
    clear selection

--- packages/lexical-playground/src/plugins/TableActionMenuPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/TableActionMenuPlugin/index.tsx
@@ -159,8 +159,11 @@ function TableActionMenu({
       if ($isTableSelection(selection)) {
         const currentSelectionCounts = computeSelectionCount(selection);
         updateSelectionCounts(computeSelectionCount(selection));
+        const isCollapsedTableSelection = selection.anchor.is(selection.focus);
         setCanMergeCells(
-          currentSelectionCounts.columns > 1 || currentSelectionCounts.rows > 1,
+          !isCollapsedTableSelection &&
+            (currentSelectionCounts.columns > 1 ||
+              currentSelectionCounts.rows > 1),
         );
       }
       // Unmerge cell
@@ -263,6 +266,7 @@ function TableActionMenu({
         $selectLastDescendant(targetCell);
         onClose();
       }
+      clearTableSelection();
     });
   };
 
@@ -270,6 +274,7 @@ function TableActionMenu({
     editor.update(() => {
       $unmergeCell();
     });
+    clearTableSelection();
   };
 
   const insertTableRowAtSelection = useCallback(

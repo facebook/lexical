c6f14a0db81301d0023aa2ca36c874483639ce57
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2026-03-11 12:45:47 +0000

    completed landing hero

--- packages/lexical-website/docusaurus.config.js
+++ packages/lexical-website/docusaurus.config.js
@@ -355,8 +355,6 @@ const config = {
   // Usually your GitHub org/user name.
   projectName: 'lexical',
 
-  tagline: 'An extensible text editor framework that does things differently',
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({

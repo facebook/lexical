d92bd217f4a05496dbb28101a28ca232c80c6afa
Bob Ippolito <bob@redivi.com>
2026-04-10 12:35:14 -0700

    remove unneeded import

--- packages/lexical-playground/src/plugins/PagesExtension/index.ts
+++ packages/lexical-playground/src/plugins/PagesExtension/index.ts
@@ -5,10 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-export {
-  PageSetupDropdownComponent,
-  type PageSetupDropdownProps,
-} from '../PagesReactExtension/PageSetupDropdown';
 export {DEFAULT_PAGE_SETUP, PAGE_SIZES} from './constants';
 export {
   $createPageContentNode,

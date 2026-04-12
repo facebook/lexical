2da5802201ff8014a8f88fb1a62e642f23705fb4
Bob Ippolito <bob@redivi.com>
2026-04-12 05:51:37 +0000

    explicitly unset playwright userAgent

--- playwright.config.js
+++ playwright.config.js
@@ -32,6 +32,7 @@ const config = {
         launchOptions: {
           slowMo: 50,
         },
+        userAgent: undefined,
       },
     },
     {
@@ -42,6 +43,7 @@ const config = {
         launchOptions: {
           slowMo: 50,
         },
+        userAgent: undefined,
       },
     },
     {
@@ -52,6 +54,7 @@ const config = {
         launchOptions: {
           slowMo: 50,
         },
+        userAgent: undefined,
       },
     },
   ],

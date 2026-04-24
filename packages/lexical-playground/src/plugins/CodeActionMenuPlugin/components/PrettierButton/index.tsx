831743c041d3cf572d14b08011a48f94f94855a4
Bob Ippolito <bob@redivi.com>
2026-04-23 22:18:36 -0700

    Force prettier to be sensitive to inline style and use useActionState for the async formatting

--- packages/lexical-playground/src/plugins/CodeActionMenuPlugin/components/PrettierButton/index.tsx
+++ packages/lexical-playground/src/plugins/CodeActionMenuPlugin/components/PrettierButton/index.tsx
@@ -9,75 +9,14 @@ import './index.css';
 
 import {$isCodeNode} from '@lexical/code';
 import {$getNearestNodeFromDOMNode, LexicalEditor} from 'lexical';
-import {Options} from 'prettier';
 import {useState} from 'react';
+
+import {formatCodeWithPrettier} from '../../formatCodeWithPrettier';
 
 interface Props {
   lang: string;
   editor: LexicalEditor;
   getCodeDOMNode: () => HTMLElement | null;
-}
-
-const PRETTIER_PARSER_MODULES = {
-  css: [() => import('prettier/parser-postcss')],
-  html: [() => import('prettier/parser-html')],
-  js: [
-    () => import('prettier/parser-babel'),
-    () => import('prettier/plugins/estree'),
-  ],
-  markdown: [() => import('prettier/parser-markdown')],
-  typescript: [
-    () => import('prettier/parser-typescript'),
-    () => import('prettier/plugins/estree'),
-  ],
-} as const;
-
-type LanguagesType = keyof typeof PRETTIER_PARSER_MODULES;
-
-async function loadPrettierParserByLang(lang: string) {
-  const dynamicImports = PRETTIER_PARSER_MODULES[lang as LanguagesType];
-  const modules = await Promise.all(
-    dynamicImports.map((dynamicImport) => dynamicImport()),
-  );
-  return modules;
-}
-
-async function loadPrettierFormat() {
-  const {format} = await import('prettier/standalone');
-  return format;
-}
-
-export async function formatCodeWithPrettier(content: string, lang: string) {
-  const format = await loadPrettierFormat();
-  const options = getPrettierOptions(lang);
-  const prettierParsers = await loadPrettierParserByLang(lang);
-  options.plugins = prettierParsers.map((parser) => parser.default || parser);
-  return await format(content, options);
-}
-
-const PRETTIER_OPTIONS_BY_LANG: Record<string, Options> = {
-  css: {parser: 'css'},
-  html: {parser: 'html'},
-  js: {parser: 'babel'},
-  markdown: {parser: 'markdown'},
-  typescript: {parser: 'typescript'},
-};
-
-const LANG_CAN_BE_PRETTIER = Object.keys(PRETTIER_OPTIONS_BY_LANG);
-
-export function canBePrettier(lang: string): boolean {
-  return LANG_CAN_BE_PRETTIER.includes(lang);
-}
-
-function getPrettierOptions(lang: string): Options {
-  const options = PRETTIER_OPTIONS_BY_LANG[lang];
-  if (!options) {
-    throw new Error(
-      `CodeActionMenuPlugin: Prettier does not support this language: ${lang}`,
-    );
-  }
-
-  return options;
 }
 
 export function PrettierButton({lang, editor, getCodeDOMNode}: Props) {

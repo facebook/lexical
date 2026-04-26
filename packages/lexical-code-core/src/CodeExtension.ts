a87df47dbdbde0480267c9526169136a66cd975a
levensta <grbchv.s@gmail.com>
2026-04-26 23:30:45 +0300

    add key handlers for creating fallback paragraphs

--- packages/lexical-code-core/src/CodeExtension.ts
+++ packages/lexical-code-core/src/CodeExtension.ts
@@ -6,16 +6,72 @@
  *
  */
 
+import {$findMatchingParent} from '@lexical/utils';
 import {
+  $createParagraphNode,
   $getSelection,
   $isRangeSelection,
   COMMAND_PRIORITY_LOW,
   defineExtension,
+  KEY_ARROW_DOWN_COMMAND,
+  KEY_ARROW_LEFT_COMMAND,
+  KEY_ARROW_RIGHT_COMMAND,
+  KEY_ARROW_UP_COMMAND,
   KEY_ENTER_COMMAND,
+  mergeRegister,
 } from 'lexical';
 
 import {CodeHighlightNode} from './CodeHighlightNode';
-import {$exitCodeNodeOnEnter, CodeNode} from './CodeNode';
+import {$exitCodeNodeOnEnter, $isCodeNode, CodeNode} from './CodeNode';
+
+const $onEscapeUp = () => {
+  const selection = $getSelection();
+  if (
+    $isRangeSelection(selection) &&
+    selection.isCollapsed() &&
+    selection.anchor.offset === 0
+  ) {
+    const code = $findMatchingParent(selection.anchor.getNode(), $isCodeNode);
+
+    if ($isCodeNode(code)) {
+      const parent = code.getParent();
+      if (parent !== null && parent.getFirstChild() === code) {
+        const contentParagraph = code.getFirstDescendant();
+        if (
+          contentParagraph !== null &&
+          selection.anchor.key === contentParagraph.getKey()
+        ) {
+          code.insertBefore($createParagraphNode());
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+const $onEscapeDown = () => {
+  const selection = $getSelection();
+  if ($isRangeSelection(selection) && selection.isCollapsed()) {
+    const code = $findMatchingParent(selection.anchor.getNode(), $isCodeNode);
+
+    if ($isCodeNode(code)) {
+      const parent = code.getParent();
+      if (parent !== null && parent.getLastChild() === code) {
+        const contentParagraph = code.getLastDescendant();
+        if (
+          contentParagraph !== null &&
+          selection.anchor.key === contentParagraph.getKey() &&
+          selection.anchor.offset === contentParagraph.getTextContentSize()
+        ) {
+          code.insertAfter($createParagraphNode());
+        }
+      }
+    }
+  }
+
+  return false;
+};
 
 /**
  * Add code blocks to the editor (syntax highlighting provided separately)
@@ -24,17 +80,47 @@ export const CodeExtension = defineExtension({
   name: '@lexical/code',
   nodes: () => [CodeNode, CodeHighlightNode],
   register(editor) {
-    return editor.registerCommand<KeyboardEvent>(
-      KEY_ENTER_COMMAND,
-      (event) => {
-        const selection = $getSelection();
-        if ($isRangeSelection(selection) && $exitCodeNodeOnEnter(selection)) {
-          event.preventDefault();
-          return true;
-        }
-        return false;
-      },
-      COMMAND_PRIORITY_LOW,
+    return mergeRegister(
+      editor.registerCommand<KeyboardEvent>(
+        KEY_ENTER_COMMAND,
+        (event) => {
+          const selection = $getSelection();
+          if ($isRangeSelection(selection) && $exitCodeNodeOnEnter(selection)) {
+            event.preventDefault();
+            return true;
+          }
+          return false;
+        },
+        COMMAND_PRIORITY_LOW,
+      ),
+      // When collapsible is the last child pressing down/right arrow will insert paragraph
+      // below it to allow adding more content. It's similar what $insertBlockNode
+      // (mainly for decorators), except it'll always be possible to continue adding
+      // new content even if trailing paragraph is accidentally deleted
+      editor.registerCommand(
+        KEY_ARROW_DOWN_COMMAND,
+        $onEscapeDown,
+        COMMAND_PRIORITY_LOW,
+      ),
+      editor.registerCommand(
+        KEY_ARROW_RIGHT_COMMAND,
+        $onEscapeDown,
+        COMMAND_PRIORITY_LOW,
+      ),
+      // When collapsible is the first child pressing up/left arrow will insert paragraph
+      // above it to allow adding more content. It's similar what $insertBlockNode
+      // (mainly for decorators), except it'll always be possible to continue adding
+      // new content even if leading paragraph is accidentally deleted
+      editor.registerCommand(
+        KEY_ARROW_UP_COMMAND,
+        $onEscapeUp,
+        COMMAND_PRIORITY_LOW,
+      ),
+      editor.registerCommand(
+        KEY_ARROW_LEFT_COMMAND,
+        $onEscapeUp,
+        COMMAND_PRIORITY_LOW,
+      ),
     );
   },
 });

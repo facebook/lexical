9fe07685b8f7eef237f3487d2cbdcb465e4b3517
Bob Ippolito <bob@redivi.com>
2026-05-02 10:47:33 -0700

    lint

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+'use strict';
+
 // onnxruntime-node and sharp ship large prebuilt native binaries that are
 // only reachable through the Node entrypoint of @huggingface/transformers,
 // which we never load - the website build aliases @huggingface/transformers

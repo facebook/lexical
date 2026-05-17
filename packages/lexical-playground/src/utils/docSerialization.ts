8856501ba041e051fa01fa8c6225d19b8b95c065
Bob Ippolito <bob@redivi.com>
2026-05-17 15:52:18 -0700

    Apply suggestions from code review
    Co-authored-by: Bob Ippolito <bob@redivi.com>

--- packages/lexical-playground/src/utils/docSerialization.ts
+++ packages/lexical-playground/src/utils/docSerialization.ts
@@ -45,20 +45,19 @@ export async function docToHash(doc: SerializedDocument): Promise<string> {
   if (typeof CompressionStream === 'undefined') {
     CompressionAPIWarning();
     return '';
-  } else {
-    const cs = new CompressionStream('gzip');
-    const writer = cs.writable.getWriter();
-    const [, output] = await Promise.all([
-      writer
-        .write(new TextEncoder().encode(JSON.stringify(doc)))
-        .then(() => writer.close()),
-      readBytestoString(cs.readable.getReader()),
-    ]);
-    return `#doc=${btoa(output)
-      .replace(/\//g, '_')
-      .replace(/\+/g, '-')
-      .replace(/=+$/, '')}`;
   }
+  const cs = new CompressionStream('gzip');
+  const writer = cs.writable.getWriter();
+  const [, output] = await Promise.all([
+    writer
+      .write(new TextEncoder().encode(JSON.stringify(doc)))
+      .then(() => writer.close()),
+    readBytestoString(cs.readable.getReader()),
+  ]);
+  return `#doc=${btoa(output)
+    .replace(/\//g, '_')
+    .replace(/\+/g, '-')
+    .replace(/=+$/, '')}`;
 }
 
 export async function docFromHash(
@@ -71,22 +70,22 @@ export async function docFromHash(
   if (typeof DecompressionStream === 'undefined') {
     CompressionAPIWarning();
     return null;
-  } else {
-    const ds = new DecompressionStream('gzip');
-    const writer = ds.writable.getWriter();
-    const b64 = atob(m[1].replace(/_/g, '/').replace(/-/g, '+'));
-    const array = new Uint8Array(b64.length);
-    for (let i = 0; i < b64.length; i++) {
-      array[i] = b64.charCodeAt(i);
-    }
-    const closed = writer.write(array).then(() => writer.close());
-    const output = [];
-    for await (const chunk of generateReader(
-      ds.readable.pipeThrough(new TextDecoderStream()).getReader(),
-    )) {
-      output.push(chunk);
-    }
-    await closed;
-    return JSON.parse(output.join(''));
+  }
+  const ds = new DecompressionStream('gzip');
+  const writer = ds.writable.getWriter();
+  const b64 = atob(m[1].replace(/_/g, '/').replace(/-/g, '+'));
+  const array = new Uint8Array(b64.length);
+  for (let i = 0; i < b64.length; i++) {
+    array[i] = b64.charCodeAt(i);
+  }
+  const closed = writer.write(array).then(() => writer.close());
+  const output = [];
+  for await (const chunk of generateReader(
+    ds.readable.pipeThrough(new TextDecoderStream()).getReader(),
+  )) {
+    output.push(chunk);
+  }
+  await closed;
+  return JSON.parse(output.join(''));
   }
 }

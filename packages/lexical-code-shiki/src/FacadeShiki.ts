531e44b19d56f0fd336936f780754c23597f7452
Olivier Chevallier <141719779+ochevallier@users.noreply.github.com>
2026-07-13 17:11:49 +0200

    [lexical-code-shiki] Bug Fix: force re-tokenize after async language load (#8830)

--- packages/lexical-code-shiki/src/FacadeShiki.ts
+++ packages/lexical-code-shiki/src/FacadeShiki.ts
@@ -92,12 +92,9 @@ export function loadCodeLanguage(
         if (editor && codeNodeKey) {
           editor.update(() => {
             const codeNode = $getNodeByKey(codeNodeKey);
-            if (
-              $isCodeNode(codeNode) &&
-              codeNode.getLanguage() === language &&
-              !codeNode.getIsSyntaxHighlightSupported()
-            ) {
+            if ($isCodeNode(codeNode) && codeNode.getLanguage() === language) {
               codeNode.setIsSyntaxHighlightSupported(true);
+              codeNode.markDirty();
             }
           });
         }

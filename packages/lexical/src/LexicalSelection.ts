b0f5225cb339ea55f77e88e0291c30249a643098
Abdul Azhar Jamesh <cb.sc.u4aie24301@cb.students.amrita.edu>
2026-09-25 23:11:02 +0000

    [lexical] Bug Fix: Preserve first block after backward range deletion (#9236)

--- packages/lexical/src/LexicalSelection.ts
+++ packages/lexical/src/LexicalSelection.ts
@@ -2071,14 +2071,6 @@ export class RangeSelection implements BaseSelection {
       this.anchor.type === 'element' &&
       this.anchor.offset === 0
     ) {
-      const anchorNode = this.anchor.getNode();
-      if (
-        anchorNode.isEmpty() &&
-        $isRootNode(anchorNode.getParent()) &&
-        anchorNode.getPreviousSibling() === null
-      ) {
-        $collapseAtStart(this, anchorNode);
-      }
       $ensureRootHasParagraph();
     }
   }
